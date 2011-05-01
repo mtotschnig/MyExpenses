cc2fd92ca9441d6e44e1408f004a75d5407312d9
Michael Totschnig <michael@totschnig.org>
2011-05-01 02:00:21 +0200

    date and time picker

--- src/org/totschnig/myexpenses/ExpenseEdit.java
+++ src/org/totschnig/myexpenses/ExpenseEdit.java
@@ -19,19 +19,24 @@ package org.totschnig.myexpenses;
 import java.sql.Timestamp;
 import java.util.Date;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class ExpenseEdit extends Activity {
 
-	private EditText mDateText;
-	private EditText mTimeText;
+	private Button DateButton;
+	private Button TimeButton;
 	private EditText mAmountText;
     private EditText mCommentText;
     private Button categoryButton;
@@ -40,6 +45,14 @@ public class ExpenseEdit extends Activity {
     private ExpensesDbAdapter mDbHelper;
     private int main_cat_id;
     private int sub_cat_id;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHours;
+    private int mMinutes;
+
+    static final int DATE_DIALOG_ID = 0;
+    static final int TIME_DIALOG_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +60,20 @@ public class ExpenseEdit extends Activity {
         mDbHelper = new ExpensesDbAdapter(this);
         mDbHelper.open();
         setContentView(R.layout.one_expense);
-        
-        mDateText = (EditText) findViewById(R.id.Date);
-        mTimeText = (EditText) findViewById(R.id.Time);
+ 
+        DateButton = (Button) findViewById(R.id.Date);
+        DateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        TimeButton = (Button) findViewById(R.id.Time);
+        TimeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
         mAmountText = (EditText) findViewById(R.id.Amount);
         mCommentText = (EditText) findViewById(R.id.Comment);
       
@@ -96,6 +120,39 @@ public class ExpenseEdit extends Activity {
 		    startActivityForResult(i, 0);
     	}
    	}
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+        new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, 
+                                  int monthOfYear, int dayOfMonth) {
+                mYear = year;
+                mMonth = monthOfYear;
+                mDay = dayOfMonth;
+                setDate();
+            }
+        };
+        private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    mHours = hourOfDay;
+                    mMinutes = minute;
+                    setTime();
+                }
+            };
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+            return new DatePickerDialog(this,
+                        mDateSetListener,
+                        mYear, mMonth, mDay);
+	    case TIME_DIALOG_ID:
+	        return new TimePickerDialog(this,
+	                    mTimeSetListener,
+	                    mHours, mMinutes, true);
+        }
+    	return null;
+    }
 //    protected Dialog onCreateDialog(int id) {
 //    	//TODO check for id
 ///*    	ArrayList<Category> items = new ArrayList<Category>();
@@ -120,7 +177,7 @@ public class ExpenseEdit extends Activity {
             String dateString = note.getString(
     	            note.getColumnIndexOrThrow(ExpensesDbAdapter.KEY_DATE));
             Timestamp date = Timestamp.valueOf(dateString);
-            setDate(date);
+            setDateTime(date);
             try {
       		  amount = Float.valueOf(note.getString(
       	            note.getColumnIndexOrThrow(ExpensesDbAdapter.KEY_AMOUNT)));
@@ -143,7 +200,7 @@ public class ExpenseEdit extends Activity {
             categoryButton.setText(note.getString(note.getColumnIndexOrThrow("label")));
         } else {
         	Date date =  new Date();
-        	setDate(date);
+        	setDateTime(date);
         	if (type == MyExpenses.INCOME) {
         		setTitle(R.string.menu_insert_inc);
         	} else {
@@ -156,9 +213,21 @@ public class ExpenseEdit extends Activity {
         	mScreen.setBackgroundColor(android.graphics.Color.RED);
         }
     }
-    private void setDate(Date date) {
-    	mDateText.setText((date.getYear()+1900) + "-" + pad(date.getMonth() + 1) + "-" + pad(date.getDate()));
-        mTimeText.setText(pad(date.getHours()) + ":" + pad(date.getMinutes()));
+    private void setDateTime(Date date) {
+    	mYear = date.getYear()+1900;
+        mMonth = date.getMonth();
+        mDay = date.getDate();
+        mHours = date.getHours();
+        mMinutes = date.getMinutes();
+        
+        setDate();
+        setTime();
+    }
+    private void setDate() {
+    	DateButton.setText(mYear + "-" + pad(mMonth + 1) + "-" + pad(mDay));
+    }
+    private void setTime() {
+    	TimeButton.setText(pad(mHours) + ":" + pad(mMinutes));
     }
     private static String pad(int c) {
         if (c >= 10)
@@ -176,7 +245,7 @@ public class ExpenseEdit extends Activity {
     private void saveState() {
         String amount = mAmountText.getText().toString();
         String comment = mCommentText.getText().toString();
-        String strDate = mDateText.getText().toString() + " " + mTimeText.getText().toString() + ":00.0";
+        String strDate = DateButton.getText().toString() + " " + TimeButton.getText().toString() + ":00.0";
     	if (type == MyExpenses.EXPENSE) {
     		amount = "-"+ amount;
     	}
