ea6dc6e81df852107573026125c8ec353bea253e
Michael Totschnig <michael@totschnig.org>
2012-02-08 18:36:20 +0100

    first working functional UI test

--- tests/src/org/totschnig/myexpenses/test/MyExpensesTest.java
+++ tests/src/org/totschnig/myexpenses/test/MyExpensesTest.java
@@ -2,12 +2,16 @@ package org.totschnig.myexpenses.test;
 
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.MyExpenses;
+import org.totschnig.myexpenses.R;
 
 import com.jayway.android.robotium.solo.Solo;
 
 import android.app.Instrumentation;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 
 /**
@@ -22,6 +26,8 @@ public class MyExpensesTest extends
   private ListAdapter mAdapter;
   private Instrumentation mInstrumentation = null;
   private Solo solo;
+  private SharedPreferences settings;
+  private static final String TEST_ID = "functest";
 
   
 
@@ -32,20 +38,60 @@ public class MyExpensesTest extends
   protected void setUp() throws Exception {
     super.setUp();
     MyApplication app = (MyApplication) getInstrumentation().getTargetContext().getApplicationContext();
-    app.setSettings(app.getSharedPreferences("functest",Context.MODE_PRIVATE));
-    app.setDatabaseName("functest");
+    settings = app.getSharedPreferences(TEST_ID,Context.MODE_PRIVATE);
+    app.setSettings(settings);
+    app.setDatabaseName(TEST_ID);
     setActivityInitialTouchMode(false); 
     mActivity = getActivity();
     mInstrumentation = getInstrumentation();
   } // end of setUp() method definition
   
-  public void testMainScreen() {
+  public void testMainScreen() throws Exception {
     mAdapter = mActivity.getListAdapter();
+    solo = new Solo(mInstrumentation, getActivity());
+    //Help dialog opened
+    assertTrue(solo.searchText(mActivity.getString(R.string.menu_help)));
+    solo.clickOnButton(mActivity.getString(R.string.close));
     assertTrue(mAdapter != null);
     assertEquals(0,mAdapter.getCount());
-    mInstrumentation.invokeMenuActionSync(mActivity, MyExpenses.RESET_ID, 0);
-    solo = new Solo(getInstrumentation(), getActivity());
-    getInstrumentation().waitForIdleSync();
-    assertTrue(solo.searchText("will be deleted"));
+    //reset should not be visible
+    //getInstrumentation().waitForIdleSync();
+    solo.sendKey(Solo.MENU);
+    assertFalse(solo.searchText(mActivity.getString(R.string.menu_reset)));
+    //mInstrumentation.waitForIdleSync();
+    if (!mInstrumentation.invokeMenuActionSync(mActivity, MyExpenses.INSERT_TA_ID, 0))
+      throw new Exception();
+    solo.enterText((EditText) solo.getView(R.id.Amount),"123.45");
+    solo.clickOnButton(mActivity.getString(R.string.done));
+    assertTrue(solo.searchText("123.45"));
+    //since we have now a transaction, reset should be visible
+    solo.sendKey(Solo.MENU);
+    assertTrue(solo.searchText(mActivity.getString(R.string.menu_reset)));
+    //mInstrumentation.waitForIdleSync();
+    if (!mInstrumentation.invokeMenuActionSync(mActivity, MyExpenses.SELECT_ACCOUNT_ID, 0))
+      throw new Exception();
+    //mInstrumentation.waitForIdleSync();
+    //in order to use invokeMenuActionSync here, I'd need to know how to get at the activity we arein
+    //if (!mInstrumentation.invokeMenuActionSync(?, SelectAccount.INSERT_ACCOUNT_ID, 0))
+    //  throw new Exception();
+    solo.pressMenuItem(0);
+    solo.enterText((EditText) solo.getView(R.id.Label),"Testing account");
+    solo.enterText((EditText) solo.getView(R.id.Description),"Created with Robotium");
+    solo.enterText((EditText) solo.getView(R.id.Opening_balance),"456.59");
+    solo.clickOnButton(mActivity.getString(R.string.done));
+    assertTrue(solo.searchText("456.59"));
+    solo.goBack();
+    //since we have now two accounts transfer should be visible
+    solo.sendKey(Solo.MENU);
+    assertTrue(solo.searchText(mActivity.getString(R.string.menu_insert_transfer)));
+//    getInstrumentation().waitForIdleSync();
+//    assertTrue(solo.searchText(mActivity.getString(R.string.warning_reset_account)));
+  }
+  protected void tearDown() throws Exception {
+    solo.finishOpenedActivities();
+    Editor editor = settings.edit();
+    editor.clear();
+    editor.commit();
+    mActivity.deleteDatabase(TEST_ID);
   }
 }
