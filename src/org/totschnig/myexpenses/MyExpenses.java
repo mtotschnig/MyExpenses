1d908a1641c28e8475da24078f91ff923b9efd16
Michael Totschnig <michael@totschnig.org>
2012-02-24 22:58:14 +0100

    changelog refactored

--- src/org/totschnig/myexpenses/MyExpenses.java
+++ src/org/totschnig/myexpenses/MyExpenses.java
@@ -21,8 +21,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -44,7 +46,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 import android.widget.AdapterView.AdapterContextMenuInfo;
@@ -449,6 +453,112 @@ public class MyExpenses extends ListActivity {
   private void openChangesDialog() {
     LayoutInflater li = LayoutInflater.from(this);
     View view = li.inflate(R.layout.changeview, null);
+    ListView changeList = (ListView) view.findViewById(R.id.changelog);
+    final String[][] versions = {
+        {
+          "1.4.5.2", 
+          "February 25 2012",
+          "Tutorial translated to German and French. Fixed problem with orientation change during categories import."
+        },
+        {
+          "1.4.5.1",
+          "February 17 2012",
+          "Problem with export (FTP, email) fixed."
+        },
+        {
+         "1.4.5",
+         "February 13 2012",
+         "Support for sending QIF file via email. Reduced memory footprint."
+        },
+        {
+          "1.4.4",
+          "February 8 2012",
+          "FTP upload done in background. Default account created in user's language and currency."
+        },
+        {
+          "1.4.3",
+          "February 3 2012",
+          "Improved parsing of number input in German locale. Upload of QIF file to FTP server."
+        },
+        {
+          "1.4.1",
+          "January 31 2012",
+          "Input of transaction amounts now respects user's locale. Bug fix: Opening balance could not be set."
+        },
+        {
+          "1.4.0",
+          "January 27 2012",
+          "Currencies now handled with java.util.Currency. Support for transfers between accounts."
+        },
+        {
+          "1.3.2",
+          "January 17 2012",
+          "Improved help and included tutorial."
+        },
+        {
+          "1.3.0",
+          "January 13 2012",
+          "Cleaned up layout."
+        },
+        {
+          "v1.2.8",
+          "January 8 2012",
+          "Fixed bug in export."
+        },
+        {
+          "1.2.7",
+          "January 7 2012",
+          "Added payee field. Ignore empty fields in QIF export."
+        },
+        {
+          "1.2.6",
+          "November 6 2011",
+          "Better management of categories: Default category files from Grisbi are now included in the app, categories can be deleted and renamed."
+        },
+        {
+          "1.2.0",
+          "October 7 2011",
+          "Support for multiple accounts"
+        },
+        {
+          "1.1.3",
+          "June 5 2011",
+          "Use workaround class for bug in SimpleCursorTreeAdapter (NullPointerException when adding the first subcategory) from http://code.google.com/p/android/issues/detail?id=9170; better feedback when category import goes wrong."
+        },
+        {
+          "1.1.2",
+          "May 18 2011",
+          "Sort categories by usages; Allow assignment of main category; Allow manual creation of categories; Improved handling of categories import."
+        },
+        {
+          "1.0",
+          "May 4 2011",
+          "Initial version"
+        }
+    };
+    ArrayList<HashMap<String, String>> versionList = new ArrayList<HashMap<String, String>>();
+    for (String[] version : versions) {
+      HashMap<String, String> map = new HashMap<String, String>();
+      map.put("version", version[0]);
+      map.put("date", version[1]);
+      map.put("changes", version[2]);
+      versionList.add(map);
+    }
+    ListAdapter adapter = new SimpleAdapter(this, versionList , R.layout.version_row, 
+        new String[] { "version", "date","changes" }, 
+        new int[] { R.id.version, R.id.date, R.id.changes }) {
+        public boolean isEnabled(int position) 
+        { 
+          return false; 
+        }
+        public boolean areAllItemsEnabled() 
+        { 
+          return false; 
+        }
+    };
+    changeList.setAdapter(adapter);
+
+    
     
     new AlertDialog.Builder(MyExpenses.this)
     .setTitle(R.string.menu_changes)
