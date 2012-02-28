bfee2e792ff1eddc828f3e4a51efb8856c4c4e12
Michael Totschnig <michael@totschnig.org>
2012-02-28 15:30:49 +0100

    script for adding lines to tutorial html for online version; javascript for handling larger images

---
+++ template/images.js
@@ -0,0 +1,33 @@
+var imageDialog;
+
+$(document).ready(function() {
+imageDialog =$("<div id='dialog'><img id='image' src=''/></div>").dialog({
+      modal: true,
+      resizable: false,
+      draggable: false,
+      width: 'auto',
+      autoOpen: false
+    });
+       
+  $('.screen > img').click(function(event){
+       
+    event.preventDefault();
+    PreviewImage($(this).attr('src'));
+                                       
+  });                     
+});
+
+PreviewImage = function(uri) {
+  uri = "large/" +uri;
+  //Get the HTML Elements
+  imageTag = $('#image');
+ 
+  //Split the URI so we can get the file name
+  uriParts = uri.split("/");
+
+  //Set the image src
+  imageTag.attr('src', uri);
+ 
+  //When the image has loaded, display the dialog
+  imageDialog.dialog("open");
+}
