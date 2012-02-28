68869aef985b30b2350e238bdb90f58cf9f09e4d
Michael Totschnig <michael@totschnig.org>
2012-02-28 18:35:03 +0100

    IE specific adaptations

--- assets/tutorial/lang.js
+++ assets/tutorial/lang.js
@@ -4,6 +4,7 @@ var userLang;
 var doc = document,
     head = doc.getElementsByTagName('head')[0],
     style = doc.createElement('style');
+head.appendChild( style);
 //unfortunately navigator.language is broken in Androids Webkit
 //we set userAgent to language through webview settings
 //and use this as test if we are displaying the tutorial as In-App-Activity
@@ -15,8 +16,10 @@ userLang = readCookie("lang");
 if (!userLang) {
   if (inAppP)
     userLang = userAgent;
-  else
-	  userLang = navigator.language.substr(0,2); 
+  else {
+    userLang = navigator.userLanguage || navigator.language;
+    userLang = userLang.substr(0,2);
+  }
 	if(!known[userLang])
 	  userLang = 'en';
   //console.log(userLang);
@@ -27,15 +30,11 @@ function init() {
 }
 function selectLang(lang) {
     document.cookie="lang=" + lang;
-    setLang(lang)
+    setLang(lang);
+    userLang = lang;
 }
 function setLang(lang) {
-    while (a= style.firstChild) {
-        style.removeChild(a);
-    }
-    style.appendChild( doc.createTextNode('*[lang="' +lang + '"] { display:block }\n\
-        span[lang="' +lang + '"] { display:inline }' ));
-    head.appendChild( style);
+    setVisible(lang);
     var imgs = doc.getElementsByTagName('img');
     for (var i = 0; i<imgs.length;i++) {
 	if (imgs[i].id.substr(0,4) == "step") {
@@ -43,6 +42,18 @@ function setLang(lang) {
 	  imgs[i].src = lang + "/" + imgs[i].id + ".png";
     }
   }
+}
+function setVisible(lang) {
+    if (inAppP) {
+	    while (a= style.firstChild) {
+	        style.removeChild(a);
+	    }
+	    style.appendChild( doc.createTextNode('*[lang="' +lang + '"] { display:block }\n\
+	        span[lang="' +lang + '"] { display:inline }' ));
+    } else {
+        $('*[lang]').hide();
+        $('*[lang="'+lang+'"]').show();
+    }        
 }
 function readCookie(name) {
     var nameEQ = name + "=";
