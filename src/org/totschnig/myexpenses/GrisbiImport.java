f5236904dd30a99690a39fd8dcb7852bc725dbce
Michael Totschnig <michael@totschnig.org>
2012-03-21 16:48:01 +0100

    move import into its own activity

---
+++ src/org/totschnig/myexpenses/GrisbiImport.java
@@ -0,0 +1,421 @@
+package org.totschnig.myexpenses;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXParseException;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class GrisbiImport extends Activity {
+  ProgressDialog mProgressDialog;
+  String sourceStr;
+  private MyAsyncTask task=null;
+  
+  /**
+   * Choice of sources for importing categories presented to the user.
+   * The first four are internal to the app, the fourth one is provided by the user 
+   */
+  private final String[] IMPORT_SOURCES = {
+      "Grisbi default (en)", 
+      "Grisbi default (fr)", 
+      "Grisbi default (de)", 
+      "Grisbi default (it)", 
+      "/sdcard/myexpenses/categories.xml"
+  };
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mProgressDialog = new ProgressDialog(this);
+    mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+    mProgressDialog.setProgress(0);
+    
+    task=(MyAsyncTask)getLastNonConfigurationInstance();
+    
+    if (task!=null) {
+      task.attach(this);
+      sourceStr = IMPORT_SOURCES[task.getSource()];
+      mProgressDialog.setTitle(getString(R.string.categories_loading,sourceStr));
+      mProgressDialog.setMax(task.getTotalCategories());
+      mProgressDialog.show();
+      updateProgress(task.getProgress());
+      
+      if (task.getStatus() == AsyncTask.Status.FINISHED) {
+        markAsDone();
+      }
+    } else {
+      importCategories();
+    }
+  }
+  @Override
+  public void onStop() {
+    super.onStop();
+    mProgressDialog.dismiss();
+  }
+  void updateProgress(int progress) {
+    mProgressDialog.setProgress(progress);
+  }
+  /**
+   * Presents dialog for selecting an import source
+   * and executes the import as an asyncTask
+   */
+  private void importCategories() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle(R.string.dialog_title_select_import_source);
+    builder.setSingleChoiceItems(IMPORT_SOURCES, -1, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int item) {
+          sourceStr = IMPORT_SOURCES[item];
+          mProgressDialog.setTitle(getString(R.string.categories_loading,sourceStr));
+          mProgressDialog.show();
+          task = new MyAsyncTask(GrisbiImport.this,item);
+          task.execute();
+          dialog.cancel();
+        }
+    });
+    builder.show();
+  }
+  void markAsDone() {
+    mProgressDialog.dismiss();
+    String msg;
+    int result = task.getTotalImported();
+    if (result != -1) {
+      msg = getString(R.string.import_categories_success,result);
+      Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+    task = null;
+  }
+  
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    if (task != null)
+      task.detach();
+    return(task);
+  }
+
+  /**
+   * This AsyncTaks has no input upon execution 
+   * report an integer for showing a progress update
+   * and gives no result
+   * @author Michael Totschnig
+   *
+   */
+  static class MyAsyncTask extends AsyncTask<Void, Integer, Void> {
+    private GrisbiImport activity;
+    private int source;
+    NodeList categories;
+    NodeList sub_categories;
+    InputStream catXML;
+    Document dom;
+    Element root;
+    Hashtable<String,Long> Foreign2LocalIdMap;
+    private int totalCategories;
+    int totalImported;
+    private ExpensesDbAdapter mDbHelper;
+    int progress=0;
+    String grisbiFileVersion;
+    String mainElementName;
+    String subElementName;
+
+    /**
+     * @param context
+     * @param source Source for the import
+     */
+    public MyAsyncTask(GrisbiImport activity,int source) {
+      attach(activity);
+      this.source = source;
+      Foreign2LocalIdMap = new Hashtable<String,Long>();
+      totalImported = -1;
+      mDbHelper = MyApplication.db();
+    }
+    public Integer getSource() {
+      return source;
+    }
+    public Integer getTotalImported() {
+      return totalImported;
+    }
+    void attach(GrisbiImport activity2) {
+      this.activity=activity2;
+    }
+    void detach() {
+      activity=null;
+    }
+    int getProgress() {
+      return(progress);
+    }
+    /* (non-Javadoc)
+     * loads the XML from the source, parses it, and sets up progress dialog
+     * @see android.os.AsyncTask#onPreExecute()
+     */
+    protected void onPreExecute() {
+      super.onPreExecute();
+      //the last entry in the array is the custom import from sdcard
+      if (source == activity.IMPORT_SOURCES.length -1) {
+        try {
+          catXML = new FileInputStream(activity.sourceStr);
+        } catch (FileNotFoundException e) {
+          Toast.makeText(activity, "Could not find file "+activity.sourceStr, Toast.LENGTH_LONG).show();
+          cancel(false);
+          return;
+        }
+      } else {
+        int sourceRes = 0;
+        switch(source) {
+        case 0:
+          sourceRes = R.raw.cat_en;
+          break;
+        case 1:
+          sourceRes = R.raw.cat_fr;
+          break;
+        case 2:
+          sourceRes = R.raw.cat_de;
+          break;
+        case 3:
+          sourceRes = R.raw.cat_it;
+          break;
+        }
+        catXML = activity.getResources().openRawResource(sourceRes);
+      }
+      try {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        dom = builder.parse(catXML);
+      } catch (SAXParseException e) {
+        Log.w("MyExpenses",e.getMessage());
+        Toast.makeText(activity, "Could not parse file "+activity.sourceStr, Toast.LENGTH_LONG).show();
+        cancel(false);
+        return;
+      } catch (Exception e) {
+        Toast.makeText(activity, "An error occured: "+e.getMessage(), Toast.LENGTH_LONG).show();
+        cancel(false);
+        return;
+      }
+      //first we do the parsing
+      root = dom.getDocumentElement();
+      Node generalNode = root.getElementsByTagName("General").item(0);
+      if (generalNode != null) {
+        grisbiFileVersion = generalNode.getAttributes().getNamedItem("File_version").getNodeValue();
+        Log.i("MyExpenses","found Grisbi version" + grisbiFileVersion);
+      }
+      else {
+        Node versionNode = root.getElementsByTagName("Version_fichier_categ").item(0);
+        if (versionNode != null) {
+          //unfortunately we have to retrieve the first text node in order to get at the value
+          grisbiFileVersion = versionNode.getChildNodes().item(0).getNodeValue();
+          Log.i("MyExpenses","found Grisbi version" + grisbiFileVersion);
+        }
+        else {
+          Log.i("MyExpenses","did not find Grisbi version, assuming 0.6.0");
+          //we are mapping the existence of Categorie to 0.5.0
+          //and Category to 0.6.0
+          if (root.getElementsByTagName("Category").getLength() > 0)
+            grisbiFileVersion = "0.6.0";
+          else if (root.getElementsByTagName("Categorie").getLength() > 0)
+            grisbiFileVersion = "0.5.0";
+          else {
+            Toast.makeText(activity, "Unable to determine Grisbi file version", Toast.LENGTH_LONG).show();
+            cancel(false);
+          }
+            
+        }
+      }
+      if (grisbiFileVersion.equals("0.6.0")) {
+        mainElementName = "Category";
+        subElementName = "Sub_category";
+      } else if (grisbiFileVersion.equals("0.5.0")) {
+        mainElementName = "Categorie";
+        subElementName = "Sous-categorie";
+      } else {
+        Toast.makeText(activity, "Unsupported Grisbi File Version: "+grisbiFileVersion, Toast.LENGTH_LONG).show();
+        cancel(false);
+      }
+    }
+    protected void onCancelled() {
+      if (activity==null) {
+        Log.w("MyAsyncTask", "onCancelled() skipped -- no activity");
+      } else {
+        activity.markAsDone();
+      }
+    }
+    /* (non-Javadoc)
+     * updates the progress dialog
+     * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+     */
+    protected void onProgressUpdate(Integer... values) {
+      progress = values[0];
+      if (activity==null) {
+        Log.w("MyAsyncTask", "onProgressUpdate() skipped -- no activity");
+      }
+      else {
+        activity.updateProgress(progress);
+      }
+    }
+    /* (non-Javadoc)
+     * reports on success (with total number of imported categories) or failure
+     * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+     */
+    protected void onPostExecute(Void unused) {
+      if (activity==null) {
+        Log.w("MyAsyncTask", "onPostExecute() skipped -- no activity");
+      }
+      else {
+        activity.markAsDone();
+      }
+    }
+
+
+    /* (non-Javadoc)
+     * this is where the bulk of the work is done via calls to {@link #importCatsMain()}
+     * and {@link #importCatsSub()}
+     * sets up {@link #categories} and {@link #sub_categories}
+     * @see android.os.AsyncTask#doInBackground(Params[])
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+      totalImported = 0;
+      categories = root.getElementsByTagName(mainElementName);
+      sub_categories = root.getElementsByTagName(subElementName);
+      totalCategories = categories.getLength() + sub_categories.getLength();
+      activity.mProgressDialog.setMax(totalCategories);
+
+      if (grisbiFileVersion.equals("0.6.0")) {
+        importCatsMain();
+        importCatsSub();
+      } else {
+        importCats050();
+      }
+      return(null);
+    }
+    private void importCats050() {
+      int count = 0;
+      String label;
+      long main_id, sub_id;
+      NamedNodeMap atts;
+      
+      for (int i=0;i<categories.getLength();i++){
+        count++;
+        atts = categories.item(i).getAttributes();
+        label = atts.getNamedItem("Nom").getNodeValue();
+        main_id = mDbHelper.createCategory(label,0);
+        if (main_id != -1) {
+          totalImported++;
+        } else {
+          //this should not happen
+          Log.w("MyExpenses","could neither retrieve nor store main category " + label);
+          continue;
+        }
+        if (count % 10 == 0) {
+          publishProgress(count);
+        }
+        NodeList children = categories.item(i).getChildNodes();
+        for (int j=0;j<children.getLength();j++){
+          Node child = children.item(j);
+          String nodeName = child.getNodeName();
+          if ((nodeName != null) & (nodeName.equals(subElementName))) {
+            count++;
+            atts = child.getAttributes();
+            label = atts.getNamedItem("Nom").getNodeValue();
+            sub_id = mDbHelper.createCategory(label,main_id);
+            if (sub_id != -1) {
+              totalImported++;
+            } else {
+              //this should not happen
+              Log.w("MyExpenses","could neither retrieve nor store main category " + label);
+            }
+            if (count % 10 == 0) {
+              publishProgress(count);
+            }
+          }
+        }
+      }
+    }
+    /**
+     * iterates over {@link #categories}
+     * maintains a map of the ids found in the XML and the ones inserted in the database
+     * needed for mapping subcats to their parents in {@link #importCatsSub()}
+     */
+    private void importCatsMain() {
+      int start = 1;
+      String label;
+      String id;
+      long _id;
+
+      for (int i=0;i<categories.getLength();i++){
+        NamedNodeMap category = categories.item(i).getAttributes();
+        label = category.getNamedItem("Na").getNodeValue();
+        id =  category.getNamedItem("Nb").getNodeValue();
+        _id = mDbHelper.getCategoryId(label, 0);
+        if (_id != -1) {
+          Foreign2LocalIdMap.put(id, _id);
+        } else {
+          _id = mDbHelper.createCategory(label,0);
+          if (_id != -1) {
+            Foreign2LocalIdMap.put(id, _id);
+            totalImported++;
+          } else {
+            //this should not happen
+            Log.w("MyExpenses","could neither retrieve nor store main category " + label);
+          }
+        }
+        if ((start+i) % 10 == 0) {
+          publishProgress(start+i);
+        }
+      }
+    }
+    /**
+     * iterates over {@link #sub_categories}
+     */
+    private void importCatsSub() {
+      int start = categories.getLength() + 1;
+      String label;
+      //String id;
+      String parent_id;
+      Long mapped_parent_id;
+      long _id;
+      for (int i=0;i<sub_categories.getLength();i++){
+        NamedNodeMap sub_category = sub_categories.item(i).getAttributes();
+        label = sub_category.getNamedItem("Na").getNodeValue();
+        //id =  sub_category.getNamedItem("Nb").getNodeValue();
+        parent_id = sub_category.getNamedItem("Nbc").getNodeValue();
+        mapped_parent_id = Foreign2LocalIdMap.get(parent_id);
+        //TODO: for the moment, we do not deal with subcategories,
+        //if we were not able to import a matching category
+        //should check if the main category exists, but the subcategory is new
+        if (mapped_parent_id != null) {
+          _id = mDbHelper.createCategory(label, mapped_parent_id);
+          if (_id != -1) {
+            totalImported++;
+          }
+        } else {
+          Log.w("MyExpenses","could not store sub category " + label);
+        }
+        if ((start+i) % 10 == 0) {
+          publishProgress(start+i);
+        }
+      }
+    }
+    int getTotalCategories() {
+      return totalCategories;
+    }
+    void setTotalCategories(int totalCategories) {
+      this.totalCategories = totalCategories;
+    }
+  }
+}
