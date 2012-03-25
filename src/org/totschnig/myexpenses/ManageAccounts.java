f694aa424230c2bf9aa3ff8d220dd2a4063df5d2
Michael Totschnig <michael@totschnig.org>
2012-03-26 00:53:55 +0200

    renamed

--- src/org/totschnig/myexpenses/ManageAccounts.java
+++ src/org/totschnig/myexpenses/ManageAccounts.java
@@ -60,7 +60,7 @@ public class ManageAccounts extends ListActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.select_account);
+    setContentView(R.layout.manage_accounts);
     setTitle(R.string.pref_manage_accounts_title);
     // Set up our adapter
     mDbHelper = MyApplication.db();
