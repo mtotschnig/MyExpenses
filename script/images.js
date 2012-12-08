331bb612deec47e7a5144decd1fafe11bb23cedf
Michael Totschnig <michael@totschnig.org>
2012-12-08 22:45:26 +0100

    go live with transition to jekyll

---
+++ script/images.js
@@ -0,0 +1,33 @@
+var imageDialog;
+
+$(document).ready(function() {
+    imageDialog =$("<div id='dialog'><img id='image' src=''/></div>").dialog({
+      modal: true,
+      resizable: false,
+      draggable: false,
+      width: "450px",
+      autoOpen: false
+    });
+       
+    $('.screenshot img').click(function(event){
+      event.preventDefault();
+      PreviewImage($(this).attr('src'));                                 
+    });
+});
+
+PreviewImage = function(uri) {
+  uri = "large/" +uri;
+  //Get the HTML Elements
+  imageTag = $('#image');
+ 
+  //Split the URI so we can get the file name
+  uriParts = uri.split("/");
+
+  //When the image has loaded, display the dialog
+  imageTag.load(function(){
+    imageDialog.dialog("open");
+  });
+
+  //Set the image src
+  imageTag.attr('src', uri);
+}
