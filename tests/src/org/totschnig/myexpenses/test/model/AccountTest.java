6fce5da634606f1ca8494a7b016dda7b216bc2cd
Michael Totschnig <michael@totschnig.org>
2013-06-13 17:45:39 +0200

    test for aggregates

--- tests/src/org/totschnig/myexpenses/test/model/AccountTest.java
+++ tests/src/org/totschnig/myexpenses/test/model/AccountTest.java
@@ -20,10 +20,13 @@ import org.totschnig.myexpenses.model.Account;
 import org.totschnig.myexpenses.model.DataObjectNotFoundException;
 import org.totschnig.myexpenses.model.Money;
 import org.totschnig.myexpenses.model.Transaction;
+import org.totschnig.myexpenses.provider.TransactionProvider;
+
+import android.database.Cursor;
 
 public class AccountTest extends ModelTest  {
   
-  public void test_Account() {
+  public void testAccount() {
     Account account,restored = null;
     Long openingBalance = (long) 100;
     account = new Account("TestAccount",openingBalance,"Testing with Junit");
@@ -56,5 +59,67 @@ public class AccountTest extends ModelTest  {
     } catch (DataObjectNotFoundException e) {
       //succeed
     }
+  }
+  /**
+   * we test if the db calculates the aggregate sums correctly
+   * this is rather a test of the cursor exposed through the content provider
+   * but set up is easier through models
+   */
+  public void testAggregates() {
+    Account account1, account2;
+    Transaction op;
+    Long openingBalance = 100L,
+        expense1 = 10L,
+        expense2 = 20L,
+        income1 = 30L,
+        income2 = 40L,
+        transferP = 50L,
+        transferN = 60L;
+
+    account1 = new Account("Account 1",openingBalance,"Account 1");
+    account1.save();
+    account2 = new Account("Account 2",openingBalance,"Account 2");
+    account2.save();
+    op = Transaction.getTypedNewInstance(MyExpenses.TYPE_TRANSACTION,account1.id);
+    op.amount = new Money(account1.currency,-expense1);
+    op.save();
+    op.amount = new Money(account1.currency,-expense2);
+    op.saveAsNew();
+    op.amount = new Money(account1.currency,income1);
+    op.saveAsNew();
+    op.amount = new Money(account1.currency,income2);
+    op.saveAsNew();
+    op = Transaction.getTypedNewInstance(MyExpenses.TYPE_TRANSFER,account1.id);
+    op.transfer_account = account2.id;
+    op.amount = new Money(account1.currency,transferP);
+    op.save();
+    op.amount = new Money(account1.currency,-transferN);
+    op.saveAsNew();
+    Cursor cursor = getMockContentResolver().query(
+        TransactionProvider.ACCOUNTS_URI,  // the URI for the main data table
+        null,            // get all the columns
+        null,                       // no selection columns, get all the records
+        null,                       // no selection criteria
+        null                        // use default the sort order
+    );
+
+    assertEquals(2, cursor.getCount());
+
+    assertTrue(cursor.moveToFirst());
+
+    // Since no projection was used, get the column indexes of the returned columns
+    int incomeIndex = cursor.getColumnIndex("sum_income");
+    int expensesIndex = cursor.getColumnIndex("sum_expenses");
+    int transferIndex = cursor.getColumnIndex("sum_transfer");
+    int balanceIndex = cursor.getColumnIndex("current_balance");
+    assertEquals(income1+income2, cursor.getLong(incomeIndex));
+    assertEquals(expense1+expense2, cursor.getLong(expensesIndex));
+    assertEquals(transferP-transferN, cursor.getLong(transferIndex));
+    assertEquals(openingBalance+income1+income2-expense1-expense2+transferP-transferN, cursor.getLong(balanceIndex));
+    assertTrue(cursor.moveToNext());
+    assertEquals(0L, cursor.getLong(incomeIndex));
+    assertEquals(0L, cursor.getLong(expensesIndex));
+    assertEquals(transferN-transferP, cursor.getLong(transferIndex));
+    assertEquals(openingBalance+transferN-transferP, cursor.getLong(balanceIndex));
   }
 }
