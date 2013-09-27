5848c8631475cb3b632a9eb8bc21bcd99dbc7a49
Michael Totschnig <michael@totschnig.org>
2013-09-27 11:33:29 +0200

    catch if resource is not found and display simple error message

--- src/org/totschnig/myexpenses/dialog/HelpDialogFragment.java
+++ src/org/totschnig/myexpenses/dialog/HelpDialogFragment.java
@@ -24,6 +24,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -51,48 +52,57 @@ public class HelpDialogFragment extends DialogFragment {
     Context wrappedCtx = DialogUtils.wrapContext2(ctx);
     final Resources res = getResources();
     final String pack = ctx.getPackageName();
+    String title;
     Bundle args = getArguments();
     String activityName = args.getString("activityName");
     String variant = args.getString("variant");
     final LayoutInflater li = LayoutInflater.from(wrappedCtx);
     View view = li.inflate(R.layout.help_dialog, null);
     LinearLayout ll = (LinearLayout) view.findViewById(R.id.help);
-    String screenInfo = getString(res.getIdentifier("help_" +activityName + "_info", "string", pack));
-    if (variant != null)
-      screenInfo += "\n" + getString(res.getIdentifier("help_" +activityName + "_" + variant + "_info", "string", pack));
-    ((TextView) view.findViewById(R.id.screen_info)).setText(screenInfo);
-    int resId = res.getIdentifier(activityName+"_menuitems", "array", pack);
-    if (resId != 0) {
-      String[] itemsAll;
-      final String[] items = res.getStringArray(resId);
-      if (variant != null &&
-          (resId = res.getIdentifier(activityName + "_" + variant +"_menuitems", "array", pack)) != 0) {
-        String[] itemsVariant = res.getStringArray(resId);
-        itemsAll = (String[])Array.newInstance(String[].class.getComponentType(), items.length + itemsVariant.length);
-        System.arraycopy(items, 0, itemsAll, 0, items.length);
-        //easier from API 9
-        //itemsAll = Arrays.copyOf(items, items.length + itemsVariant.length);
-        System.arraycopy(itemsVariant, 0, itemsAll, items.length, itemsVariant.length);
-      } else
-        itemsAll = items;
-      for (String item: itemsAll) {
-        View row = li.inflate(R.layout.help_dialog_action_row, null);
-        ((ImageView) row.findViewById(R.id.list_image)).setImageDrawable(
-            res.getDrawable(res.getIdentifier(item+"_icon", "drawable", pack)));
-        ((TextView) row.findViewById(R.id.title)).setText(
-            res.getString(res.getIdentifier("menu_"+item,"string",pack)));
-        ((TextView) row.findViewById(R.id.help_text)).setText(
-            res.getString(res.getIdentifier("menu_"+item+"_help_text","string",pack)));
-        ll.addView(row);
+    try {
+      String screenInfo = getString(res.getIdentifier("help_" +activityName + "_info", "string", pack));
+      if (variant != null)
+        screenInfo += "\n" + getString(res.getIdentifier("help_" +activityName + "_" + variant + "_info", "string", pack));
+      ((TextView) view.findViewById(R.id.screen_info)).setText(screenInfo);
+      int resId = res.getIdentifier(activityName+"_menuitems", "array", pack);
+      if (resId != 0) {
+        String[] itemsAll;
+        final String[] items = res.getStringArray(resId);
+        if (variant != null &&
+            (resId = res.getIdentifier(activityName + "_" + variant +"_menuitems", "array", pack)) != 0) {
+          String[] itemsVariant = res.getStringArray(resId);
+          itemsAll = (String[])Array.newInstance(String[].class.getComponentType(), items.length + itemsVariant.length);
+          System.arraycopy(items, 0, itemsAll, 0, items.length);
+          //easier from API 9
+          //itemsAll = Arrays.copyOf(items, items.length + itemsVariant.length);
+          System.arraycopy(itemsVariant, 0, itemsAll, items.length, itemsVariant.length);
+        } else
+          itemsAll = items;
+        for (String item: itemsAll) {
+          View row = li.inflate(R.layout.help_dialog_action_row, null);
+          ((ImageView) row.findViewById(R.id.list_image)).setImageDrawable(
+              res.getDrawable(res.getIdentifier(item+"_icon", "drawable", pack)));
+          ((TextView) row.findViewById(R.id.title)).setText(
+              res.getString(res.getIdentifier("menu_"+item,"string",pack)));
+          ((TextView) row.findViewById(R.id.help_text)).setText(
+              res.getString(res.getIdentifier("menu_"+item+"_help_text","string",pack)));
+          ll.addView(row);
+        }
+      } else {
+        view.findViewById(R.id.menu_commands_heading).setVisibility(View.GONE);
       }
-    } else {
-      view.findViewById(R.id.menu_commands_heading).setVisibility(View.GONE);
+      String titleIdentifier = "help_" +activityName
+          + (variant != null ? "_" + variant : "")
+          + "_title";
+      title = getString(res.getIdentifier(titleIdentifier, "string", pack));
+    } catch (NotFoundException e) {
+      return new AlertDialog.Builder(wrappedCtx)
+          .setMessage("Error generating Help dialog")
+          .setIcon(android.R.drawable.ic_dialog_alert)
+          .create();
     }
-    String titleIdentifier = "help_" +activityName
-        + (variant != null ? "_" + variant : "")
-        + "_title";
     return new AlertDialog.Builder(wrappedCtx)
-      .setTitle(getString(res.getIdentifier(titleIdentifier, "string", pack)))
+      .setTitle(title)
       .setIcon(android.R.drawable.ic_menu_help)
       .setView(view)
       .setPositiveButton(android.R.string.ok,new DialogInterface.OnClickListener() {
