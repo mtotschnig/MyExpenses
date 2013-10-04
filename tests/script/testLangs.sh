e2f43a8743bc6b1de9a67eb7d069fe68db1f7539
Michael Totschnig <michael@totschnig.org>
2013-10-04 10:27:28 +0200

    updated to new package name

--- tests/script/testLangs.sh
+++ tests/script/testLangs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Prerequisite: execute "ant install" from tests directory
 #TODO: create dirs if needed, for the moment need to be created by hand:
 #for i in en fr de it es tr vi; do mkdir $i; done
 if [ $# -ne 1 ]
@@ -8,7 +9,7 @@ then
 fi
 lang=$1
 adb shell pm clear org.totschnig.myexpenses
-adb shell am instrument -w -e class "org.totschnig.myexpenses.test.activity.TestMain#testLang_${lang}" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
+adb shell am instrument -w -e class "org.totschnig.myexpenses.test.screenshots.TestMain#testLang_${lang}" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 case $lang in
  en) country=US ;;
  ar) country=SA ;;
