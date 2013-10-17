7527f14ede088308b184e268e4e4c5966c9901b6
Michael Totschnig <michael@totschnig.org>
2013-10-17 12:47:46 +0200

    test ManageAccounts

---
+++ tests/src/org/totschnig/myexpenses/test/activity/manageaccounts/A_InstallTest.java
@@ -0,0 +1,55 @@
+package org.totschnig.myexpenses.test.activity.manageaccounts;
+
+import java.util.Currency;
+import java.util.Locale;
+
+import org.totschnig.myexpenses.R;
+import org.totschnig.myexpenses.activity.ManageAccounts;
+import org.totschnig.myexpenses.fragment.AccountList;
+import org.totschnig.myexpenses.test.activity.MyActivityTest;
+import org.totschnig.myexpenses.test.util.Fixture;
+
+import android.database.DataSetObserver;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.widget.ListView;
+
+
+/**
+ * This class runs first and tests if fragment, list and adapter are set up
+ * 
+ * @author Michael Totschnig
+ */
+public class A_InstallTest extends MyActivityTest<ManageAccounts> {
+
+  ViewPager mPager;
+  FragmentPagerAdapter mAdapter;
+  
+  public A_InstallTest() {
+    super(ManageAccounts.class,true);
+  }
+  public void setUp() throws Exception {
+    super.setUp();
+    Fixture.setup(mInstrumentation, new Locale("en","US"), Currency.getInstance("USD"),1);
+  }
+  public void testFragmentIsSetup() {
+    FragmentActivity activity = (FragmentActivity) mActivity;
+    FragmentManager manager = activity.getSupportFragmentManager();
+    assertNotNull(manager);
+    AccountList fragment = (AccountList) manager.findFragmentById(R.id.account_list);
+    assertNotNull(fragment);
+    final ListView list = (ListView) activity.findViewById(R.id.list);
+    assertNotNull(list);
+    list.getAdapter().registerDataSetObserver(new DataSetObserver() {
+      @Override
+      public void onChanged() {
+        Log.i("DEBUG",""+list.getCount());
+        assertTrue(list.getCount() > 0);
+      }
+    });
+    mInstrumentation.waitForIdleSync();
+  }
+}
---
+++ tests/src/org/totschnig/myexpenses/test/activity/manageaccounts/B_MenuTest.java
@@ -0,0 +1,50 @@
+package org.totschnig.myexpenses.test.activity.manageaccounts;
+
+import org.totschnig.myexpenses.activity.AccountEdit;
+import org.totschnig.myexpenses.activity.ManageAccounts;
+import org.totschnig.myexpenses.activity.MyPreferenceActivity;
+import org.totschnig.myexpenses.test.activity.MyActivityTest;
+import org.totschnig.myexpenses.R;
+
+import android.view.KeyEvent;
+
+
+/**
+ * @author Michael Totschnig
+ * It is run on a Nexus S in portrait mode, and on a Android x86 VM, in landscape mode
+ * It operates on the assumption that on pre-ICS device, keys invoked with invokeMenuActionSync
+ * are not part of the visible action bar, but in the menu
+ */
+public class B_MenuTest extends MyActivityTest<ManageAccounts> {
+  
+  public B_MenuTest() {
+    super(ManageAccounts.class);
+  }
+  public void testInsertAccount() {
+    clickOnActionBarItem(R.id.CREATE_COMMAND);
+    assertTrue(mSolo.waitForActivity(AccountEdit.class.getSimpleName()));
+  }
+  public void testHelp() {
+    assertTrue(mInstrumentation.invokeMenuActionSync(mActivity, R.id.HELP_COMMAND, 0));
+    assertTrue("Help Dialog not shown", mSolo.searchText(mContext.getString(R.string.help_ManageAccounts_title)));
+    assertTrue("Close button not shown",mSolo.searchButton(mContext.getString(android.R.string.ok), true));
+    
+  }
+  public void testSettings() {
+    assertTrue(mInstrumentation.invokeMenuActionSync(mActivity, R.id.SETTINGS_COMMAND, 0));
+    assertTrue(mSolo.waitForActivity(MyPreferenceActivity.class.getSimpleName()));
+  }
+  public void testInactiveItems() {
+    //only when we send this key event, onPrepareOptionsMenu is called before the test
+    mInstrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+    for (String command : new String[] {
+        "AGGREGATES",
+        "RESET_ACCOUNT_ALL",
+    }) {
+      int resourceId = mContext.getResources().getIdentifier(command+"_COMMAND", "id", mContext.getPackageName());
+      assertFalse(
+          "Found " + command + " command that should be inactive",
+          actionBarItemVisible(resourceId));
+    }
+  }
+}
