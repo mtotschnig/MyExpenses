eee3ae20821e835d5db4d2ed681b455ed8a48e29
Michael Totschnig <michael@totschnig.org>
2013-11-26 16:12:19 +0100

    got lost in merge conflict resolution

--- src/org/totschnig/myexpenses/activity/LaunchActivity.java
+++ src/org/totschnig/myexpenses/activity/LaunchActivity.java
@@ -3,6 +3,7 @@ package org.totschnig.myexpenses.activity;
 import static org.totschnig.myexpenses.provider.DatabaseConstants.KEY_ACCOUNTID;
 
 import org.totschnig.myexpenses.MyApplication;
+import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.dialog.VersionDialogFragment;
 import org.totschnig.myexpenses.dialog.MessageDialogFragment;
 import org.totschnig.myexpenses.model.Transaction;
