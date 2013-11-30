9434151829be9e69e3d8b6a0c3a26308234d5425
Michael Totschnig <michael@totschnig.org>
2013-11-30 15:52:57 +0100

    added doc

--- tests/script/runActivityTests.sh
+++ tests/script/runActivityTests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#before running this script, ant debug install should be executed from directory tests
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.myexpenses" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.manageaccounts" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.expenseedit" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
