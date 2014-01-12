3586284eade0c3cad2a326be58c8445e57ab2c9f
Michael Totschnig <michael@totschnig.org>
2014-01-12 12:13:40 +0100

    make planexecutor intervall dependent on debug setting, and check in test

--- tests/src/org/totschnig/myexpenses/test/misc/SafeGuardTests.java
+++ tests/src/org/totschnig/myexpenses/test/misc/SafeGuardTests.java
@@ -1,6 +1,7 @@
 package org.totschnig.myexpenses.test.misc;
 
 import org.totschnig.myexpenses.MyApplication;
+import org.totschnig.myexpenses.service.PlanExecutor;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -8,5 +9,6 @@ import junit.framework.TestCase;
 public class SafeGuardTests extends TestCase {
   public void testDebugIsFalse() {
     Assert.assertFalse(MyApplication.debug);
+    Assert.assertEquals(21600000, PlanExecutor.INTERVAL);
   }
 }
