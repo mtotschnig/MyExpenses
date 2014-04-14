9b0530bb8115813293997d1e65fdbb393b468dc3
Michael Totschnig <michael@totschnig.org>
2014-04-14 19:00:28 +0200

    inform about new app dir location

--- src/org/totschnig/myexpenses/activity/LaunchActivity.java
+++ src/org/totschnig/myexpenses/activity/LaunchActivity.java
@@ -55,7 +55,7 @@ public abstract class LaunchActivity extends ProtectedFragmentActivity {
         //if they are already above both tresholds, so we set some delay
         mSettings.edit().putLong("nextReminderContrib",Transaction.getSequenceCount()+23).commit();
       }
-      if (prev_version < 117) {
+      if (prev_version < 132) {
         MyApplication.getInstance().showImportantUpgradeInfo = true;
       }
       VersionDialogFragment.newInstance(prev_version)
