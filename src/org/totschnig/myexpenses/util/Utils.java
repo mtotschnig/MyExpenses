e57e72a9ece3fa62e2b23a4107a992fe78ee4066
Michael Totschnig <michael@totschnig.org>
2014-04-26 18:20:22 +0200

    protect against invalid data

--- src/org/totschnig/myexpenses/util/Utils.java
+++ src/org/totschnig/myexpenses/util/Utils.java
@@ -224,7 +224,13 @@ public class Utils {
    * @return formated string
    */
   public static String convAmount(String text, Currency currency) {
-    return convAmount(Long.valueOf(text), currency);
+    Long amount;
+    try {
+      amount = Long.valueOf(text);
+    } catch (NumberFormatException e) {
+      amount = 0L;
+    }
+    return convAmount(amount, currency);
   }
   public static Currency getSaveInstance(String strCurrency) {
     try {
