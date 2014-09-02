17f24c62260f5d06901a1ff9f5195588bfa2f1b5
Michael Totschnig <michael@totschnig.org>
2014-09-02 23:03:07 +0200

    unused imports

--- src/org/totschnig/myexpenses/activity/MyExpenses.java
+++ src/org/totschnig/myexpenses/activity/MyExpenses.java
@@ -53,14 +53,14 @@ import org.totschnig.myexpenses.task.TaskExecutionFragment;
 import org.totschnig.myexpenses.ui.CursorFragmentPagerAdapter;
 import org.totschnig.myexpenses.ui.FragmentPagerAdapter;
 import org.totschnig.myexpenses.ui.SimpleCursorAdapter;
-import org.totschnig.myexpenses.util.Distrib;
+//import org.totschnig.myexpenses.util.Distrib;
 import org.totschnig.myexpenses.util.Result;
 import org.totschnig.myexpenses.util.Utils;
 
 //import com.batch.android.Batch;
 //import com.batch.android.BatchUnlockListener;
 //import com.batch.android.Offer;
-import com.google.android.vending.licensing.PreferenceObfuscator;
+//import com.google.android.vending.licensing.PreferenceObfuscator;
 
 import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
