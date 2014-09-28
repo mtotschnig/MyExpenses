5daa1a32f8ad186391c80681606244df9c71c5b1
Michael Totschnig <michael@totschnig.org>
2014-09-28 12:14:44 +0200

    managecurrencies test added

--- tests/script/runAllTests.sh
+++ tests/script/runAllTests.sh
@@ -5,6 +5,8 @@ adb shell pm clear org.totschnig.myexpenses
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.myexpenses" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell pm clear org.totschnig.myexpenses
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.expenseedit" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
+adb shell pm clear org.totschnig.myexpenses
+adb shell am instrument -w -e package "org.totschnig.myexpenses.test.activity.managecurrencies" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.misc" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.model" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
 adb shell am instrument -w -e package "org.totschnig.myexpenses.test.provider" org.totschnig.myexpenses.test/android.test.InstrumentationTestRunner
