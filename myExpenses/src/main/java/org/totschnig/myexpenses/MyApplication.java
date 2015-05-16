d0d9b261868cd6bed0d536ad82fa2d9d2d776a38
Michael Totschnig <michael@totschnig.org>
2015-05-16 22:53:34 +0200

    protect against both tend and duration being present

--- myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
@@ -585,12 +585,17 @@ public class MyApplication extends Application implements
    */
   public static void copyEventData(Cursor eventCursor, ContentValues eventValues) {
     eventValues.put(Events.DTSTART, DbUtils.getLongOrNull(eventCursor, 0));
-    eventValues.put(Events.DTEND, DbUtils.getLongOrNull(eventCursor, 1));
+    Long dtEnd = DbUtils.getLongOrNull(eventCursor, 1);
+    eventValues.put(Events.DTEND, dtEnd);
     eventValues.put(Events.RRULE, eventCursor.getString(2));
     eventValues.put(Events.TITLE, eventCursor.getString(3));
     eventValues.put(Events.ALL_DAY, eventCursor.getInt(4));
     eventValues.put(Events.EVENT_TIMEZONE, eventCursor.getString(5));
-    eventValues.put(Events.DURATION, eventCursor.getString(6));
+    if (dtEnd==null) {
+      //older Android versions have populated both dtend and duration
+      //restoring those on newer versions leads to IllegalArgumentexception
+      eventValues.put(Events.DURATION, eventCursor.getString(6));
+    }
     eventValues.put(Events.DESCRIPTION, eventCursor.getString(7));
     if (android.os.Build.VERSION.SDK_INT >= 16) {
       eventValues.put(Events.CUSTOM_APP_PACKAGE, eventCursor.getString(8));
