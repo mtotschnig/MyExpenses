78c229197927314583dab7c9f3b80415793c9b29
Michael Totschnig <michael@totschnig.org>
2015-06-09 17:33:26 +0200

    fixes #232

--- myExpenses/src/main/java/org/totschnig/myexpenses/fragment/DbWriteFragment.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/fragment/DbWriteFragment.java
@@ -16,7 +16,6 @@
 
 package org.totschnig.myexpenses.fragment;
 
-import org.acra.ACRA;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.model.Model;
 import org.totschnig.myexpenses.model.Transaction;
