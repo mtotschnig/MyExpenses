145d525ee924768fb2cdf468da287559128c5e9a
Michael Totschnig <michael@totschnig.org>
2015-06-21 23:38:48 +0200

    format

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/ContribInfoDialogActivity.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/ContribInfoDialogActivity.java
@@ -15,64 +15,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-  public class ContribInfoDialogActivity extends FragmentActivity
-      implements MessageDialogListener,ContribIFace {
-    protected long sequenceCount;
-    public final static String KEY_FEATURE = "feature";
-    public static final String KEY_TAG = "tag";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setTheme(MyApplication.getThemeId());
-      ContribFeature f = (ContribFeature) getIntent().getSerializableExtra(KEY_FEATURE);
+public class ContribInfoDialogActivity extends FragmentActivity
+    implements MessageDialogListener, ContribIFace {
+  protected long sequenceCount;
+  public final static String KEY_FEATURE = "feature";
+  public static final String KEY_TAG = "tag";
 
-      if (f==null) {
-        sequenceCount = getIntent().getLongExtra(ContribInfoDialogFragment.KEY_SEQUENCE_COUNT, -1);
-        ContribInfoDialogFragment.newInstance(sequenceCount)
-            .show(getSupportFragmentManager(),"CONTRIB_INFO");
-      } else {
-        ContribDialogFragment.newInstance(
-            f,getIntent().getSerializableExtra(KEY_TAG))
-            .show(getSupportFragmentManager(),"CONTRIB");
-      }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTheme(MyApplication.getThemeId());
+    ContribFeature f = (ContribFeature) getIntent().getSerializableExtra(KEY_FEATURE);
+
+    if (f == null) {
+      sequenceCount = getIntent().getLongExtra(ContribInfoDialogFragment.KEY_SEQUENCE_COUNT, -1);
+      ContribInfoDialogFragment.newInstance(sequenceCount)
+          .show(getSupportFragmentManager(), "CONTRIB_INFO");
+    } else {
+      ContribDialogFragment.newInstance(
+          f, getIntent().getSerializableExtra(KEY_TAG))
+          .show(getSupportFragmentManager(), "CONTRIB");
     }
-    @Override
-    public boolean dispatchCommand(int command, Object tag) {
-      switch (command) {
+  }
+
+  @Override
+  public boolean dispatchCommand(int command, Object tag) {
+    switch (command) {
       case R.id.REMIND_LATER_CONTRIB_COMMAND:
-        PrefKey.NEXT_REMINDER_CONTRIB.putLong(sequenceCount+MyExpenses.TRESHOLD_REMIND_CONTRIB);
+        PrefKey.NEXT_REMINDER_CONTRIB.putLong(sequenceCount + MyExpenses.TRESHOLD_REMIND_CONTRIB);
         break;
       case R.id.REMIND_NO_CONTRIB_COMMAND:
         PrefKey.NEXT_REMINDER_CONTRIB.putLong(-1);
-      }
-      finish();
-      return true;
     }
+    finish();
+    return true;
+  }
 
-    public void contribBuyDo() {
-        DonateDialogFragment.newInstance().show(getSupportFragmentManager(),"CONTRIB");
-    }
+  public void contribBuyDo() {
+    DonateDialogFragment.newInstance().show(getSupportFragmentManager(), "CONTRIB");
+  }
 
-    @Override
-    public void onMessageDialogDismissOrCancel() {
-     finish();
-    }
+  @Override
+  public void onMessageDialogDismissOrCancel() {
+    finish();
+  }
 
-    @Override
-    public void contribFeatureCalled(ContribFeature feature, Serializable tag) {
-      Intent i = new Intent();
-      i.putExtra(KEY_FEATURE, feature);
-      i.putExtra(KEY_TAG,tag);
-      setResult(RESULT_OK, i);
-      finish();
-    }
+  @Override
+  public void contribFeatureCalled(ContribFeature feature, Serializable tag) {
+    Intent i = new Intent();
+    i.putExtra(KEY_FEATURE, feature);
+    i.putExtra(KEY_TAG, tag);
+    setResult(RESULT_OK, i);
+    finish();
+  }
 
-    @Override
-    public void contribFeatureNotCalled() {
-      finish();
-    }
-    @Override
-    protected void onActivityResult(int arg0, int arg1, Intent arg2) {
-      finish();
-    }
+  @Override
+  public void contribFeatureNotCalled() {
+    finish();
+  }
+
+  @Override
+  protected void onActivityResult(int arg0, int arg1, Intent arg2) {
+    finish();
+  }
 }
