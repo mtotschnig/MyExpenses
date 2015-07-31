ea7717d936db104aa46733b1bbcdf2662ccf0251
Michael Totschnig <michael@totschnig.org>
2015-07-31 22:08:46 +0200

    move parse command to action bar

--- myExpenses/src/main/java/org/totschnig/myexpenses/fragment/CsvImportParseFragment.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/fragment/CsvImportParseFragment.java
@@ -7,9 +7,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -22,6 +24,7 @@ import org.totschnig.myexpenses.export.qif.QifDateFormat;
 import org.totschnig.myexpenses.preference.SharedPreferencesCompat;
 import org.totschnig.myexpenses.task.TaskExecutionFragment;
 
+
 /**
  * Created by privat on 30.06.15.
  */
@@ -30,8 +33,11 @@ public class CsvImportParseFragment extends Fragment implements View.OnClickList
   static final String PREFKEY_IMPORT_CSV_ENCODING = "import_csv_encoding";
   static final String PREFKEY_IMPORT_CSV_DELIMITER = "import_csv_delimiter";
   private Uri mUri;
+  public void setmUri(Uri mUri) {
+    this.mUri = mUri;
+    getActivity().supportInvalidateOptionsMenu();
+  }
   private EditText mFilename;
-  private Button mParse;
   private Spinner mDateFormatSpinner, mEncodingSpinner, mDelimiterSpinner;
 
   @Override
@@ -42,10 +48,93 @@ public class CsvImportParseFragment extends Fragment implements View.OnClickList
     mDelimiterSpinner = DialogUtils.configureDelimiter(view, getActivity(), PREFKEY_IMPORT_CSV_DELIMITER);
     mFilename = DialogUtils.configureFilename(view);
     view.findViewById(R.id.btn_browse).setOnClickListener(this);
-    mParse = (Button) view.findViewById(R.id.btn_parse);
-    mParse.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
+    return view;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == ProtectedFragmentActivity.IMPORT_FILENAME_REQUESTCODE) {
+      if (resultCode == Activity.RESULT_OK && data != null) {
+        setmUri(DialogUtils.handleFilenameRequestResult(data, mFilename, "CSV", this));
+      }
+    }
+  }
+  String getPrefKey() {
+    return "import_csv_file_uri";
+  }
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (mUri==null) {
+      String restoredUriString = MyApplication.getInstance().getSettings()
+          .getString(getPrefKey(), "");
+      if (!restoredUriString.equals("")) {
+        Uri restoredUri = Uri.parse(restoredUriString);
+        String displayName = DialogUtils.getDisplayName(restoredUri);
+        if (displayName != null) {
+          setmUri(restoredUri);
+          mFilename.setText(displayName);
+        }
+      }
+    }
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    if (mUri != null) {
+      outState.putString(getPrefKey(), mUri.toString());
+    }
+  }
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    if (savedInstanceState != null) {
+      String restoredUriString = savedInstanceState.getString(getPrefKey());
+      if (restoredUriString != null) {
+        Uri restoredUri = Uri.parse(restoredUriString);
+        String displayName = DialogUtils.getDisplayName(restoredUri);
+        if (displayName != null) {
+          setmUri(restoredUri);
+          mFilename.setText(displayName);
+        }
+      }
+    }
+  }
+
+  public static CsvImportParseFragment newInstance() {
+    return new CsvImportParseFragment();
+  }
+
+  @Override
+  public void onClick(View v) {
+    DialogUtils.openBrowse(mUri, this);
+  }
+
+  @Override
+  public boolean checkTypeParts(String[] typeParts) {
+   return DialogUtils.checkTypePartsDefault(typeParts);
+  }
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    inflater.inflate(R.menu.cvs_parse, menu);
+  }
+
+  @Override
+  public void onPrepareOptionsMenu(Menu menu) {
+    menu.findItem(R.id.PARSE_COMMAND).setEnabled(mUri!=null);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.PARSE_COMMAND:
         QifDateFormat format = (QifDateFormat) mDateFormatSpinner.getSelectedItem();
         String encoding = (String) mEncodingSpinner.getSelectedItem();
         String delimiter = getResources().getStringArray(R.array.pref_csv_import_delimiter_values)
@@ -68,74 +157,9 @@ public class CsvImportParseFragment extends Fragment implements View.OnClickList
                     null, ProgressDialog.STYLE_SPINNER, false),
                 "PROGRESS")
             .commit();
-      }
-    });
-    return view;
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == ProtectedFragmentActivity.IMPORT_FILENAME_REQUESTCODE) {
-      if (resultCode == Activity.RESULT_OK && data != null) {
-        mUri = DialogUtils.handleFilenameRequestResult(data, mFilename, "CSV", this);
-      }
+        break;
     }
-  }
-  String getPrefKey() {
-    return "import_csv_file_uri";
-  }
-  @Override
-  public void onResume() {
-    super.onResume();
-    if (mUri==null) {
-      String restoredUriString = MyApplication.getInstance().getSettings()
-          .getString(getPrefKey(), "");
-      if (!restoredUriString.equals("")) {
-        Uri restoredUri = Uri.parse(restoredUriString);
-        String displayName = DialogUtils.getDisplayName(restoredUri);
-        if (displayName != null) {
-          mUri = restoredUri;
-          mFilename.setText(displayName);
-        }
-      }
-    }
-    mParse.setEnabled(mUri!=null);
+    return super.onOptionsItemSelected(item);
   }
 
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    if (mUri != null) {
-      outState.putString(getPrefKey(), mUri.toString());
-    }
-  }
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    if (savedInstanceState != null) {
-      String restoredUriString = savedInstanceState.getString(getPrefKey());
-      if (restoredUriString != null) {
-        Uri restoredUri = Uri.parse(restoredUriString);
-        String displayName = DialogUtils.getDisplayName(restoredUri);
-        if (displayName != null) {
-          mUri = restoredUri;
-          mFilename.setText(displayName);
-        }
-      }
-    }
-  }
-
-  public static CsvImportParseFragment newInstance() {
-    return new CsvImportParseFragment();
-  }
-
-  @Override
-  public void onClick(View v) {
-    DialogUtils.openBrowse(mUri, this);
-  }
-
-  @Override
-  public boolean checkTypeParts(String[] typeParts) {
-   return DialogUtils.checkTypePartsDefault(typeParts);
-  }
 }
