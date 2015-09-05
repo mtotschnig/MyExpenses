1e23c68a66b9a678da92b40a5e8338a6f2dcc2f6
Michael Totschnig <michael@totschnig.org>
2015-09-05 18:34:23 +0200

    handle new status code sent from Play version of MyExpenses contrib

--- myExpenses/src/main/java/org/totschnig/myexpenses/service/UnlockHandler.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/service/UnlockHandler.java
@@ -1,5 +1,6 @@
 package org.totschnig.myexpenses.service;
 
+import org.totschnig.myexpenses.BuildConfig;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.activity.MyExpenses;
@@ -26,6 +27,7 @@ import com.google.android.vending.licensing.PreferenceObfuscator;
 public class UnlockHandler extends Handler {
   private static final int STATUS_TEMPORARY = 3;
   private static final int STATUS_PERMANENT = 4;
+  private static final int STATUS_FINAL = 7;
   
   @Override
   public void handleMessage(Message msg) {
@@ -34,22 +36,49 @@ public class UnlockHandler extends Handler {
       return;
     }
     Log.i(MyApplication.TAG,"Now handling answer from license verification service; got status "+msg.what);
-    if (msg.what == STATUS_PERMANENT || msg.what == STATUS_TEMPORARY) {
-      PreferenceObfuscator mPreferences = Distrib.getLicenseStatusPrefs(app);
-      app.setContribStatus(Distrib.STATUS_ENABLED_LEGACY_SECOND);
-      mPreferences.putString(MyApplication.PrefKey.LICENSE_STATUS.getKey(), String.valueOf(Distrib.STATUS_ENABLED_LEGACY_SECOND));
-      mPreferences.commit();
-      NotificationManager notificationManager =
-          (NotificationManager) app.getSystemService(Context.NOTIFICATION_SERVICE);
-      NotificationCompat.Builder builder =
-          new NotificationCompat.Builder(app)
-            .setSmallIcon(R.drawable.ic_home_dark)
-            .setContentTitle(app.getString(R.string.licence_validation_premium))
-            .setContentText(app.getString(R.string.thank_you))
-            .setContentIntent(PendingIntent.getActivity(app, 0, new Intent(app, MyExpenses.class),0));
-      Notification notification  = builder.build();
-      notification.flags = Notification.FLAG_AUTO_CANCEL;
-      notificationManager.notify(0, notification);
+    NotificationManager notificationManager =
+        (NotificationManager) app.getSystemService(Context.NOTIFICATION_SERVICE);
+    NotificationCompat.Builder builder;
+    Notification notification;
+    switch(msg.what) {
+      case STATUS_FINAL:
+        doUnlock();
+        break;
+      case STATUS_TEMPORARY:
+      case STATUS_PERMANENT:
+        if (!BuildConfig.FLAVOR_distribution.equals("play")) {
+          doUnlock();
+        } else {
+          builder =
+              new NotificationCompat.Builder(app)
+                  .setSmallIcon(R.drawable.ic_home_dark)
+                  .setContentTitle(app.getString(R.string.licence_validation_failure))
+                  .setContentText("Please upgrade My Expenses Contrib to version 1.5")
+                  .setContentIntent(PendingIntent.getActivity(app, 0, new Intent(app, MyExpenses.class), 0));
+          notification = builder.build();
+          notification.flags = Notification.FLAG_AUTO_CANCEL;
+          notificationManager.notify(0, notification);
+        }
+        break;
     }
+  }
+
+  private void doUnlock() {
+    MyApplication app = MyApplication.getInstance();
+    NotificationManager notificationManager =
+        (NotificationManager) app.getSystemService(Context.NOTIFICATION_SERVICE);
+    PreferenceObfuscator mPreferences = Distrib.getLicenseStatusPrefs(app);
+    app.setContribStatus(Distrib.STATUS_ENABLED_LEGACY_SECOND);
+    mPreferences.putString(MyApplication.PrefKey.LICENSE_STATUS.getKey(), String.valueOf(Distrib.STATUS_ENABLED_LEGACY_SECOND));
+    mPreferences.commit();
+    NotificationCompat.Builder builder =
+        new NotificationCompat.Builder(app)
+          .setSmallIcon(R.drawable.ic_home_dark)
+          .setContentTitle(app.getString(R.string.licence_validation_premium))
+          .setContentText(app.getString(R.string.thank_you))
+          .setContentIntent(PendingIntent.getActivity(app, 0, new Intent(app, MyExpenses.class), 0));
+    Notification notification  = builder.build();
+    notification.flags = Notification.FLAG_AUTO_CANCEL;
+    notificationManager.notify(0, notification);
   }
 }
