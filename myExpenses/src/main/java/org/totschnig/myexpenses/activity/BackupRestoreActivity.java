34212f295aa2b9b18d74b8e8b0d7a0a3d1031ab1
Michael Totschnig <michael@totschnig.org>
2015-09-06 10:33:54 +0200

    No need to recreate fragments if activity is restored from savedInstanceState

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/BackupRestoreActivity.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/BackupRestoreActivity.java
@@ -52,6 +52,9 @@ public class BackupRestoreActivity extends ProtectedFragmentActivityNoAppCompat
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
+    if (savedInstanceState!=null) {
+      return;
+    }
     String action = getIntent().getAction();
     if (action != null && action.equals("myexpenses.intent.backup")) {
       Result appDirStatus = Utils.checkAppDir();
