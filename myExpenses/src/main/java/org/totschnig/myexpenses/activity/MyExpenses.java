8b093a932db25ca7757550a2c37cfccb595fa4c5
Michael Totschnig <michael@totschnig.org>
2015-12-13 19:36:30 +0100

    fixes #253

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
@@ -585,7 +585,7 @@ public class MyExpenses extends LaunchActivity implements
         return true;
       case R.id.CREATE_ACCOUNT_COMMAND:
         if (mAccountCount == 0) {
-          Toast.makeText(this, "Account list not yet loaded. Please try again", Toast.LENGTH_LONG).show();
+          Toast.makeText(this, R.string.account_list_not_yet_loaded, Toast.LENGTH_LONG).show();
         }
         //we need the accounts to be loaded in order to evaluate if the limit has been reached
         else if (ContribFeature.ACCOUNTS_UNLIMITED.hasAccess() || mAccountCount < 5) {
