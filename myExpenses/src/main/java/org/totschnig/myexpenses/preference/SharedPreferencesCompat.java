d31aeb9566ea4da884d59717f182de44478222c7
Michael Totschnig <michael@totschnig.org>
2016-03-08 22:31:36 +0100

    use support library helper

--- myExpenses/src/main/java/org/totschnig/myexpenses/preference/SharedPreferencesCompat.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/preference/SharedPreferencesCompat.java
@@ -25,29 +25,8 @@ import java.lang.reflect.Method;
  * falling back to commit when apply isn't available.
  */
 public class SharedPreferencesCompat {
-    private static final Method sApplyMethod = findApplyMethod();
-
-    private static Method findApplyMethod() {
-        try {
-            Class cls = SharedPreferences.Editor.class;
-            return cls.getMethod("apply");
-        } catch (NoSuchMethodException unused) {
-            // fall through
-        }
-        return null;
-    }
 
     public static void apply(SharedPreferences.Editor editor) {
-        if (sApplyMethod != null) {
-            try {
-                sApplyMethod.invoke(editor);
-                return;
-            } catch (InvocationTargetException unused) {
-                // fall through
-            } catch (IllegalAccessException unused) {
-                // fall through
-            }
-        }
-        editor.commit();
+        android.support.v4.content.SharedPreferencesCompat.EditorCompat.getInstance().apply(editor);
     }
 }
