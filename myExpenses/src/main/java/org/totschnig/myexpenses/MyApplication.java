6b6d6e5ea19e1ff7b5ac03cb60043af00834ec6b
Michael Totschnig <michael@totschnig.org>
2016-03-25 11:37:59 +0100

    possible workaround for crashes on Gingerbread

--- myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
@@ -271,6 +271,9 @@ public class MyApplication extends Application implements
   @Override
   public void onCreate() {
     super.onCreate();
+    //Maybe prevents occasional crashes on Gingerbread
+    //https://code.google.com/p/android/issues/detail?id=81083
+    try {Class.forName("android.os.AsyncTask");} catch(Throwable ignore) {}
     mSelf = this;
     // sets up mSettings
     getSettings().registerOnSharedPreferenceChangeListener(this);
