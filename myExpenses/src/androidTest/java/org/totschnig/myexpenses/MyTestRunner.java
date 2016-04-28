d99882223bc33a5882c82c1266593bdc2dc3a2e6
Michael Totschnig <michael@totschnig.org>
2016-04-28 08:45:10 +0200

    Please ensure animations are switched of for Espresso tests

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/MyTestRunner.java
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/MyTestRunner.java
@@ -1,6 +1,7 @@
 package org.totschnig.myexpenses;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.test.runner.AndroidJUnitRunner;
 import android.util.Log;
 
@@ -12,8 +13,40 @@ public final class MyTestRunner extends AndroidJUnitRunner {
   }
 
   @Override
+  public void onStart() {
+    String[] criticalSettings = {
+        Settings.System.TRANSITION_ANIMATION_SCALE,
+        Settings.System.WINDOW_ANIMATION_SCALE,
+        Settings.System.ANIMATOR_DURATION_SCALE
+    };
+    try {
+      for (String setting: criticalSettings) {
+        if (Settings.Global.getFloat(getContext().getContentResolver(), setting) != 0) {
+          throw new AnimationsNotDisabledException(setting);
+        }
+      }
+    } catch (Settings.SettingNotFoundException e) {
+      throw new RuntimeException("Unable to determine animation settings");
+    }
+    super.onStart();
+  }
+
+  @Override
   public void finish(int resultCode, Bundle results) {
     MyApplication.cleanUpAfterTest();
     super.finish(resultCode, results);
+  }
+
+  public class AnimationsNotDisabledException extends RuntimeException {
+    String setting;
+
+    public AnimationsNotDisabledException(String setting) {
+      this.setting = setting;
+    }
+
+    @Override
+    public String getMessage() {
+      return String.format("%s  must be disabled for reliable Espresso tests", setting);
+    }
   }
 }
