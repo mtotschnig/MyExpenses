bc61b1fd5fb811475e7b13149580c9ff18ee8f34
Michael Totschnig <michael@totschnig.org>
2016-05-29 23:33:28 +0200

    Paypal does not send fragment

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/DeepLinkActivity.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/DeepLinkActivity.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.dialog.PaypalPaymentCompletedCallbackDialog;
 
-import java.util.List;
-
 public class DeepLinkActivity extends ProtectedFragmentActivity {
+
+  public static final String FRAGMENT_TAG = PaypalPaymentCompletedCallbackDialog.class.getName();
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -19,18 +21,24 @@ public class DeepLinkActivity extends ProtectedFragmentActivity {
     if (savedInstanceState == null) {
       if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
         Uri data = getIntent().getData();
-        String page = data.getFragment();
-        if ("thankyou".equals(page)) {
-          FragmentManager fm = getSupportFragmentManager();
-          if (fm.findFragmentByTag(PaypalPaymentCompletedCallbackDialog.class.getName()) == null) {
-            PaypalPaymentCompletedCallbackDialog.newInstance(data.getQueryParameter("tx"))
-                .show(fm, PaypalPaymentCompletedCallbackDialog.class.getName());
-          }
-        } else if ("verify".equals(page)) {
+        String fragment = data.getFragment();
+        if ("verify".equals(fragment)) {
           String key = data.getQueryParameter("key");
           MyApplication.PrefKey.ENTER_LICENCE.putString(key);
           CommonCommands.dispatchCommand(this, R.id.VERIFY_LICENCE_COMMAND, key);
           finish();
+        } else {
+          String tx = data.getQueryParameter("tx");
+          if (TextUtils.isEmpty(tx)) {
+            //nothing to do for us
+            CommonCommands.dispatchCommand(this, R.id.WEB_COMMAND, null);
+            finish();
+          }
+          FragmentManager fm = getSupportFragmentManager();
+          if (fm.findFragmentByTag(PaypalPaymentCompletedCallbackDialog.class.getName()) == null) {
+            PaypalPaymentCompletedCallbackDialog.newInstance(tx).show(fm,
+                FRAGMENT_TAG);
+          }
         }
       }
     }
