f5ac25a37eaa6d29710ee675cb2f410cd248ab6b
Michael Totschnig <michael@totschnig.org>
2017-01-17 20:24:15 +0000

    formatted

--- myExpenses/src/main/java/org/totschnig/myexpenses/task/RestoreTask.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/task/RestoreTask.java
@@ -44,6 +44,7 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
   private int restorePlanStrategy;
   private Uri fileUri;
   private String dirNameLegacy;
+
   RestoreTask(TaskExecutionFragment taskExecutionFragment, Bundle b) {
     this.taskExecutionFragment = taskExecutionFragment;
     this.fileUri = b.getParcelable(TaskExecutionFragment.KEY_FILE_PATH);
@@ -53,6 +54,7 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
     this.restorePlanStrategy = b.getInt(
         BackupRestoreActivity.KEY_RESTORE_PLAN_STRATEGY);
   }
+
   /*
    * (non-Javadoc) shows toast about success or failure
    * 
@@ -87,11 +89,11 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
     if (fileUri != null) {
       workingDir = Utils.getCacheDir();
       if (workingDir == null) {
-        return new Result(false,R.string.external_storage_unavailable);
+        return new Result(false, R.string.external_storage_unavailable);
       }
       try {
         InputStream is = cr.openInputStream(fileUri);
-        boolean zipResult = ZipUtils.unzip(is,workingDir);
+        boolean zipResult = ZipUtils.unzip(is, workingDir);
         try {
           is.close();
         } catch (IOException e) {
@@ -111,7 +113,7 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
             e.getMessage());
       }
     } else {
-      workingDir = new File(Utils.getAppDir().getUri().getPath(),dirNameLegacy);
+      workingDir = new File(Utils.getAppDir().getUri().getPath(), dirNameLegacy);
     }
     File backupFile = MyApplication.getBackupDbFile(workingDir);
     File backupPrefFile = MyApplication.getBackupPrefFile(workingDir);
@@ -119,7 +121,7 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
       return new Result(
           false,
           R.string.restore_backup_file_not_found,
-          MyApplication.BACKUP_DB_FILE_NAME,workingDir);
+          MyApplication.BACKUP_DB_FILE_NAME, workingDir);
     }
     if (!backupPrefFile.exists()) {
       return new Result(
@@ -136,16 +138,16 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
           null,
           SQLiteDatabase.OPEN_READONLY);
       int version = db.getVersion();
-      if (version>TransactionDatabase.DATABASE_VERSION) {
+      if (version > TransactionDatabase.DATABASE_VERSION) {
         db.close();
         return new Result(
             false,
             R.string.restore_cannot_downgrade,
-            version,TransactionDatabase.DATABASE_VERSION);
+            version, TransactionDatabase.DATABASE_VERSION);
       }
       db.close();
     } catch (SQLiteException e) {
-      return new Result(false,R.string.restore_db_not_valid);
+      return new Result(false, R.string.restore_db_not_valid);
     }
 
     //peek into preferences to see if there is a calendar configured
@@ -154,19 +156,19 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
     sharedPrefsDir.mkdir();
     if (!sharedPrefsDir.isDirectory()) {
       AcraHelper.report(
-          new Exception(String.format(Locale.US,"Could not access shared preferences directory at %s",
+          new Exception(String.format(Locale.US, "Could not access shared preferences directory at %s",
               sharedPrefsDir.getAbsolutePath())));
-      return new Result(false,R.string.restore_preferences_failure);
+      return new Result(false, R.string.restore_preferences_failure);
     }
-    File tempPrefFile = new File(sharedPrefsDir,"backup_temp.xml");
-    if (!FileCopyUtils.copy(backupPrefFile,tempPrefFile)) {
+    File tempPrefFile = new File(sharedPrefsDir, "backup_temp.xml");
+    if (!FileCopyUtils.copy(backupPrefFile, tempPrefFile)) {
       AcraHelper.report(
           new Exception("Preferences restore failed"),
           "FAILED_COPY_OPERATION",
           String.format("%s => %s",
               backupPrefFile.getAbsolutePath(),
               tempPrefFile.getAbsolutePath()));
-      return new Result(false,R.string.restore_preferences_failure);
+      return new Result(false, R.string.restore_preferences_failure);
     }
     SharedPreferences backupPref =
         application.getSharedPreferences("backup_temp", 0);
@@ -174,23 +176,23 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
       currentPlannerId = application.checkPlanner();
       currentPlannerPath = PrefKey.PLANNER_CALENDAR_PATH.getString("");
       if (currentPlannerId.equals("-1")) {
-      return new Result(
-          false,
-          R.string.restore_not_possible_local_calendar_missing);
+        return new Result(
+            false,
+            R.string.restore_not_possible_local_calendar_missing);
       }
     } else if (restorePlanStrategy == R.id.restore_calendar_handling_backup) {
-      boolean found =false;
+      boolean found = false;
       String calendarId = backupPref
-          .getString(PrefKey.PLANNER_CALENDAR_ID.getKey(),"-1");
+          .getString(PrefKey.PLANNER_CALENDAR_ID.getKey(), "-1");
       String calendarPath = backupPref
-          .getString(PrefKey.PLANNER_CALENDAR_PATH.getKey(),"");
+          .getString(PrefKey.PLANNER_CALENDAR_PATH.getKey(), "");
       if (!(calendarId.equals("-1") || calendarPath.equals(""))) {
         Cursor c = cr
             .query(
                 Calendars.CONTENT_URI,
                 new String[]{Calendars._ID},
-                MyApplication.CALENDAR_FULL_PATH_PROJECTION  + " = ?",
-                new String[] {calendarPath},
+                MyApplication.CALENDAR_FULL_PATH_PROJECTION + " = ?",
+                new String[]{calendarPath},
                 null);
         if (c != null) {
           if (c.moveToFirst()) {
@@ -206,10 +208,10 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
             calendarPath);
       }
     }
-    
+
     if (DbUtils.restore(backupFile)) {
-      publishProgress(new Result(true,R.string.restore_db_success));
-      
+      publishProgress(new Result(true, R.string.restore_db_success));
+
       //since we already started reading settings, we can not just copy the file
       //unless I found a way
       //either to close the shared preferences and read it again
@@ -230,16 +232,16 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
         key = entry.getKey();
         val = entry.getValue();
         if (val.getClass() == Long.class) {
-          edit.putLong(key,backupPref.getLong(key,0));
+          edit.putLong(key, backupPref.getLong(key, 0));
         } else if (val.getClass() == Integer.class) {
-          edit.putInt(key,backupPref.getInt(key,0));
+          edit.putInt(key, backupPref.getInt(key, 0));
         } else if (val.getClass() == String.class) {
-          edit.putString(key, backupPref.getString(key,""));
+          edit.putString(key, backupPref.getString(key, ""));
         } else if (val.getClass() == Boolean.class) {
-          edit.putBoolean(key,backupPref.getBoolean(key,false));
+          edit.putBoolean(key, backupPref.getBoolean(key, false));
         } else {
           Log.i(MyApplication.TAG,
-              "Found: "+key+ " of type "+val.getClass().getName());
+              "Found: " + key + " of type " + val.getClass().getName());
         }
       }
       if (!oldLicenceKey.equals("")) {
@@ -247,23 +249,23 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
       }
       if (restorePlanStrategy == R.id.restore_calendar_handling_configured) {
         edit.putString(PrefKey.PLANNER_CALENDAR_PATH.getKey(), currentPlannerPath);
-        edit.putString(PrefKey.PLANNER_CALENDAR_ID.getKey(),currentPlannerId);
+        edit.putString(PrefKey.PLANNER_CALENDAR_ID.getKey(), currentPlannerId);
       }
-      
+
       SharedPreferencesCompat.apply(edit);
       application.getSettings()
-        .registerOnSharedPreferenceChangeListener(application);
+          .registerOnSharedPreferenceChangeListener(application);
       tempPrefFile.delete();
       if (fileUri != null) {
         backupFile.delete();
         backupPrefFile.delete();
       }
-      publishProgress(new Result(true,R.string.restore_preferences_success));
+      publishProgress(new Result(true, R.string.restore_preferences_success));
       //if a user restores a backup we do not want past plan instances to flood the database
       PrefKey.PLANNER_LAST_EXECUTION_TIMESTAMP
           .putLong(System.currentTimeMillis());
       //now handling plans
-      if (restorePlanStrategy!=R.id.restore_calendar_handling_ignore) {
+      if (restorePlanStrategy != R.id.restore_calendar_handling_ignore) {
         publishProgress(application.restorePlanner());
       } else {
         //we remove all links to plans we did not restore
@@ -272,10 +274,10 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
         cr.update(Template.CONTENT_URI,
             planValues, null, null);
       }
-      Log.i(MyApplication.TAG,"now emptying event cache");
+      Log.i(MyApplication.TAG, "now emptying event cache");
       cr.delete(
           TransactionProvider.EVENT_CACHE_URI, null, null);
-      
+
       //now handling pictures
       //1.stale uris in the backup can be ignored1
       //delete from db
@@ -287,23 +289,23 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
       registerAsStale(true);
 
       //3. move pictures home and update uri
-      File backupPictureDir = new File(workingDir,ZipUtils.PICTURES);
+      File backupPictureDir = new File(workingDir, ZipUtils.PICTURES);
       Cursor c = cr.query(TransactionProvider.TRANSACTIONS_URI,
-          new String[]{DatabaseConstants.KEY_ROWID,DatabaseConstants.KEY_PICTURE_URI},
-          DatabaseConstants.KEY_PICTURE_URI + " IS NOT NULL",null,null);
-      if (c==null)
-        return new Result(false,R.string.restore_db_failure);
+          new String[]{DatabaseConstants.KEY_ROWID, DatabaseConstants.KEY_PICTURE_URI},
+          DatabaseConstants.KEY_PICTURE_URI + " IS NOT NULL", null, null);
+      if (c == null)
+        return new Result(false, R.string.restore_db_failure);
       if (c.moveToFirst()) {
         do {
           ContentValues uriValues = new ContentValues();
           Uri fromBackup = Uri.parse(c.getString(1));
           String fileName = fromBackup.getLastPathSegment();
-          File backupImage = new File(backupPictureDir,fileName);
+          File backupImage = new File(backupPictureDir, fileName);
           Uri restored = null;
           if (backupImage.exists()) {
-            File restoredImage = Utils.getOutputMediaFile(fileName.substring(0,fileName.lastIndexOf('.')),false);
-            if (restoredImage == null || !FileCopyUtils.copy(backupImage,restoredImage)) {
-              Log.e(MyApplication.TAG,String.format("Could not restore file %s from backup",fromBackup.toString()));
+            File restoredImage = Utils.getOutputMediaFile(fileName.substring(0, fileName.lastIndexOf('.')), false);
+            if (restoredImage == null || !FileCopyUtils.copy(backupImage, restoredImage)) {
+              Log.e(MyApplication.TAG, String.format("Could not restore file %s from backup", fromBackup.toString()));
             } else {
               restored = application.isProtected() ?
                   FileProvider.getUriForFile(application,
@@ -311,9 +313,9 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
                   Uri.fromFile(restoredImage);
             }
           } else {
-            Log.e(MyApplication.TAG,String.format("Could not restore file %s from backup",fromBackup.toString()));
+            Log.e(MyApplication.TAG, String.format("Could not restore file %s from backup", fromBackup.toString()));
           }
-          if (restored!=null) {
+          if (restored != null) {
             uriValues.put(DatabaseConstants.KEY_PICTURE_URI, restored.toString());
           } else {
             uriValues.putNull(DatabaseConstants.KEY_PICTURE_URI);
@@ -321,27 +323,27 @@ public class RestoreTask extends AsyncTask<Void, Result, Result> {
           cr.update(
               TransactionProvider.TRANSACTIONS_URI,
               uriValues,
-              DatabaseConstants.KEY_PICTURE_URI + " = "+c.getInt(0),null);
+              DatabaseConstants.KEY_PICTURE_URI + " = " + c.getInt(0), null);
         } while (c.moveToNext());
       }
       c.close();
       return new Result(true);
     } else {
-      return new Result(false,R.string.restore_db_failure);
+      return new Result(false, R.string.restore_db_failure);
     }
   }
 
   private void registerAsStale(boolean secure) {
     File dir = Utils.getPictureDir(secure);
-    if (dir==null) return;
+    if (dir == null) return;
     ContentValues values = new ContentValues();
-    for (File file: dir.listFiles()) {
+    for (File file : dir.listFiles()) {
       Uri uri = secure ? FileProvider.getUriForFile(MyApplication.getInstance(),
-          "org.totschnig.myexpenses.fileprovider",file) :
+          "org.totschnig.myexpenses.fileprovider", file) :
           Uri.fromFile(file);
-      values.put(DatabaseConstants.KEY_PICTURE_URI,uri.toString());
+      values.put(DatabaseConstants.KEY_PICTURE_URI, uri.toString());
       MyApplication.getInstance().getContentResolver().insert(
-          TransactionProvider.STALE_IMAGES_URI,values);
+          TransactionProvider.STALE_IMAGES_URI, values);
     }
   }
 }
