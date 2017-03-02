41e41a06888d3ffcf3e79bec41caeac714dd62b1
Michael Totschnig <michael@totschnig.org>
2017-03-02 17:29:36 +0100

    formatted

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/provider/DateCalculationTest.java
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/provider/DateCalculationTest.java
@@ -1,22 +1,21 @@
 package org.totschnig.myexpenses.test.provider;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-
-import org.totschnig.myexpenses.provider.DatabaseConstants;
-import org.totschnig.myexpenses.provider.DbUtils;
-import org.totschnig.myexpenses.provider.TransactionProvider;
-import org.totschnig.myexpenses.util.Utils;
-
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.test.ProviderTestCase2;
 import android.test.suitebuilder.annotation.Suppress;
-import android.util.Log;
+
+import org.totschnig.myexpenses.provider.DatabaseConstants;
+import org.totschnig.myexpenses.provider.DbUtils;
+import org.totschnig.myexpenses.provider.TransactionProvider;
+import org.totschnig.myexpenses.util.Utils;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class DateCalculationTest extends ProviderTestCase2<TransactionProvider> {
 
@@ -25,28 +24,29 @@ public class DateCalculationTest extends ProviderTestCase2<TransactionProvider> 
 
   String TABLE = "test_dates";
   String KEY_DATE = "date";
+
   public DateCalculationTest() {
-    super(TransactionProvider.class,TransactionProvider.AUTHORITY);
+    super(TransactionProvider.class, TransactionProvider.AUTHORITY);
   }
-  
+
   /*
    * Sets up the test environment before each test method. Creates a mock content resolver,
    * gets the provider under test, and creates a new database for the provider.
    */
   @Override
   protected void setUp() throws Exception {
-      // Calls the base class implementation of this method.
-      super.setUp();
+    // Calls the base class implementation of this method.
+    super.setUp();
 
       /*
        * Gets a handle to the database underlying the provider. Gets the provider instance
        * created in super.setUp(), gets the DatabaseOpenHelper for the provider, and gets
        * a database object from the helper.
        */
-      mDb = getProvider().getOpenHelperForTest().getWritableDatabase();
-      mDb.execSQL("CREATE TABLE " + TABLE + " (" + KEY_DATE + " DATETIME not null)");
+    mDb = getProvider().getOpenHelperForTest().getWritableDatabase();
+    mDb.execSQL("CREATE TABLE " + TABLE + " (" + KEY_DATE + " DATETIME not null)");
   }
-  
+
   @Override
   protected void tearDown() throws Exception {
     super.tearDown();
@@ -54,14 +54,14 @@ public class DateCalculationTest extends ProviderTestCase2<TransactionProvider> 
   }
 
   public void testDateCalculationsForWeekGroupsWeekStartsOnMonday() {
-    DatabaseConstants.buildLocalized(new Locale("de","DE"));
-    assertEquals(Calendar.MONDAY,DatabaseConstants.weekStartsOn);
+    DatabaseConstants.buildLocalized(new Locale("de", "DE"));
+    assertEquals(Calendar.MONDAY, DatabaseConstants.weekStartsOn);
     doTheTest();
   }
-  
+
   public void testDateCalculationsForWeekGroupsWeekStartsOnSunday() {
-    DatabaseConstants.buildLocalized(new Locale("en","US"));
-    assertEquals(Calendar.SUNDAY,DatabaseConstants.weekStartsOn);
+    DatabaseConstants.buildLocalized(new Locale("en", "US"));
+    assertEquals(Calendar.SUNDAY, DatabaseConstants.weekStartsOn);
     doTheTest();
   }
 
@@ -69,57 +69,63 @@ public class DateCalculationTest extends ProviderTestCase2<TransactionProvider> 
   @Suppress
   public void testDateCalculationsForWeekGroupsWeekStartsOnSaturday() {
     if ("goldfish".equals(Build.HARDWARE)) return; //on emulator locale is not configured correctly
-    DatabaseConstants.buildLocalized(new Locale("ar","SA"));
-    assertEquals(Calendar.SATURDAY,DatabaseConstants.weekStartsOn);
+    DatabaseConstants.buildLocalized(new Locale("ar", "SA"));
+    assertEquals(Calendar.SATURDAY, DatabaseConstants.weekStartsOn);
     doTheTest();
   }
-  
+
   private void doTheTest() {
     DateFormat dateformat = Utils.localizedYearlessDateFormat();
-    Log.i("DEBUG", DatabaseConstants.getWeekEnd());
+    //Log.i("DEBUG", DatabaseConstants.getWeekEnd());
     ContentValues v = new ContentValues();
     for (int year = 2010; year < 2022; year++) {
       int month = 12, day = 26;
       while (true) {
-        v.put(KEY_DATE, new GregorianCalendar(year, month, day).getTime().getTime()/1000);
-        mDb.insertOrThrow(TABLE,null,v);
+        v.put(KEY_DATE, new GregorianCalendar(year, month, day).getTime().getTime() / 1000);
+        mDb.insertOrThrow(TABLE, null, v);
         day++;
-        if (day==32) {
-          month=1;
-          day=1;
+        if (day == 32) {
+          month = 1;
+          day = 1;
         }
-        if (day==7) {
+        if (day == 7) {
           break;
         }
       }
     }
+    String[] projection = {
+        DatabaseConstants.getYearOfWeekStart() + " AS year",
+        DatabaseConstants.getWeek() + " AS week",
+        DatabaseConstants.getWeekStart() + " AS week_start",
+        DatabaseConstants.getWeekEnd() + " AS week_end"
+    };
     Cursor c = mDb.query(
         TABLE,
-        new String[] {
-            DatabaseConstants.getYearOfWeekStart() + " AS year",
-            DatabaseConstants.getWeek() + " AS week",
-            DatabaseConstants.getWeekStart() + " AS week_start",
-            DatabaseConstants.getWeekEnd() + " AS week_end",},
+        projection,
         null, null, null, null, null);
-    assertEquals(12*12,c.getCount());
+    assertEquals(12 * 12, c.getCount());
     c.moveToFirst();
     while (!c.isAfterLast()) {
       int year = c.getInt(0);
       int week = c.getInt(1);
+      String weekStartFromGroupSqlExpression = DbUtils.weekStartFromGroupSqlExpression(year, week);
+      String weekEndFromGroupSqlExpression = DbUtils.weekEndFromGroupSqlExpression(year, week);
+      //Log.i("Debug", weekStartFromGroupSqlExpression);
+      //Log.i("Debug", weekEndFromGroupSqlExpression);
       Cursor check = mDb.query(
           TABLE,
           new String[]{
-              DbUtils.weekStartFromGroupSqlExpression(year,week),
-              DbUtils.weekEndFromGroupSqlExpression(year,week)
+              weekStartFromGroupSqlExpression,
+              weekEndFromGroupSqlExpression
           },
           null, null, null, null, null);
       check.moveToFirst();
       assertEquals(
-          Utils.convDateTime(c.getString(2),dateformat),
-          Utils.convDateTime(check.getString(0),dateformat));
+          Utils.convDateTime(c.getString(2), dateformat),
+          Utils.convDateTime(check.getString(0), dateformat));
       assertEquals(
-          Utils.convDateTime(c.getString(3),dateformat),
-          Utils.convDateTime(check.getString(1),dateformat));
+          Utils.convDateTime(c.getString(3), dateformat),
+          Utils.convDateTime(check.getString(1), dateformat));
       check.close();
       c.moveToNext();
     }
