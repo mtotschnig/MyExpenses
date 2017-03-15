40bca02f6f005f249f77f189ea8b721ff54fce33
Michael Totschnig <michael@totschnig.org>
2017-03-15 19:19:16 +0100

    store constant value independent from localization for No synchronization,

--- myExpenses/src/main/java/org/totschnig/myexpenses/preference/AccountPreference.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/preference/AccountPreference.java
@@ -4,28 +4,36 @@ import android.content.Context;
 import android.support.v7.preference.ListPreference;
 import android.util.AttributeSet;
 
-import com.annimon.stream.Stream;
+import com.annimon.stream.Collectors;
 
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.sync.GenericAccountService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountPreference extends ListPreference {
+
+  public static final String SYNCHRONIZATION_NONE = "_SYNCHRONIZATION_NONE_";
 
   public AccountPreference(Context context, AttributeSet attrs) {
     super(context, attrs);
-    CharSequence[] accounts = entries();
-    setEntries(accounts);
-    setEntryValues(accounts);
+    List<String> entries = GenericAccountService.getAccountsAsStream().map(account -> account.name).collect(Collectors.toList());
+    List<String> entryValues = new ArrayList<>(entries);
+    entries.add(0, getContext().getString(R.string.synchronization_none));
+    entryValues.add(0, SYNCHRONIZATION_NONE);
+
+    setEntries(entries.toArray(new String[entries.size()]));
+    setEntryValues(entryValues.toArray(new String[entryValues.size()]));
   }
 
   public AccountPreference(Context context) {
     this(context, null);
   }
 
-  private CharSequence[] entries() {
-    return Stream.concat(
-        Stream.of(getContext().getString(R.string.synchronization_none)),
-        GenericAccountService.getAccountsAsStream().map(account -> account.name))
-        .toArray(size -> new String[size]);
+  @Override
+  public int findIndexOfValue(String value) {
+    int indexOfValue = super.findIndexOfValue(value);
+    return indexOfValue > 0 ? indexOfValue : 0;
   }
 }
