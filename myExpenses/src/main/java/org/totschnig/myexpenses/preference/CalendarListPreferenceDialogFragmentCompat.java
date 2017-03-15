bf7721eb6c821495d2fdbd0684aebe42783823b2
Michael Totschnig <michael@totschnig.org>
2017-03-15 17:39:42 +0100

    formatting, clean code

--- myExpenses/src/main/java/org/totschnig/myexpenses/preference/CalendarListPreferenceDialogFragmentCompat.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/preference/CalendarListPreferenceDialogFragmentCompat.java
@@ -17,12 +17,9 @@ import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.provider.DbUtils;
 
-/**
- * Created by privat on 07.11.15.
- */
 public class CalendarListPreferenceDialogFragmentCompat extends PreferenceDialogFragmentCompat {
   @Override
-  protected void onPrepareDialogBuilder( AlertDialog.Builder builder ) {
+  protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
     final ListPreference preference = (ListPreference) getPreference();
     boolean localExists = false;
     Cursor selectionCursor;
@@ -55,26 +52,26 @@ public class CalendarListPreferenceDialogFragmentCompat extends PreferenceDialog
             selectedIndex = calCursor.getPosition();
           }
           if (DbUtils.getString(calCursor, 1).equals(MyApplication.PLANNER_ACCOUNT_NAME)
-              && DbUtils.getString(calCursor,2).equals(CalendarContractCompat.ACCOUNT_TYPE_LOCAL)
-              && DbUtils.getString(calCursor,3).equals(MyApplication.PLANNER_CALENDAR_NAME))
+              && DbUtils.getString(calCursor, 2).equals(CalendarContractCompat.ACCOUNT_TYPE_LOCAL)
+              && DbUtils.getString(calCursor, 3).equals(MyApplication.PLANNER_CALENDAR_NAME))
             localExists = true;
         } while (calCursor.moveToNext());
       }
       if (localExists || Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
         selectionCursor = calCursor;
       } else {
-        MatrixCursor extras = new MatrixCursor(new String[] {
+        MatrixCursor extras = new MatrixCursor(new String[]{
             CalendarContractCompat.Calendars._ID,
             CalendarContractCompat.Calendars.ACCOUNT_NAME,
             CalendarContractCompat.Calendars.ACCOUNT_TYPE,
             CalendarContractCompat.Calendars.NAME,
             "full_name"});
-        extras.addRow(new String[] {
-            "-1", "","","",
-            getContext().getString(R.string.pref_planning_calendar_create_local) });
-        selectionCursor = new MergeCursor(new Cursor[] {calCursor,extras});
+        extras.addRow(new String[]{
+            "-1", "", "", "",
+            getContext().getString(R.string.pref_planning_calendar_create_local)});
+        selectionCursor = new MergeCursor(new Cursor[]{calCursor, extras});
       }
-      builder.setSingleChoiceItems(selectionCursor,selectedIndex,"full_name",
+      builder.setSingleChoiceItems(selectionCursor, selectedIndex, "full_name",
           new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
               long itemId = ((AlertDialog) dialog).getListView().getItemIdAtPosition(which);
@@ -90,7 +87,7 @@ public class CalendarListPreferenceDialogFragmentCompat extends PreferenceDialog
                   preference.setValue(plannerId);
                 }
               } else {
-                if(preference.callChangeListener(itemId)) {
+                if (preference.callChangeListener(itemId)) {
                   preference.setValue(String.valueOf(itemId));
                 }
               }
@@ -102,12 +99,12 @@ public class CalendarListPreferenceDialogFragmentCompat extends PreferenceDialog
     } else {
       builder.setMessage("Calendar provider not available");
     }
-    builder.setPositiveButton( null, null );
+    builder.setPositiveButton(null, null);
   }
 
   @Override
   public void onDialogClosed(boolean b) {
-    //nothing to do since directly handlind in onClickListener of SinglechoiceItems
+    //nothing to do since directly handled in onClickListener of SingleChoiceItems
   }
 
   public static CalendarListPreferenceDialogFragmentCompat newInstance(String key) {
