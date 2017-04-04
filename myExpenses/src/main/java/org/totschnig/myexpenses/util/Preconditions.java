b5ad2c4d2d47070e59695f4865158064ee8ee14b
Michael Totschnig <michael@totschnig.org>
2017-04-04 08:45:24 +0200

    helper to check conditions

---
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/Preconditions.java
@@ -0,0 +1,49 @@
+package org.totschnig.myexpenses.util;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import static java.lang.String.format;
+
+public class Preconditions {
+
+  @NonNull
+  public static <T> T checkNotNull(@Nullable T reference) {
+    return dagger.internal.Preconditions.checkNotNull(reference);
+  }
+
+  @NonNull
+  public static <T> T checkNotNull(@Nullable T reference, String errorMessage) {
+    return dagger.internal.Preconditions.checkNotNull(reference, errorMessage);
+  }
+
+  public static void checkArgument(boolean expression) {
+    if (!expression) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  public static void checkArgument(boolean expression, @Nullable Object errorMessage) {
+    if (!expression) {
+      throw new IllegalArgumentException(String.valueOf(errorMessage));
+    }
+  }
+
+  public static void checkState(boolean expression) {
+    if (!expression) {
+      throw new IllegalStateException();
+    }
+  }
+
+  public static void checkState(boolean expression, @Nullable Object errorMessage) {
+    if (!expression) {
+      throw new IllegalStateException(String.valueOf(errorMessage));
+    }
+  }
+
+  public static void checkState(boolean expression, String errorMessageTemplate, Object... errorMessageArgs) {
+    if (!expression) {
+      throw new IllegalStateException(format(errorMessageTemplate, errorMessageArgs));
+    }
+  }
+}
