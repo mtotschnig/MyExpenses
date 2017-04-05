bd1bcbdfbc6dad25d2bef1c34da8ba5fd55b320b
Michael Totschnig <michael@totschnig.org>
2017-04-06 00:39:31 +0200

    read acra config from properties

--- myExpenses/src/main/java/org/totschnig/myexpenses/di/AppModule.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/di/AppModule.java
@@ -6,8 +6,11 @@ import org.acra.ReportingInteractionMode;
 import org.acra.config.ACRAConfiguration;
 import org.acra.config.ACRAConfigurationException;
 import org.acra.config.ConfigurationBuilder;
+import org.acra.sender.HttpSender;
+import org.totschnig.myexpenses.BuildConfig;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
+import org.totschnig.myexpenses.util.DistribHelper;
 import org.totschnig.myexpenses.util.HashLicenceHandler;
 import org.totschnig.myexpenses.util.LicenceHandler;
 
@@ -37,13 +40,25 @@ public class AppModule {
   ACRAConfiguration providesAcraConfiguration() {
     if (MyApplication.isInstrumentationTest()) return null;
     try {
-      return new ConfigurationBuilder(application)
-          .setReportingInteractionMode(ReportingInteractionMode.DIALOG)
-          .setMailTo("bug-reports@myexpenses.mobi")
-          .setResDialogText(R.string.crash_dialog_text)
-          .setResDialogTitle(R.string.crash_dialog_title)
-          .setResDialogCommentPrompt(R.string.crash_dialog_comment_prompt)
-          .build();
+      if (DistribHelper.isGithub()) {
+        return new ConfigurationBuilder(application)
+            .setReportingInteractionMode(ReportingInteractionMode.DIALOG)
+            .setMailTo("bug-reports@myexpenses.mobi")
+            .setResDialogText(R.string.crash_dialog_text)
+            .setResDialogTitle(R.string.crash_dialog_title)
+            .setResDialogCommentPrompt(R.string.crash_dialog_comment_prompt)
+            .build();
+      } else {
+        return new ConfigurationBuilder(application)
+            .setFormUri(BuildConfig.ACRA_FORM_URI)
+            .setReportType(HttpSender.Type.JSON)
+            .setHttpMethod(HttpSender.Method.PUT)
+            .setFormUriBasicAuthLogin(BuildConfig.ACRA_FORM_URI_BASIC_AUTH_LOGIN)
+            .setFormUriBasicAuthPassword(BuildConfig.ACRA_FORM_URI_BASIC_AUTH_PASSWORD)
+            .setLogcatArguments("-t", "250", "-v", "long", "ActivityManager:I", "MyExpenses:V", "*:S")
+            .setExcludeMatchingSharedPreferencesKeys(new String[]{"planner_calendar_path","password"})
+            .build();
+      }
     } catch (ACRAConfigurationException e) {
       Timber.e(e, "ACRA not initialized");
       return null;
