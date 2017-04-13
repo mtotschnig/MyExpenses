abf844ca023f431ad1b8ce4b7d3687cca10928ec
Michael Totschnig <michael@totschnig.org>
2017-04-13 19:42:20 +0200

    updated test to new UI

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/ManageCurrenciesTest.java
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/ManageCurrenciesTest.java
@@ -2,9 +2,7 @@ package org.totschnig.myexpenses.test.espresso;
 
 import android.content.OperationApplicationException;
 import android.os.RemoteException;
-import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
-import android.view.KeyEvent;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,7 +20,6 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -34,9 +31,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.totschnig.myexpenses.model.CurrencyEnum.EUR;
 
-/**
- * Created by michaeltotschnig on 01.03.16.
- */
 public class ManageCurrenciesTest {
 
 
@@ -65,8 +59,9 @@ public class ManageCurrenciesTest {
     assertEquals(0, before.getAmountMajor().compareTo(new BigDecimal(38)));
     onData(is(EUR))
         .inAdapterView(withId(android.R.id.list)).perform(click());
-    onView(withId(R.id.EditTextDialogInput))
-        .perform(replaceText("3"), closeSoftKeyboard(), pressKey(KeyEvent.KEYCODE_ENTER));
+    onView(withId(R.id.edt_number_fraction_digits))
+        .perform(replaceText("3"), closeSoftKeyboard());
+    onView(withText(android.R.string.ok)).perform(click());
     if (withUpdate)
       onView(withId(R.id.checkbox)).perform(click());
     onView(withText(android.R.string.ok)).perform(click());
