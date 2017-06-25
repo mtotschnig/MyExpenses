1a66b413f235b9d41958977bb252f7e21d5474c3
Michael Totschnig <michael@totschnig.org>
2017-06-25 22:22:05 +0200

    formatted

--- myExpenses/src/main/java/org/totschnig/myexpenses/service/DailyAutoBackupScheduler.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/service/DailyAutoBackupScheduler.java
@@ -21,62 +21,60 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Denis Solonenko
- * Date: 12/16/11 12:54 AM
+ * Original implementation based on Financisto
  */
 public class DailyAutoBackupScheduler {
 
-    private DailyAutoBackupScheduler() {
-    }
+  private DailyAutoBackupScheduler() {
+  }
 
-    public static void updateAutoBackupAlarms(Context context) {
-        if (PrefKey.AUTO_BACKUP.getBoolean(false) &&
-            PrefKey.AUTO_BACKUP_DIRTY.getBoolean(true)) {
-            if (ContribFeature.AUTO_BACKUP.hasAccess() || ContribFeature.AUTO_BACKUP.usagesLeft()>0) {
-                scheduleAutoBackup(context);
-            }
-        } else {
-            cancelAutoBackup(context);
-        }
+  public static void updateAutoBackupAlarms(Context context) {
+    if (PrefKey.AUTO_BACKUP.getBoolean(false) &&
+        PrefKey.AUTO_BACKUP_DIRTY.getBoolean(true)) {
+      if (ContribFeature.AUTO_BACKUP.hasAccess() || ContribFeature.AUTO_BACKUP.usagesLeft() > 0) {
+        scheduleAutoBackup(context);
+      }
+    } else {
+      cancelAutoBackup(context);
     }
+  }
 
-    public static void scheduleAutoBackup(Context context) {
-        //cancel existing one
-        cancelAutoBackup(context);
-        AlarmManager service = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pendingIntent = createPendingIntent(context);
-        Date scheduledTime = getScheduledTime();
-        service.set(AlarmManager.RTC_WAKEUP, scheduledTime.getTime(), pendingIntent);
-    }
+  public static void scheduleAutoBackup(Context context) {
+    //cancel existing one
+    cancelAutoBackup(context);
+    AlarmManager service = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    PendingIntent pendingIntent = createPendingIntent(context);
+    Date scheduledTime = getScheduledTime();
+    service.set(AlarmManager.RTC_WAKEUP, scheduledTime.getTime(), pendingIntent);
+  }
 
-    public static void cancelAutoBackup(Context context) {
-        AlarmManager service = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pendingIntent = createPendingIntent(context);
-        service.cancel(pendingIntent);
-        pendingIntent.cancel();
-    }
+  public static void cancelAutoBackup(Context context) {
+    AlarmManager service = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    PendingIntent pendingIntent = createPendingIntent(context);
+    service.cancel(pendingIntent);
+    pendingIntent.cancel();
+  }
 
-    private static PendingIntent createPendingIntent(Context context) {
-        Intent intent = new Intent(GenericAlarmReceiver.SCHEDULED_BACKUP);
-        return PendingIntent.getBroadcast(context, -100, intent, PendingIntent.FLAG_ONE_SHOT);
-    }
+  private static PendingIntent createPendingIntent(Context context) {
+    Intent intent = new Intent(GenericAlarmReceiver.SCHEDULED_BACKUP);
+    return PendingIntent.getBroadcast(context, -100, intent, PendingIntent.FLAG_ONE_SHOT);
+  }
 
-    private static Date getScheduledTime() {
-        int hhmm = PrefKey.AUTO_BACKUP_TIME.getInt(TimePreference.DEFAULT_VALUE);
-        int hh = hhmm/100;
-        int mm = hhmm - 100*hh;
-        Calendar c = Calendar.getInstance();
-        long now = System.currentTimeMillis();
-        c.setTimeInMillis(now);
-        c.set(Calendar.HOUR_OF_DAY, hh);
-        c.set(Calendar.MINUTE, mm);
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MILLISECOND, 0);
-        if (c.getTimeInMillis() < now) {
-            c.add(Calendar.DAY_OF_MONTH, 1);
-        }
-        return c.getTime();
+  private static Date getScheduledTime() {
+    int hhmm = PrefKey.AUTO_BACKUP_TIME.getInt(TimePreference.DEFAULT_VALUE);
+    int hh = hhmm / 100;
+    int mm = hhmm - 100 * hh;
+    Calendar c = Calendar.getInstance();
+    long now = System.currentTimeMillis();
+    c.setTimeInMillis(now);
+    c.set(Calendar.HOUR_OF_DAY, hh);
+    c.set(Calendar.MINUTE, mm);
+    c.set(Calendar.SECOND, 0);
+    c.set(Calendar.MILLISECOND, 0);
+    if (c.getTimeInMillis() < now) {
+      c.add(Calendar.DAY_OF_MONTH, 1);
     }
+    return c.getTime();
+  }
 
 }
