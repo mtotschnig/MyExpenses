23bf5dbc04547011a609ff521d35fc107ebe6348
Michael Totschnig <michael@totschnig.org>
2017-11-18 01:38:03 +0100

    added missing stub

--- myExpenses/src/main/java/org/onepf/oms/appstore/googleUtils/Inventory.java
+++ myExpenses/src/main/java/org/onepf/oms/appstore/googleUtils/Inventory.java
@@ -1,7 +1,14 @@
 package org.onepf.oms.appstore.googleUtils;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Inventory {
   public Purchase getPurchase(String sku) {
     return null;
+  }
+
+  public List<String> getAllOwnedSkus() {
+    return Collections.emptyList();
   }
 }
