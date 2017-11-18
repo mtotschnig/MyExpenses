c535856326ba5fc967f57b531fc124a410cda704
Michael Totschnig <michael@totschnig.org>
2017-11-18 16:23:25 +0100

    add blackberry pro package

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/Package.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/Package.java
@@ -11,7 +11,7 @@ import org.totschnig.myexpenses.util.Preconditions;
 import java.util.Currency;
 
 public enum Package {
-  Contrib(300), Upgrade(250), Extended(500), Professional_1(100), Professional_6(500), Professional_12(900), Professional_36(2000), Professional_Amazon(900);
+  Contrib(300), Upgrade(250), Extended(500), Professional_1(100), Professional_6(500), Professional_12(900), Professional_36(2000), Professional_Amazon(900), Professional_Blackberry(2000);
 
   public long getDefaultPrice() {
     return defaultPrice;
