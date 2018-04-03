bbb8a042dcf0b244446e8d423a0d8c69e78c7606
Michael Totschnig <michael@totschnig.org>
2018-04-03 17:53:17 +0200

    Trailing dots not needed Clean code: Protect agains possible NPE build fork of  latest unreleased upstream SimpleDialogFragments, with fix for https://github.com/eltos/SimpleDialogFragments/issues/27

--- myExpenses/src/main/java/org/totschnig/myexpenses/service/PlanNotificationClickHandler.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/service/PlanNotificationClickHandler.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
@@ -34,9 +35,12 @@ public class PlanNotificationClickHandler extends IntentService {
   }
 
   @Override
-  protected void onHandleIntent(Intent intent) {
+  protected void onHandleIntent(@Nullable Intent intent) {
     String message;
-    Bundle extras = intent.getExtras();
+    if (intent == null) return;
+    final Bundle extras = intent.getExtras();
+    final String action = intent.getAction();
+    if (extras == null || action == null) return;
     String title = extras.getString(PlanExecutor.KEY_TITLE);
     NotificationBuilderWrapper builder = new NotificationBuilderWrapper(this,
         NotificationBuilderWrapper.CHANNEL_ID_PLANNER)
@@ -45,43 +49,48 @@ public class PlanNotificationClickHandler extends IntentService {
     int notificationId = extras.getInt(MyApplication.KEY_NOTIFICATION_ID);
     Long templateId = extras.getLong(DatabaseConstants.KEY_TEMPLATEID);
     Long instanceId = extras.getLong(DatabaseConstants.KEY_INSTANCEID);
-    if (intent.getAction().equals(PlanExecutor.ACTION_APPLY)) {
-      Transaction t = Transaction.getInstanceFromTemplate(templateId);
-      if (t == null) {
-        message = getString(R.string.save_transaction_template_deleted);
-      } else {
-        t.setDate(new Date(extras.getLong(DatabaseConstants.KEY_DATE)));
-        t.originPlanInstanceId = instanceId;
-        if (t.save() == null) {
-          message = getString(R.string.save_transaction_error);
+    switch (action) {
+      case PlanExecutor.ACTION_APPLY:
+        Transaction t = Transaction.getInstanceFromTemplate(templateId);
+        if (t == null) {
+          message = getString(R.string.save_transaction_template_deleted);
         } else {
-          message = getResources().getQuantityString(
-              R.plurals.save_transaction_from_template_success, 1, 1);
-          Intent displayIntent = new Intent(this, MyExpenses.class)
-              .putExtra(DatabaseConstants.KEY_ROWID, t.getAccountId())
-              .putExtra(DatabaseConstants.KEY_TRANSACTIONID, t.getId());
-          PendingIntent resultIntent = PendingIntent.getActivity(this, notificationId, displayIntent,
-              PendingIntent.FLAG_UPDATE_CURRENT);
-          builder.setContentIntent(resultIntent);
-          builder.setAutoCancel(true);
+          t.setDate(new Date(extras.getLong(DatabaseConstants.KEY_DATE)));
+          t.originPlanInstanceId = instanceId;
+          if (t.save() == null) {
+            message = getString(R.string.save_transaction_error);
+          } else {
+            message = getResources().getQuantityString(
+                R.plurals.save_transaction_from_template_success, 1, 1);
+            Intent displayIntent = new Intent(this, MyExpenses.class)
+                .putExtra(DatabaseConstants.KEY_ROWID, t.getAccountId())
+                .putExtra(DatabaseConstants.KEY_TRANSACTIONID, t.getId());
+            PendingIntent resultIntent = PendingIntent.getActivity(this, notificationId, displayIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(resultIntent);
+            builder.setAutoCancel(true);
+          }
         }
-      }
-    } else if (intent.getAction().equals(PlanExecutor.ACTION_CANCEL)) {
-      ContentValues values = new ContentValues();
-      values.putNull(KEY_TRANSACTIONID);
-      values.put(KEY_TEMPLATEID, templateId);
-      values.put(KEY_INSTANCEID, instanceId);
-      try {
-        getContentResolver().insert(TransactionProvider.PLAN_INSTANCE_STATUS_URI, values);
-        message = getString(R.string.plan_execution_canceled);
-      } catch (SQLiteConstraintException e) {
-        message = getString(R.string.save_transaction_template_deleted);
-      }
-    } else {
-      return;
+        break;
+      case PlanExecutor.ACTION_CANCEL:
+        ContentValues values = new ContentValues();
+        values.putNull(KEY_TRANSACTIONID);
+        values.put(KEY_TEMPLATEID, templateId);
+        values.put(KEY_INSTANCEID, instanceId);
+        try {
+          getContentResolver().insert(TransactionProvider.PLAN_INSTANCE_STATUS_URI, values);
+          message = getString(R.string.plan_execution_canceled);
+        } catch (SQLiteConstraintException e) {
+          message = getString(R.string.save_transaction_template_deleted);
+        }
+        break;
+      default:
+        return;
     }
     builder.setContentText(message);
-    ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE))
-        .notify(notificationId, builder.build());
+    final NotificationManager systemService = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    if (systemService != null) {
+      systemService.notify(notificationId, builder.build());
+    }
   }
 }
