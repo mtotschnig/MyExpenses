0fa8a2c4fc49cc3820e961f0ff2a6d1ed0ae99d1
Michael Totschnig <michael@totschnig.org>
2018-04-10 13:39:57 +0200

    Synced with Transifex No need to restrict support message. Google Translate is here to help Add configuration to support message template

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/CommonCommands.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/CommonCommands.java
@@ -17,14 +17,20 @@ package org.totschnig.myexpenses.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
+import android.util.SparseArray;
 
+import org.acra.ACRA;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.dialog.HelpDialogFragment;
@@ -36,12 +42,33 @@ import org.totschnig.myexpenses.util.licence.LicenceHandler;
 import org.totschnig.myexpenses.util.licence.LicenceStatus;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
+import static org.acra.ACRA.LOG_TAG;
 import static org.totschnig.myexpenses.util.DistribHelper.getMarketSelfUri;
 import static org.totschnig.myexpenses.util.DistribHelper.getVersionInfo;
 
+
+
 public class CommonCommands {
+  private static final String SUFFIX_MASK = "_MASK";
+  private static final String FIELD_SCREENLAYOUT = "screenLayout";
+  private static final String FIELD_UIMODE = "uiMode";
+  private static final String FIELD_MNC = "mnc";
+  private static final String FIELD_MCC = "mcc";
+  private static final String PREFIX_UI_MODE = "UI_MODE_";
+  private static final String PREFIX_TOUCHSCREEN = "TOUCHSCREEN_";
+  private static final String PREFIX_SCREENLAYOUT = "SCREENLAYOUT_";
+  private static final String PREFIX_ORIENTATION = "ORIENTATION_";
+  private static final String PREFIX_NAVIGATIONHIDDEN = "NAVIGATIONHIDDEN_";
+  private static final String PREFIX_NAVIGATION = "NAVIGATION_";
+  private static final String PREFIX_KEYBOARDHIDDEN = "KEYBOARDHIDDEN_";
+  private static final String PREFIX_KEYBOARD = "KEYBOARD_";
+  private static final String PREFIX_HARDKEYBOARDHIDDEN = "HARDKEYBOARDHIDDEN_";
   private CommonCommands() {
   }
 
@@ -83,14 +110,13 @@ public class CommonCommands {
             "[" + ctx.getString(R.string.app_name) + "] Feedback"
         );
         String messageBody = String.format(Locale.ROOT,
-            "APP_VERSION:%s\nANDROID_VERSION:%s\nBRAND:%s\nMODEL:%s\nLANGUAGE:%s%s\n\n%s\n\n",
+            "APP_VERSION:%s\nANDROID_VERSION:%s\nBRAND:%s\nMODEL:%s\nCONFIGURATION:%s%s\n\n",
             getVersionInfo(ctx),
             Build.VERSION.RELEASE,
             Build.BRAND,
             Build.MODEL,
-            Locale.getDefault().toString(),
-            licenceInfo,
-            ctx.getString(R.string.feedback_email_message));
+            configToJson(ctx.getResources().getConfiguration()),
+            licenceInfo);
         i.putExtra(android.content.Intent.EXTRA_TEXT, messageBody);
         if (!Utils.isIntentAvailable(ctx, i)) {
           ctx.showSnackbar(R.string.no_app_handling_email_available, Snackbar.LENGTH_LONG);
@@ -110,7 +136,7 @@ public class CommonCommands {
       case R.id.HELP_COMMAND:
         i = new Intent(ctx, Help.class);
         i.putExtra(HelpDialogFragment.KEY_VARIANT,
-            tag != null ? (Enum<?>) tag : ((ProtectedFragmentActivity) ctx).helpVariant);
+            tag != null ? (Enum<?>) tag : ctx.helpVariant);
         //for result is needed since it allows us to inspect the calling activity
         ctx.startActivityForResult(i, 0);
         return true;
@@ -147,4 +173,165 @@ public class CommonCommands {
     ctx.startActivityForResult(i, ProtectedFragmentActivity.CONTRIB_REQUEST);
   }
 
+  //#### FROM ACRA ####
+  /**
+   * Creates a {@link JSONObject} listing all values human readable
+   * from the provided Configuration instance.
+   *
+   * @param conf The Configuration to be described.
+   * @return A JSONObject with all fields of the given Configuration,
+   * with values replaced by constant names.
+   */
+  @NonNull
+  private static JSONObject configToJson(@NonNull Configuration conf) {
+    final JSONObject result = new JSONObject();
+    final Map<String, SparseArray<String>> valueArrays = getValueArrays();
+    for (final Field f : conf.getClass().getFields()) {
+      try {
+        if (!Modifier.isStatic(f.getModifiers())) {
+          final String fieldName = f.getName();
+          try {
+            if (f.getType().equals(int.class)) {
+              result.put(fieldName, getFieldValueName(valueArrays, conf, f));
+            } else if (f.get(conf) != null) {
+              result.put(fieldName, f.get(conf));
+            }
+          } catch (JSONException e) {
+            ACRA.log.w(LOG_TAG, "Could not collect configuration field " + fieldName, e);
+          }
+        }
+      } catch (@NonNull IllegalArgumentException e) {
+        ACRA.log.e(LOG_TAG, "Error while inspecting device configuration: ", e);
+      } catch (@NonNull IllegalAccessException e) {
+        ACRA.log.e(LOG_TAG, "Error while inspecting device configuration: ", e);
+      }
+    }
+    return result;
+  }
+
+  @NonNull
+  private static Map<String, SparseArray<String>> getValueArrays() {
+    final Map<String, SparseArray<String>> valueArrays = new HashMap<>();
+    final SparseArray<String> hardKeyboardHiddenValues = new SparseArray<>();
+    final SparseArray<String> keyboardValues = new SparseArray<>();
+    final SparseArray<String> keyboardHiddenValues = new SparseArray<>();
+    final SparseArray<String> navigationValues = new SparseArray<>();
+    final SparseArray<String> navigationHiddenValues = new SparseArray<>();
+    final SparseArray<String> orientationValues = new SparseArray<>();
+    final SparseArray<String> screenLayoutValues = new SparseArray<>();
+    final SparseArray<String> touchScreenValues = new SparseArray<>();
+    final SparseArray<String> uiModeValues = new SparseArray<>();
+
+    for (final Field f : Configuration.class.getFields()) {
+      if (Modifier.isStatic(f.getModifiers()) && Modifier.isFinal(f.getModifiers())) {
+        final String fieldName = f.getName();
+        try {
+          if (fieldName.startsWith(PREFIX_HARDKEYBOARDHIDDEN)) {
+            hardKeyboardHiddenValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_KEYBOARD)) {
+            keyboardValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_KEYBOARDHIDDEN)) {
+            keyboardHiddenValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_NAVIGATION)) {
+            navigationValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_NAVIGATIONHIDDEN)) {
+            navigationHiddenValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_ORIENTATION)) {
+            orientationValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_SCREENLAYOUT)) {
+            screenLayoutValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_TOUCHSCREEN)) {
+            touchScreenValues.put(f.getInt(null), fieldName);
+          } else if (fieldName.startsWith(PREFIX_UI_MODE)) {
+            uiModeValues.put(f.getInt(null), fieldName);
+          }
+        } catch (@NonNull IllegalArgumentException e) {
+          ACRA.log.w(LOG_TAG, "Error while inspecting device configuration: ", e);
+        } catch (@NonNull IllegalAccessException e) {
+          ACRA.log.w(LOG_TAG, "Error while inspecting device configuration: ", e);
+        }
+      }
+    }
+
+    valueArrays.put(PREFIX_HARDKEYBOARDHIDDEN, hardKeyboardHiddenValues);
+    valueArrays.put(PREFIX_KEYBOARD, keyboardValues);
+    valueArrays.put(PREFIX_KEYBOARDHIDDEN, keyboardHiddenValues);
+    valueArrays.put(PREFIX_NAVIGATION, navigationValues);
+    valueArrays.put(PREFIX_NAVIGATIONHIDDEN, navigationHiddenValues);
+    valueArrays.put(PREFIX_ORIENTATION, orientationValues);
+    valueArrays.put(PREFIX_SCREENLAYOUT, screenLayoutValues);
+    valueArrays.put(PREFIX_TOUCHSCREEN, touchScreenValues);
+    valueArrays.put(PREFIX_UI_MODE, uiModeValues);
+    return valueArrays;
+  }
+
+  /**
+   * Retrieve the name of the constant defined in the {@link Configuration}
+   * class which defines the value of a field in a {@link Configuration}
+   * instance.
+   *
+   * @param conf The instance of {@link Configuration} where the value is
+   *             stored.
+   * @param f    The {@link Field} to be inspected in the {@link Configuration}
+   *             instance.
+   * @return The value of the field f in instance conf translated to its
+   * constant name.
+   * @throws IllegalAccessException if the supplied field is inaccessible.
+   */
+  private static Object getFieldValueName(@NonNull Map<String, SparseArray<String>> valueArrays, @NonNull Configuration conf, @NonNull Field f) throws IllegalAccessException {
+    final String fieldName = f.getName();
+    switch (fieldName) {
+      case FIELD_MCC:
+      case FIELD_MNC:
+        return f.getInt(conf);
+      case FIELD_UIMODE:
+        return activeFlags(valueArrays.get(PREFIX_UI_MODE), f.getInt(conf));
+      case FIELD_SCREENLAYOUT:
+        return activeFlags(valueArrays.get(PREFIX_SCREENLAYOUT), f.getInt(conf));
+      default:
+        final SparseArray<String> values = valueArrays.get(fieldName.toUpperCase() + '_');
+        if (values == null) {
+          // Unknown field, return the raw int as String
+          return f.getInt(conf);
+        }
+
+        final String value = values.get(f.getInt(conf));
+        if (value == null) {
+          // Unknown value, return the raw int as String
+          return f.getInt(conf);
+        }
+        return value;
+    }
+  }
+
+  /**
+   * Some fields contain multiple value types which can be isolated by
+   * applying a bitmask. That method returns the concatenation of active
+   * values.
+   *
+   * @param valueNames The array containing the different values and names for this
+   *                   field. Must contain mask values too.
+   * @param bitfield   The bitfield to inspect.
+   * @return The names of the different values contained in the bitfield,
+   * separated by '+'.
+   */
+  @NonNull
+  private static String activeFlags(@NonNull SparseArray<String> valueNames, int bitfield) {
+    final StringBuilder result = new StringBuilder();
+
+    // Look for masks, apply it an retrieve the masked value
+    for (int i = 0; i < valueNames.size(); i++) {
+      final int maskValue = valueNames.keyAt(i);
+      if (valueNames.get(maskValue).endsWith(SUFFIX_MASK)) {
+        final int value = bitfield & maskValue;
+        if (value > 0) {
+          if (result.length() > 0) {
+            result.append('+');
+          }
+          result.append(valueNames.get(value));
+        }
+      }
+    }
+    return result.toString();
+  }
 }
