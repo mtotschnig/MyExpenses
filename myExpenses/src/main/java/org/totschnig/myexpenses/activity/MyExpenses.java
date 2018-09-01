467b453fb49c396a0d8ee6153b1687c5b9b70402
Michael Totschnig <michael@totschnig.org>
2018-09-01 22:06:47 +0200

    remove unneeded cast

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
@@ -27,7 +27,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -269,7 +268,7 @@ public class MyExpenses extends LaunchActivity implements
     mDrawerListAdapter = new MyGroupedAdapter(this, null, currencyFormatter, prefHandler);
 
     navigationView.setNavigationItemSelectedListener(item -> dispatchCommand(item.getItemId(), null));
-    NavigationMenuView navigationMenuView = (NavigationMenuView) navigationView.getChildAt(0);
+    View navigationMenuView = navigationView.getChildAt(0);
     if (navigationMenuView != null) {
       navigationMenuView.setVerticalScrollBarEnabled(false);
     }
