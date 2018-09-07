d889d949e0b7e27a925ab75c0adb5c97cbf89f7c
Michael Totschnig <michael@totschnig.org>
2018-09-07 11:17:07 +0200

    release

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/misc/SafeGuardTests.java
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/misc/SafeGuardTests.java
@@ -1,14 +1,15 @@
 package org.totschnig.myexpenses.test.misc;
 
-import org.totschnig.myexpenses.MyApplication;
-import org.totschnig.myexpenses.service.PlanExecutor;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.totschnig.myexpenses.MyApplication;
+import org.totschnig.myexpenses.util.licence.LicenceHandler;
+
 public class SafeGuardTests extends TestCase {
   public void testContribIsNotEnabled() {
-    Assert.assertFalse(MyApplication.getInstance().getLicenceHandler().isContribEnabled());
-    
+    final LicenceHandler licenceHandler = MyApplication.getInstance().getLicenceHandler();
+    Assert.assertFalse(licenceHandler.isContribEnabled());
+    Assert.assertNull(licenceHandler.getLicenceStatus());
   }
 }
