778b5f61979b6f113c8cef3e25c97f935bf9fcd1
Michael Totschnig <michael@totschnig.org>
2019-04-13 07:29:15 +0200

    We will live without Conscrypt as fallback (received one report from Blackberry device)

--- myExpenses/src/conscript/java/org/totschnig/myexpenses/di/SecurityProvider.java
+++ myExpenses/src/conscript/java/org/totschnig/myexpenses/di/SecurityProvider.java
@@ -5,10 +5,16 @@ import android.os.Build;
 
 import java.security.Security;
 
+import timber.log.Timber;
+
 public class SecurityProvider {
   public static void init(Context context) {
     if (Build.VERSION.SDK_INT < 22) {
-      Security.insertProviderAt(new org.conscrypt.OpenSSLProvider(), 1);
+      try {
+        Security.insertProviderAt(new org.conscrypt.OpenSSLProvider(), 1);
+      } catch (Exception e) {
+        Timber.e(e);
+      }
     }
   }
 }
