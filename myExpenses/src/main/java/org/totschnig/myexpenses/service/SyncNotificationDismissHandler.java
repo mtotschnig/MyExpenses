bcb26cd94a7fbe20832930a86a52f9dabe7b3d17
Michael Totschnig <michael@totschnig.org>
2019-06-23 07:04:54 +0200

    intent  can be null

--- myExpenses/src/main/java/org/totschnig/myexpenses/service/SyncNotificationDismissHandler.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/service/SyncNotificationDismissHandler.java
@@ -23,15 +23,17 @@ public class SyncNotificationDismissHandler extends IntentService {
 
   @Override
   protected void onHandleIntent(Intent intent) {
-    String accountName = intent.getStringExtra(DatabaseConstants.KEY_SYNC_ACCOUNT_NAME);
-    if (accountName != null) {
-      Account account = GenericAccountService.GetAccount(accountName);
-      if (!ContentResolver.isSyncActive(account, TransactionProvider.AUTHORITY)) {
-        Bundle bundle = new Bundle();
-        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
-        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
-        bundle.putBoolean(SyncAdapter.KEY_NOTIFICATION_CANCELLED, true);
-        ContentResolver.requestSync(account, TransactionProvider.AUTHORITY, bundle);
+    if (intent != null) {
+      String accountName = intent.getStringExtra(DatabaseConstants.KEY_SYNC_ACCOUNT_NAME);
+      if (accountName != null) {
+        Account account = GenericAccountService.GetAccount(accountName);
+        if (!ContentResolver.isSyncActive(account, TransactionProvider.AUTHORITY)) {
+          Bundle bundle = new Bundle();
+          bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+          bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+          bundle.putBoolean(SyncAdapter.KEY_NOTIFICATION_CANCELLED, true);
+          ContentResolver.requestSync(account, TransactionProvider.AUTHORITY, bundle);
+        }
       }
     }
   }
