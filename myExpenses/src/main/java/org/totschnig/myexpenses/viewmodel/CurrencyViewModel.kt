21aa2f85a264cf46f24a81a12a900bd9a60e1cff
Michael Totschnig <michael@totschnig.org>
2019-12-07 14:36:38 +0100

    Collator.getInstance observed to throw llegalArgumentException on Galaxy S7,  report 7fe493029bef140dfe332d242782340d

--- myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/CurrencyViewModel.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/CurrencyViewModel.kt
@@ -3,8 +3,10 @@ package org.totschnig.myexpenses.viewmodel
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import org.totschnig.myexpenses.provider.DatabaseConstants.KEY_CODE
 import org.totschnig.myexpenses.provider.TransactionProvider
 import org.totschnig.myexpenses.util.Utils
+import org.totschnig.myexpenses.util.crashreporting.CrashHandler
 import org.totschnig.myexpenses.viewmodel.data.Currency
 import java.text.Collator
 import java.util.*
@@ -22,16 +24,23 @@ open class CurrencyViewModel(application: Application) : ContentResolvingAndroid
     }
 
     fun loadCurrencies() {
-        val collator = Collator.getInstance()
-        disposable = briteContentResolver.createQuery(TransactionProvider.CURRENCIES_URI, null, null, null, null, true)
+        val collator: Collator? = try {
+            Collator.getInstance()
+        } catch (e: Exception) {
+            CrashHandler.report(e)
+            null
+        }
+        disposable = briteContentResolver.createQuery(TransactionProvider.CURRENCIES_URI, null, null, null, KEY_CODE, true)
                 .mapToList { Currency.create(it) }
                 .subscribe { currencies ->
-                    Collections.sort(currencies) { lhs, rhs ->
-                        val classCompare = Utils.compare(lhs.sortClass(), rhs.sortClass())
-                        if (classCompare == 0)
-                            collator.compare(lhs.toString(), rhs.toString())
-                        else
-                            classCompare
+                    if (collator != null) {
+                        Collections.sort(currencies) { lhs, rhs ->
+                            val classCompare = Utils.compare(lhs.sortClass(), rhs.sortClass())
+                            if (classCompare == 0)
+                                collator.compare(lhs.toString(), rhs.toString())
+                            else
+                                classCompare
+                        }
                     }
                     this.currencies.postValue(currencies)
                 }
