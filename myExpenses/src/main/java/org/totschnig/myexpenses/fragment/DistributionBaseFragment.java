ce8206d7b208f1a17af97af5f887d06be68da94a
Michael Totschnig <michael@totschnig.org>
2020-05-02 23:41:21 +0200

    fix for crash 318bc1aca9c466a58e9430444df200ad

--- myExpenses/src/main/java/org/totschnig/myexpenses/fragment/DistributionBaseFragment.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/fragment/DistributionBaseFragment.java
@@ -251,12 +251,16 @@ public abstract class DistributionBaseFragment extends CategoryList {
   }
 
   @Override
+  protected boolean hasSelectMultiple() {
+    return false;
+  }
+
+  @Override
   protected void configureMenuInternal(Menu menu, boolean hasChildren) {
     menu.findItem(R.id.EDIT_COMMAND).setVisible(false);
     menu.findItem(R.id.DELETE_COMMAND).setVisible(false);
     menu.findItem(R.id.SELECT_ALL_COMMAND).setVisible(false);
     menu.findItem(R.id.SELECT_COMMAND).setTitle(R.string.menu_show_transactions);
-    menu.findItem(R.id.SELECT_COMMAND_MULTIPLE).setVisible(false);
     menu.findItem(R.id.CREATE_COMMAND).setVisible(false);
     menu.findItem(R.id.MOVE_COMMAND).setVisible(false);
   }
