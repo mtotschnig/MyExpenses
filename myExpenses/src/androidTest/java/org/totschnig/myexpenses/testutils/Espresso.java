3cd2ca6074374687299ade926ca9277aea3a2e3c
Michael Totschnig <michael@totschnig.org>
2020-05-19 00:26:26 +0200

    remove legacy that may lead to failure

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/testutils/Espresso.java
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/testutils/Espresso.java
@@ -3,7 +3,6 @@ package org.totschnig.myexpenses.testutils;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.View;
-import android.view.ViewConfiguration;
 
 import org.hamcrest.Matcher;
 
@@ -18,7 +17,6 @@ import androidx.test.espresso.util.TreeIterables;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.pressMenuKey;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -36,25 +34,13 @@ import static org.hamcrest.Matchers.endsWith;
 public class Espresso {
 
   public static void openActionBarOverflowOrOptionsMenu(Context context, boolean isCab) {
-    if (hasVirtualOverflowButton(context)) {
-      // If we're using virtual keys - theres a chance we're in mid animation of switching
-      // between a contextual action bar and the non-contextual action bar. In this case there
-      // are 2 'More Options' buttons present. Lets wait till that is no longer the case.
-      onView(isRoot()).perform(new TransitionBridgingViewAction());
+    onView(isRoot()).perform(new TransitionBridgingViewAction());
 
-      onView(isCab ? localizedContextualOverFlowButtonMatcher(context) : localizedOverFlowButtonMatcher(context)).perform(click());
-    } else {
-      // either a hardware button exists, or we're on a pre-HC os.
-      onView(isRoot()).perform(pressMenuKey());
-    }
+    onView(isCab ? localizedContextualOverFlowButtonMatcher(context) : localizedOverFlowButtonMatcher(context)).perform(click());
   }
 
   public static void openActionBarOverflowOrOptionsMenu(Context context) {
     openActionBarOverflowOrOptionsMenu(context, false);
-  }
-
-  private static boolean hasVirtualOverflowButton(Context context) {
-    return !ViewConfiguration.get(context).hasPermanentMenuKey();
   }
 
   public static void checkEffectiveVisible(int... viewIds) {
