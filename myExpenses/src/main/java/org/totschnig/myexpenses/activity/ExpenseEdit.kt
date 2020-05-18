602e63d54f5b455b2de53ff029faff9b6f362705
Michael Totschnig <michael@totschnig.org>
2020-05-18 17:25:39 +0200

    delegate has been observed to not be initialized

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/ExpenseEdit.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/ExpenseEdit.kt
@@ -350,10 +350,14 @@ open class ExpenseEdit : AmountActivity(), LoaderManager.LoaderCallbacks<Cursor?
             }
         }
         viewModel.getMethods().observe(this, Observer { paymentMethods ->
-            delegate.setMethods(paymentMethods)
+            if (::delegate.isInitialized) {
+                delegate.setMethods(paymentMethods)
+            }
         })
         currencyViewModel.getCurrencies().observe(this, Observer<List<Currency?>> { currencies ->
-            delegate.setCurrencies(currencies, currencyContext)
+            if (::delegate.isInitialized) {
+                delegate.setCurrencies(currencies, currencyContext)
+            }
         })
         viewModel.getAccounts().observe(this, Observer { accounts ->
             if (accounts.isEmpty()) {
