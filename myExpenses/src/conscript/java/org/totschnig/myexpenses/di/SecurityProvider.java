229b06f6a4f2db6e4cb0c05295c268fc589ba6da
Michael Totschnig <michael@totschnig.org>
2020-06-05 18:07:34 +0200

    update conscrypt

--- myExpenses/src/conscript/java/org/totschnig/myexpenses/di/SecurityProvider.java
+++ myExpenses/src/conscript/java/org/totschnig/myexpenses/di/SecurityProvider.java
@@ -3,6 +3,8 @@ package org.totschnig.myexpenses.di;
 import android.content.Context;
 import android.os.Build;
 
+import org.conscrypt.Conscrypt;
+
 import java.security.Security;
 
 import timber.log.Timber;
@@ -11,7 +13,7 @@ public class SecurityProvider {
   public static void init(Context context) {
     if (Build.VERSION.SDK_INT < 22) {
       try {
-        Security.insertProviderAt(new org.conscrypt.OpenSSLProvider(), 1);
+        Security.insertProviderAt(Conscrypt.newProvider(), 1);
       } catch (Exception e) {
         Timber.e(e);
       }
