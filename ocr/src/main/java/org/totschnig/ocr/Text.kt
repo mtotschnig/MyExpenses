336de7a862de08ad07b34ef744f8e98a61315790
Michael Totschnig <michael@totschnig.org>
2020-10-06 15:34:15 +0200

    version upgrades keep result class

--- ocr/src/main/java/org/totschnig/ocr/Text.kt
+++ ocr/src/main/java/org/totschnig/ocr/Text.kt
@@ -2,16 +2,21 @@ package org.totschnig.ocr
 
 import android.graphics.Rect
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Keep
 data class Text(val textBlocks: List<TextBlock>): Parcelable
 
 @Parcelize
+@Keep
 data class TextBlock(val lines: List<Line>): Parcelable
 
 @Parcelize
+@Keep
 data class Line(val text: String, val boundingBox: Rect?, val elements: List<Element>): Parcelable
 
 @Parcelize
+@Keep
 data class Element(val text: String, val boundingBox: Rect?): Parcelable
