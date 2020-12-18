14863a5d4c5f5643131920304453823cb111594c
Michael Totschnig <michael@totschnig.org>
2020-12-18 16:09:24 +0100

    update to upstream HEAD mark test as flaky

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/SplitEditTest.kt
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/SplitEditTest.kt
@@ -20,6 +20,7 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.intercepting.SingleActivityFactory
@@ -139,6 +140,7 @@ class SplitEditTest : BaseUiTest() {
     }
 
     @Test
+    @FlakyTest
     fun loadEditSaveSplit() {
         mActivityRule.launchActivity(baseIntent.apply { putExtra(KEY_ROWID, prepareSplit()) })
         assertThat(waitForAdapter().count).isEqualTo(2)
