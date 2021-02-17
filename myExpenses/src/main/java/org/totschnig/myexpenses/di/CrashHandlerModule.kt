0f71aaed41da7aa1243542dc9b09c05df7685cca
Michael Totschnig <michael@totschnig.org>
2021-02-17 17:06:52 +0100

    fixes suggested by Lint library upgrades

--- myExpenses/src/main/java/org/totschnig/myexpenses/di/CrashHandlerModule.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/di/CrashHandlerModule.kt
@@ -2,7 +2,7 @@ package org.totschnig.myexpenses.di
 
 import dagger.Module
 import dagger.Provides
-import org.totschnig.myexpenses.MyApplication
+import org.totschnig.myexpenses.preference.PrefHandler
 import org.totschnig.myexpenses.util.crashreporting.AcraCrashHandler
 import org.totschnig.myexpenses.util.crashreporting.CrashHandler
 import javax.inject.Singleton
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 open class CrashHandlerModule {
     @Provides
     @Singleton
-    open fun providesCrashHandler(): CrashHandler {
+    open fun providesCrashHandler(prefHandler: PrefHandler): CrashHandler {
         return AcraCrashHandler()
     }
 }
