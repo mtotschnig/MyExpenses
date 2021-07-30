a280e9e93b7f8655e63d92a200aec96734cd4e14
Michael Totschnig <michael@totschnig.org>
2021-07-30 09:39:28 +0200

    accessing context from static companion object is not possible. Do not stop test on single failure, but collect all failures

--- myExpenses/src/test/java/org/totschnig/myexpenses/util/AppNameLocalizationTest.kt
+++ myExpenses/src/test/java/org/totschnig/myexpenses/util/AppNameLocalizationTest.kt
@@ -6,66 +6,88 @@ import com.squareup.phrase.Phrase
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.totschnig.myexpenses.R
 
-@RunWith(ParameterizedRobolectricTestRunner::class)
-class AppNameLocalizationTest(private val locale: String) {
+
+@RunWith(RobolectricTestRunner::class)
+class AppNameLocalizationTest {
     @Test
     fun shouldBuildWithAppName() {
         val context = ApplicationProvider.getApplicationContext<Application>()
-        setLocale(locale)
-        for (resId in intArrayOf(
-            R.string.dialog_contrib_reminder_remove_limitation,
-            R.string.dialog_contrib_text_1,
-            R.string.dialog_contrib_text_2,
-            R.string.dialog_remind_rate_how_many_stars,
-            R.string.dialog_remind_rate_1,
-            R.string.plan_calendar_name,
-            R.string.calendar_permission_required,
-            R.string.description_webdav_url,
-            R.string.warning_synchronization_folder_usage,
-            R.string.onboarding_ui_title,
-            R.string.crash_dialog_title,
-            R.string.crash_reports_user_info
-        )) {
-            try {
-                Utils.getTextWithAppName(context, resId)
-            } catch (e: Exception) {
-                Assert.fail(
-                    String.format(
-                        "Non-compliant resource %s for locale %s",
-                        context.resources.getResourceName(resId),
-                        locale
-                    )
-                )
+        val locales = context.resources.getStringArray(R.array.pref_ui_language_values).asList()
+            .minus("default")
+        val failures = mutableListOf<Pair<String, String>>()
+        for (locale in locales) {
+            setLocale(locale)
+            for (resId in intArrayOf(
+                R.string.dialog_contrib_reminder_remove_limitation,
+                R.string.dialog_contrib_text_1,
+                R.string.dialog_contrib_text_2,
+                R.string.dialog_remind_rate_how_many_stars,
+                R.string.dialog_remind_rate_1,
+                R.string.plan_calendar_name,
+                R.string.calendar_permission_required,
+                R.string.description_webdav_url,
+                R.string.warning_synchronization_folder_usage,
+                R.string.onboarding_ui_title,
+                R.string.crash_dialog_title,
+                R.string.crash_reports_user_info
+            )) {
+                try {
+                    Utils.getTextWithAppName(context, resId)
+                } catch (e: Exception) {
+                    failures.add(locale to context.resources.getResourceName(resId))
+                }
             }
+        }
+        if (failures.size > 0) {
+            Assert.fail("Non-compliant resources: " + failures.joinToString()
+            )
         }
     }
 
     @Test
     fun shouldBuildTellAFriendMessage() {
         val context = ApplicationProvider.getApplicationContext<Application>()
-        setLocale(locale)
-        Utils.getTellAFriendMessage(context)
+        val locales = context.resources.getStringArray(R.array.pref_ui_language_values).asList()
+            .minus("default")
+        val failures = mutableListOf<String>()
+        for (locale in locales) {
+            setLocale(locale)
+            try {
+                Utils.getTellAFriendMessage(context)
+            } catch (e: Exception) {
+                failures.add(locale)
+            }
+        }
+        if (failures.size > 0) {
+            Assert.fail("Non-compliant resources: " + failures.joinToString()
+            )
+        }
     }
 
     @Test
     fun shouldBuildWithPhrase() {
         val context = ApplicationProvider.getApplicationContext<Application>()
-        setLocale(locale)
-        try {
-            Phrase.from(context, R.string.gdpr_consent_message)
-                .put(Utils.PLACEHOLDER_APP_NAME, context.getString(R.string.app_name))
-                .put("ad_provider", "PubNative")
-                .format()
-        } catch (e: Exception) {
+        val locales = context.resources.getStringArray(R.array.pref_ui_language_values).asList()
+            .minus("default")
+        val failures = mutableListOf<String>()
+        for (locale in locales) {
+            setLocale(locale)
+            try {
+                Phrase.from(context, R.string.gdpr_consent_message)
+                    .put(Utils.PLACEHOLDER_APP_NAME, context.getString(R.string.app_name))
+                    .put("ad_provider", "PubNative")
+                    .format()
+            } catch (e: Exception) {
+                failures.add(locale)
+            }
+        }
+        if (failures.size > 0) {
             Assert.fail(
-                String.format(
-                    "Non-compliant resource gdpr_consent_message for locale %s",
-                    locale
-                )
+                "Non-compliant resources: " + failures.joinToString()
             )
         }
     }
@@ -79,12 +101,5 @@ class AppNameLocalizationTest(private val locale: String) {
         return if (parts.size == 2) {
             parts[0] + "-r" + parts[1]
         } else locale
-    }
-
-    companion object {
-        @JvmStatic
-        @ParameterizedRobolectricTestRunner.Parameters(name = "Locale: {0}")
-        fun params() = ApplicationProvider.getApplicationContext<Application>().resources
-            .getStringArray(R.array.pref_ui_language_values).asList().minus("default")
     }
 }
