d643783013d41f2c3ea4b9adcb26b7265e1ee170
Michael Totschnig <michael@totschnig.org>
2022-01-30 13:43:24 +0100

    Try to get at possible edge case where only transfer_peer has null uuid

--- myExpenses/src/main/java/org/totschnig/myexpenses/provider/TransactionProvider.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/provider/TransactionProvider.java
@@ -1620,7 +1620,7 @@ public class TransactionProvider extends BaseTransactionProvider {
           db.beginTransaction();
           try {
             db.delete(TABLE_CHANGES, KEY_ACCOUNTID + " = ?", accountIdBindArgs);
-            c = db.query(TABLE_TRANSACTIONS, new String[]{KEY_ROWID}, KEY_UUID + " IS NULL AND ("
+            c = db.query(TABLE_TRANSACTIONS, new String[]{KEY_ROWID}, "(" + KEY_UUID + " IS NULL OR (SELECT "+ KEY_UUID + " from "+ TABLE_TRANSACTIONS +  " peer where peer." + KEY_TRANSFER_PEER + " = "+ KEY_ROWID + ") is null ) AND ("
                 + KEY_TRANSFER_PEER + " IS NULL OR " + KEY_ROWID + " < " + KEY_TRANSFER_PEER + ")", null, null, null, null);
             if (c.moveToFirst()) {
               while (!c.isAfterLast()) {
