0860bc48f518243f2b35e8e83373951c36b65ddc
Michael Totschnig <michael@totschnig.org>
2022-02-03 11:39:35 +0100

    exit on error, escape & in name

--- business/generatePaypalInvoices.sh
+++ business/generatePaypalInvoices.sh
@@ -1,5 +1,6 @@
 #!/usr/local/bin/bash
+set -e
 tail -n +2 $1 | xsv fmt -t ";" | while IFS=";" read -r DATE GROSS TX EMAIL USER VAT COUNTRY PACKAGE; do
   VAT=${VAT/-/}
-  generateInvoicePaypal.sh -g "${GROSS/,/.}" -t "$TX" -p "$PACKAGE" -c "$COUNTRY" -u "$USER" -e "${EMAIL/_/\\_}" -v "${VAT/,/.}" -d "$DATE"
+  generateInvoicePaypal.sh -g "${GROSS/,/.}" -t "$TX" -p "$PACKAGE" -c "$COUNTRY" -u "${USER/&/\\&}" -e "${EMAIL/_/\\_}" -v "${VAT/,/.}" -d "$DATE"
 done
