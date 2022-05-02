3d7774e8dd4c2c233ee8e88f4b5da63fbf20041e
Michael Totschnig <michael@totschnig.org>
2022-05-02 13:02:00 +0200

    cleanup

--- myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
@@ -110,8 +110,6 @@ public class MyApplication extends Application implements
 
   public static final String KEY_NOTIFICATION_ID = "notification_id";
 
-  public static final String CONTRIB_SECRET = "RANDOM_SECRET";
-
   private long mLastPause = 0;
 
   private boolean isLocked;
@@ -459,8 +457,7 @@ public class MyApplication extends Application implements
   }
 
   public static String[] buildEventProjection() {
-    String[] projection = new String[android.os.Build.VERSION.SDK_INT >= 16 ? 10
-        : 8];
+    String[] projection = new String[10];
     projection[0] = Events.DTSTART;
     projection[1] = Events.DTEND;
     projection[2] = Events.RRULE;
@@ -469,10 +466,8 @@ public class MyApplication extends Application implements
     projection[5] = Events.EVENT_TIMEZONE;
     projection[6] = Events.DURATION;
     projection[7] = Events.DESCRIPTION;
-    if (android.os.Build.VERSION.SDK_INT >= 16) {
-      projection[8] = Events.CUSTOM_APP_PACKAGE;
-      projection[9] = Events.CUSTOM_APP_URI;
-    }
+    projection[8] = Events.CUSTOM_APP_PACKAGE;
+    projection[9] = Events.CUSTOM_APP_URI;
     return projection;
   }
 
@@ -500,10 +495,8 @@ public class MyApplication extends Application implements
     eventValues.put(Events.EVENT_TIMEZONE, eventCursor.getString(5));
     eventValues.put(Events.DURATION, duration);
     eventValues.put(Events.DESCRIPTION, eventCursor.getString(7));
-    if (android.os.Build.VERSION.SDK_INT >= 16) {
-      eventValues.put(Events.CUSTOM_APP_PACKAGE, eventCursor.getString(8));
-      eventValues.put(Events.CUSTOM_APP_URI, eventCursor.getString(9));
-    }
+    eventValues.put(Events.CUSTOM_APP_PACKAGE, eventCursor.getString(8));
+    eventValues.put(Events.CUSTOM_APP_URI, eventCursor.getString(9));
   }
 
   private boolean insertEventAndUpdatePlan(ContentValues eventValues,
