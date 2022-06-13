78d20d4c7e5c235ac8dc99bc9b591dcfcaf49dd9
Michael Totschnig <michael@totschnig.org>
2022-06-13 17:45:21 +0200

    Fixes #765

--- webui_css_sources/tailwind.config.js
+++ webui_css_sources/tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  content: {
-    preserveHtmlElements: false,
-    content: [
-      '../webui/src/main/assets/form.html'
-    ]
-  },
+  content: [
+        '../webui/src/main/assets/form.html'
+      ],
   safelist: [
       {
             pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/,
       }
     ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
