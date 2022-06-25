fffba53d4207619d73f96b963f2031c18bce1bdc
Michael Totschnig <michael@totschnig.org>
2022-06-25 20:38:40 +0200

    reuse key

--- webui/src/main/java/org/totschnig/webui/WebInputService.kt
+++ webui/src/main/java/org/totschnig/webui/WebInputService.kt
@@ -239,7 +239,7 @@ class WebInputService : Service(), IWebInputService {
                                     ${i18nJson("dialog_confirm_discard_changes")},
                                     ${i18nJson("menu_clone_transaction")},
                                     ${i18nJson("menu_delete")},
-,                                   ${i18nJson("no_expenses")},
+                                    ${i18nJson("no_expenses")},
                                     ${i18nJsonPlurals("warning_delete_transaction")}
                                     };
                                 """.trimIndent(), ContentType.Text.JavaScript)
