a9701f77729827332d0aab09fc74a3425f9bcbca
Michael Totschnig <michael@totschnig.org>
2022-07-01 16:44:59 +0200

    move work to background task

--- myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/ExportViewModel.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/ExportViewModel.kt
@@ -4,10 +4,12 @@ import android.app.Application
 import android.net.Uri
 import android.os.Bundle
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.totschnig.myexpenses.MyApplication
 import org.totschnig.myexpenses.R
 import org.totschnig.myexpenses.export.CsvExporter
@@ -57,157 +59,165 @@ class ExportViewModel(application: Application) : ContentResolvingAndroidViewMod
     val result: StateFlow<Pair<ExportFormat, List<Uri>>?> = _result
 
     fun startExport(args: Bundle) {
-        val format: ExportFormat = args.getSerializable(KEY_FORMAT) as ExportFormat
-        _result.update {
-            format to buildList {
+        viewModelScope.launch(coroutineDispatcher) {
+            val format: ExportFormat = args.getSerializable(KEY_FORMAT) as ExportFormat
+            _result.update {
+                format to buildList {
 
-                val application = getApplication<MyApplication>()
-                val deleteP = args.getBoolean(KEY_DELETE_P)
-                val notYetExportedP = args.getBoolean(KEY_NOT_YET_EXPORTED_P)
-                val mergeP = args.getBoolean(KEY_MERGE_P)
-                val dateFormat = args.getString(KEY_DATE_FORMAT)!!
-                val decimalSeparator: Char = args.getChar(KEY_DECIMAL_SEPARATOR)
-                val accountId = args.getLong(DatabaseConstants.KEY_ROWID)
-                val currency = args.getString(DatabaseConstants.KEY_CURRENCY)
-                val encoding = args.getString(KEY_ENCODING)!!
-                val handleDelete = args.getInt(KEY_EXPORT_HANDLE_DELETED)
-                val filter =
-                    WhereFilter(args.getParcelableArrayList(BaseTransactionList.KEY_FILTER)!!)
-                val fileName = args.getString(KEY_FILE_NAME)!!
-                val delimiter = args.getChar(KEY_DELIMITER)
+                    val application = getApplication<MyApplication>()
+                    val deleteP = args.getBoolean(KEY_DELETE_P)
+                    val notYetExportedP = args.getBoolean(KEY_NOT_YET_EXPORTED_P)
+                    val mergeP = args.getBoolean(KEY_MERGE_P)
+                    val dateFormat = args.getString(KEY_DATE_FORMAT)!!
+                    val decimalSeparator: Char = args.getChar(KEY_DECIMAL_SEPARATOR)
+                    val accountId = args.getLong(DatabaseConstants.KEY_ROWID)
+                    val currency = args.getString(DatabaseConstants.KEY_CURRENCY)
+                    val encoding = args.getString(KEY_ENCODING)!!
+                    val handleDelete = args.getInt(KEY_EXPORT_HANDLE_DELETED)
+                    val filter =
+                        WhereFilter(args.getParcelableArrayList(BaseTransactionList.KEY_FILTER)!!)
+                    val fileName = args.getString(KEY_FILE_NAME)!!
+                    val delimiter = args.getChar(KEY_DELIMITER)
 
-                val accountIds: Array<Long> = if (accountId > 0L) {
-                    arrayOf(accountId)
-                } else {
-                    var selection: String? = null
-                    var selectionArgs: Array<String>? = null
-                    if (currency != null) {
-                        selection = DatabaseConstants.KEY_CURRENCY + " = ?"
-                        selectionArgs = arrayOf(currency)
-                    }
-                    application.contentResolver.query(
-                        TransactionProvider.ACCOUNTS_URI,
-                        arrayOf(DatabaseConstants.KEY_ROWID),
-                        selection,
-                        selectionArgs,
-                        null
-                    )?.use {
-                        DbUtils.getLongArrayFromCursor(it, DatabaseConstants.KEY_ROWID)
-                    } ?: throw IOException("Cursor was null")
-                }
-                var account: Account?
-                val appDir = AppDirHelper.getAppDir(application)
-                val context = ContextHelper.wrap(
-                    application,
-                    application.appComponent.userLocaleProvider().getUserPreferredLocale()
-                )
-                if (appDir == null) {
-                    publishProgress(context.getString(R.string.external_storage_unavailable))
-                    return
-                }
-                val oneFile = accountIds.size == 1 || mergeP
-                val destDir = if (oneFile) {
-                    appDir
-                } else {
-                    AppDirHelper.newDirectory(appDir, fileName)
-                }
-                if (destDir != null) {
-                    val successfullyExported = ArrayList<Account>()
-                    val simpleDateFormat = SimpleDateFormat("yyyMMdd-HHmmss", Locale.US)
-                    val now = Date()
-                    for (i in accountIds.indices) {
-                        account = Account.getInstanceFromDb(accountIds[i])
-                        if (account == null) continue
-                        publishProgress(account.label + " ...")
-                        try {
-                            val append = mergeP && i > 0
-                            val fileNameForAccount = if (oneFile) fileName else String.format(
-                                "%s-%s", Utils.escapeForFileName(account.label),
-                                simpleDateFormat.format(now)
-                            )
-                            val exporter = when (format) {
-                                ExportFormat.CSV -> CsvExporter(
-                                    account,
-                                    filter,
-                                    notYetExportedP,
-                                    dateFormat,
-                                    decimalSeparator,
-                                    encoding,
-                                    !append,
-                                    delimiter,
-                                    mergeP
-                                )
-                                ExportFormat.QIF -> QifExporter(
-                                    account,
-                                    filter,
-                                    notYetExportedP,
-                                    dateFormat,
-                                    decimalSeparator,
-                                    encoding
-                                )
-                                ExportFormat.JSON -> JSONExporter(
-                                    account,
-                                    filter,
-                                    notYetExportedP,
-                                    dateFormat,
-                                    decimalSeparator,
-                                    encoding,
-                                    gson
-                                )
-                            }
-                            val result = exporter.export(context, lazy {
-                                Result.success(
-                                    AppDirHelper.buildFile(
-                                        destDir, fileNameForAccount, format.mimeType,
-                                        append, true
-                                    ) ?: throw createFileFailure(context, destDir, fileName)
-                                )
-                            }, append)
-                            result.onSuccess {
-                                if (!append && prefHandler.getBoolean(PrefKey.PERFORM_SHARE,false)) {
-                                    add(it)
-                                }
-                                successfullyExported.add(account)
-                                publishProgress(
-                                    "..." + context.getString(
-                                        R.string.export_sdcard_success,
-                                        FileUtils.getPath(context, it)
-                                    )
-                                )
-                            }.onFailure {
-                                publishProgress("... " + it.message)
-                            }
-                        } catch (e: IOException) {
-                            publishProgress(
-                                "... " + context.getString(
-                                    R.string.export_sdcard_failure,
-                                    appDir.name,
-                                    e.message
-                                )
-                            )
+                    val accountIds: Array<Long> = if (accountId > 0L) {
+                        arrayOf(accountId)
+                    } else {
+                        var selection: String? = null
+                        var selectionArgs: Array<String>? = null
+                        if (currency != null) {
+                            selection = DatabaseConstants.KEY_CURRENCY + " = ?"
+                            selectionArgs = arrayOf(currency)
                         }
+                        application.contentResolver.query(
+                            TransactionProvider.ACCOUNTS_URI,
+                            arrayOf(DatabaseConstants.KEY_ROWID),
+                            selection,
+                            selectionArgs,
+                            null
+                        )?.use {
+                            DbUtils.getLongArrayFromCursor(it, DatabaseConstants.KEY_ROWID)
+                        } ?: throw IOException("Cursor was null")
                     }
-                    for (a in successfullyExported) {
-                        try {
-                            if (deleteP) {
-                                check(!a.isSealed) { "Trying to reset account that is sealed" }
-                                a.reset(filter, handleDelete, fileName)
-                            } else {
-                                a.markAsExported(filter)
-                            }
-                        } catch (e: Exception) {
-                            publishProgress("ERROR: " + e.message)
-                            CrashHandler.report(e)
-                        }
-                    }
-                } else {
-                    publishProgress(
-                        "ERROR: " + createFileFailure(
-                            context,
-                            appDir,
-                            fileName
-                        ).message
+                    var account: Account?
+                    val appDir = AppDirHelper.getAppDir(application)
+                    val context = ContextHelper.wrap(
+                        application,
+                        application.appComponent.userLocaleProvider().getUserPreferredLocale()
                     )
+                    if (appDir == null) {
+                        publishProgress(context.getString(R.string.external_storage_unavailable))
+
+                    } else {
+                        val oneFile = accountIds.size == 1 || mergeP
+                        val destDir = if (oneFile) {
+                            appDir
+                        } else {
+                            AppDirHelper.newDirectory(appDir, fileName)
+                        }
+                        if (destDir != null) {
+                            val successfullyExported = ArrayList<Account>()
+                            val simpleDateFormat = SimpleDateFormat("yyyMMdd-HHmmss", Locale.US)
+                            val now = Date()
+                            for (i in accountIds.indices) {
+                                account = Account.getInstanceFromDb(accountIds[i])
+                                if (account == null) continue
+                                publishProgress(account.label + " ...")
+                                try {
+                                    val append = mergeP && i > 0
+                                    val fileNameForAccount =
+                                        if (oneFile) fileName else String.format(
+                                            "%s-%s", Utils.escapeForFileName(account.label),
+                                            simpleDateFormat.format(now)
+                                        )
+                                    val exporter = when (format) {
+                                        ExportFormat.CSV -> CsvExporter(
+                                            account,
+                                            filter,
+                                            notYetExportedP,
+                                            dateFormat,
+                                            decimalSeparator,
+                                            encoding,
+                                            !append,
+                                            delimiter,
+                                            mergeP
+                                        )
+                                        ExportFormat.QIF -> QifExporter(
+                                            account,
+                                            filter,
+                                            notYetExportedP,
+                                            dateFormat,
+                                            decimalSeparator,
+                                            encoding
+                                        )
+                                        ExportFormat.JSON -> JSONExporter(
+                                            account,
+                                            filter,
+                                            notYetExportedP,
+                                            dateFormat,
+                                            decimalSeparator,
+                                            encoding,
+                                            gson
+                                        )
+                                    }
+                                    val result = exporter.export(context, lazy {
+                                        Result.success(
+                                            AppDirHelper.buildFile(
+                                                destDir, fileNameForAccount, format.mimeType,
+                                                append, true
+                                            ) ?: throw createFileFailure(context, destDir, fileName)
+                                        )
+                                    }, append)
+                                    result.onSuccess {
+                                        if (!append && prefHandler.getBoolean(
+                                                PrefKey.PERFORM_SHARE,
+                                                false
+                                            )
+                                        ) {
+                                            add(it)
+                                        }
+                                        successfullyExported.add(account)
+                                        publishProgress(
+                                            "..." + context.getString(
+                                                R.string.export_sdcard_success,
+                                                FileUtils.getPath(context, it)
+                                            )
+                                        )
+                                    }.onFailure {
+                                        publishProgress("... " + it.message)
+                                    }
+                                } catch (e: IOException) {
+                                    publishProgress(
+                                        "... " + context.getString(
+                                            R.string.export_sdcard_failure,
+                                            appDir.name,
+                                            e.message
+                                        )
+                                    )
+                                }
+                            }
+                            for (a in successfullyExported) {
+                                try {
+                                    if (deleteP) {
+                                        check(!a.isSealed) { "Trying to reset account that is sealed" }
+                                        a.reset(filter, handleDelete, fileName)
+                                    } else {
+                                        a.markAsExported(filter)
+                                    }
+                                } catch (e: Exception) {
+                                    publishProgress("ERROR: " + e.message)
+                                    CrashHandler.report(e)
+                                }
+                            }
+                        } else {
+                            publishProgress(
+                                "ERROR: " + createFileFailure(
+                                    context,
+                                    appDir,
+                                    fileName
+                                ).message
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -238,15 +248,17 @@ class ExportViewModel(application: Application) : ContentResolvingAndroidViewMod
 
     fun hasExported(accountId: Long) = liveData(coroutineDispatcher) {
         val (selection, selectionArgs) =
-        if (accountId != Account.HOME_AGGREGATE_ID) {
-            if (accountId < 0L) {
-                //aggregate account
-                val aa = AggregateAccount.getInstanceFromDb(accountId)
-                "${DatabaseConstants.KEY_ACCOUNTID} IN (SELECT ${DatabaseConstants.KEY_ROWID} FROM ${DatabaseConstants.TABLE_ACCOUNTS} WHERE ${DatabaseConstants.KEY_CURRENCY} = ?)" to arrayOf(aa.currencyUnit.code)
-            } else {
-                DatabaseConstants.KEY_ACCOUNTID + " = ?" to arrayOf(accountId.toString())
-            }
-        } else null to null
+            if (accountId != Account.HOME_AGGREGATE_ID) {
+                if (accountId < 0L) {
+                    //aggregate account
+                    val aa = AggregateAccount.getInstanceFromDb(accountId)
+                    "${DatabaseConstants.KEY_ACCOUNTID} IN (SELECT ${DatabaseConstants.KEY_ROWID} FROM ${DatabaseConstants.TABLE_ACCOUNTS} WHERE ${DatabaseConstants.KEY_CURRENCY} = ?)" to arrayOf(
+                        aa.currencyUnit.code
+                    )
+                } else {
+                    DatabaseConstants.KEY_ACCOUNTID + " = ?" to arrayOf(accountId.toString())
+                }
+            } else null to null
         contentResolver.query(
             Transaction.CONTENT_URI,
             arrayOf("max(" + DatabaseConstants.KEY_STATUS + ")"),
