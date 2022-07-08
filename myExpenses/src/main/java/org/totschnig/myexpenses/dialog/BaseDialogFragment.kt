3aeafd5dfe0eb17a261a023f676dd784b4ee05ef
Michael Totschnig <michael@totschnig.org>
2022-07-08 10:19:03 +0200

    prevent android.view.WindowManager$BadTokenException

--- myExpenses/src/main/java/org/totschnig/myexpenses/dialog/BaseDialogFragment.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/dialog/BaseDialogFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.viewbinding.ViewBinding
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import org.totschnig.myexpenses.MyApplication
@@ -47,13 +47,20 @@ abstract class BaseDialogFragment : DialogFragment() {
     }
 
     fun report(e: IllegalStateException?) {
-        val activity = activity
-        if (activity == null) {
+        activity?.also {
+            Timber.w("Activity is finishing?: %b", it.isFinishing)
+        } ?: run {
             Timber.w("Activity is null")
-        } else {
-            Timber.w("Activity is finishing?: %b", activity.isFinishing)
         }
         CrashHandler.report(e)
+    }
+
+    override fun show(manager: FragmentManager, tag: String?) {
+        if (activity?.isFinishing == false) {
+            super.show(manager, tag)
+        } else {
+            report(IllegalStateException())
+        }
     }
 
     override fun dismiss() {
