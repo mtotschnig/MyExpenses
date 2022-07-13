bbc557b9a84c3025f19ed29d5caedef2fca1a8a3
Michael Totschnig <michael@totschnig.org>
2022-07-13 19:00:28 +0200

    moved test to connected, because it failed as unitTest due to undebuggable reasons

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/SelectedSumTest.kt
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/SelectedSumTest.kt
@@ -1,42 +1,32 @@
-package org.totschnig.myexpenses.activity
+package org.totschnig.myexpenses.test.espresso
 
 import android.content.Intent
 import android.database.Cursor
-import android.view.View
-import android.widget.AdapterView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.longClick
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.containsString
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.totschnig.myexpenses.R
+import org.totschnig.myexpenses.activity.MyExpenses
 import org.totschnig.myexpenses.model.Account
 import org.totschnig.myexpenses.model.AccountType
 import org.totschnig.myexpenses.model.CurrencyUnit
 import org.totschnig.myexpenses.model.Money
 import org.totschnig.myexpenses.model.Transaction
 import org.totschnig.myexpenses.provider.DatabaseConstants
+import org.totschnig.myexpenses.testutils.BaseUiTest
 import java.util.*
 
-@RunWith(AndroidJUnit4::class)
-class SelectedSumTest {
-    private lateinit var scenario: ActivityScenario<DistributionActivity>
+class SelectedSumTest : BaseUiTest<MyExpenses>() {
+    private lateinit var activityScenario: ActivityScenario<MyExpenses>
 
     @Before
     fun fixture() {
@@ -50,8 +40,8 @@ class SelectedSumTest {
         for (i in 0 until times) {
             op0.saveAsNew()
         }
-        scenario = ActivityScenario.launch(
-                Intent(InstrumentationRegistry.getInstrumentation().context, MyExpenses::class.java).apply {
+        activityScenario = ActivityScenario.launch(
+                Intent(targetContext, MyExpenses::class.java).apply {
                     putExtra(DatabaseConstants.KEY_ROWID, account.id)
                 })
     }
@@ -72,23 +62,13 @@ class SelectedSumTest {
             .check(matches(withText(containsString(String.format("%.2f", sum.toFloat())))))
     }
 
-    private val wrappedList: Matcher<View>
-        get() = Matchers.allOf(
-                isAssignableFrom(AdapterView::class.java),
-                isDescendantOfA(withId(R.id.list)),
-                isDisplayed()
-        )
-
-    private fun openCab() {
-        onData(CoreMatchers.`is`(instanceOf(Cursor::class.java)))
-                .inAdapterView(wrappedList)
-                .atPosition(1)
-                .perform(longClick())
-    }
     private fun select(position: Int) {
         onData(CoreMatchers.`is`(instanceOf(Cursor::class.java)))
             .inAdapterView(wrappedList)
             .atPosition(position)
             .perform(click())
     }
+
+    override val testScenario: ActivityScenario<MyExpenses>
+        get() = activityScenario
 }
