549de3ba0057aa5c25481975fe737d84530fef10
Michael Totschnig <michael@totschnig.org>
2022-08-10 21:59:01 +0200

    Appcompat 1.5.0 Release

--- myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/data/VersionInfo.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/data/VersionInfo.kt
@@ -72,6 +72,9 @@ class VersionInfo(val code: Int, val name: String) : Parcelable {
                 "${t(R.string.customize)}: ${ctx.getString(R.string.export_to_format, "CSV")}",
                 "${ctx.getString(R.string.export_to_format, "JSON")}: 2.0",
             )
+            "344" -> arrayOf(
+                "${t(R.string.whats_new_344)}: 2.0",
+            )
             else -> {
                 val resId = res.getIdentifier(
                     "whats_new_$nameCondensed",
