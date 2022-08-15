fc7193a9721a6b003cec3f4e61569e769d87f1c2
Michael Totschnig <michael@totschnig.org>
2022-08-15 16:23:06 +0200

    fixes #1018

--- myExpenses/src/main/java/org/totschnig/myexpenses/ui/ExchangeRateEdit.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/ui/ExchangeRateEdit.java
@@ -7,6 +7,11 @@ import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.LifecycleOwner;
+
 import com.google.android.material.snackbar.Snackbar;
 
 import org.totschnig.myexpenses.MyApplication;
@@ -20,13 +25,8 @@ import org.totschnig.myexpenses.util.crashreporting.CrashHandler;
 import org.totschnig.myexpenses.viewmodel.ExchangeRateViewModel;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.math.MathContext;
 import java.time.LocalDate;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.lifecycle.LifecycleOwner;
 
 public class ExchangeRateEdit extends ConstraintLayout {
 
@@ -34,7 +34,7 @@ public class ExchangeRateEdit extends ConstraintLayout {
     void afterExchangeRateChanged(BigDecimal rate, BigDecimal inverse);
   }
 
-  private static final int EXCHANGE_RATE_FRACTION_DIGITS = 5;
+  private static final int EXCHANGE_RATE_FRACTION_DIGITS = 20;
   private static final BigDecimal nullValue = new BigDecimal(0);
 
   AmountEditText rate1Edit;
@@ -123,8 +123,8 @@ public class ExchangeRateEdit extends ConstraintLayout {
     if (amount1 != null && amount2 != null && amount1.compareTo(nullValue) != 0 && amount2.compareTo(nullValue) != 0) {
       final BigDecimal a2Abs = amount2.abs();
       final BigDecimal a1Abs = amount1.abs();
-      exchangeRate = a2Abs.divide(a1Abs, EXCHANGE_RATE_FRACTION_DIGITS, RoundingMode.HALF_EVEN);
-      inverseExchangeRate = a1Abs.divide(a2Abs, EXCHANGE_RATE_FRACTION_DIGITS, RoundingMode.HALF_EVEN);
+      exchangeRate = a2Abs.divide(a1Abs, MathContext.DECIMAL32);
+      inverseExchangeRate = a1Abs.divide(a2Abs, MathContext.DECIMAL32);
     } else {
       exchangeRate = nullValue;
       inverseExchangeRate = nullValue;
@@ -219,7 +219,7 @@ public class ExchangeRateEdit extends ConstraintLayout {
 
   private BigDecimal calculateInverse(BigDecimal input) {
     return input.compareTo(nullValue) != 0 ?
-        new BigDecimal(1).divide(input, EXCHANGE_RATE_FRACTION_DIGITS, RoundingMode.HALF_EVEN) :
+        new BigDecimal(1).divide(input, MathContext.DECIMAL32) :
         nullValue;
   }
 
