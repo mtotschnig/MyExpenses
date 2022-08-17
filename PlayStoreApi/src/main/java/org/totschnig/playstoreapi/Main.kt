873e281695b1362f2d6c4dad056bc14aba5efc2e
Michael Totschnig <michael@totschnig.org>
2022-08-17 07:59:31 +0200

    play store api used for new addons

--- PlayStoreApi/src/main/java/org/totschnig/playstoreapi/Main.kt
+++ PlayStoreApi/src/main/java/org/totschnig/playstoreapi/Main.kt
@@ -14,9 +14,7 @@ class Main {
 
         private const val PACKAGE_NAME = "org.totschnig.myexpenses"
 
-        private const val SKU = "categorytree" //sku_professional_monthly sku_professional_yearly sku_extended2professional_yearly
-
-        private val categorytree: Map<String, Pair<String, String>> = mapOf(
+        private val SKU_category = "categorytree" to mapOf(
             "en-US" to ("Category Tree" to "Arbitrarily deep category hierarchy"),
             "bg" to ("Дърво на категориите" to "Произволно дълбока йерархия на категориите"),
             "de-DE" to ("Kategorienbaum" to "Beliebig tiefe Kategorienhierarchie"),
@@ -32,6 +30,75 @@ class Main {
             "zh-CN" to ("类别树" to "任意深的类别层次结构")
         )
 
+        private val SKU_csv_import = "csvimport" to mapOf(
+            "en-US" to ("CSV import" to "Sophisticated, customizable CSV import (spreadsheet applications)"),
+            "ar"    to ("إستيراد ملف CSV" to "استيراد CSV متطور وقابل للتخصيص (تطبيقات جداول البيانات)"),
+            "bg"    to ("Импорт от CSV" to "Удобен, персонализируем импорт от CSV файл (приложения за електронни таблици)"),
+            "ca"    to ("Importació CSV" to " Importació sofisticada i personalitzable CSV (aplicacions de full de càlcul)"),
+            "cs-CZ" to ("Import CSV souboru" to "Propracovaný a přizpůsobitelný import CSV (tabulkové aplikace)"),
+            "da-DK" to ("CSV import" to "Sofistikeret, brugerdefinerbar CSV import (regnearksprogrammer)"),
+            "de-DE" to ("CSV Import" to "Ausgefeilter, anpassbarer CSV-Import (Tabellenkalkulationsanwendungen)"),
+            "el-gr" to ("Εισαγωγή CSV" to "Εκλεπτυσμένη, προσαρμόσιμη εισαγωγή CSV (εφαρμογές λογιστικών φύλλων)"),
+            "es-ES" to ("Importar desde CSV" to "Una sofisticada y personalizable importación a CSV (para aplicaciones de hojas de cálculo)"),
+            "eu-ES" to ("CSV inportazioa" to "CSV inportazio sofistikatu eta pertsonalizagarria (kalkulu orrien aplikazioak)"),
+            "fr-FR" to ("Import CSV" to "Import CSV sophistiqué et personnalisable (applications tableur)"),
+            "hr"    to ("CSV uvoz" to "Sofisticiran, prilagodljiv CSV uvoz (programi proračunskih tablica)"),
+            "hu-HU" to ("CSV-importálás" to "Kifinomult, testreszabható CSV import (táblázatkezelő alkalmazások)"),
+            "it-IT" to ("Importa CSV" to "Importazione CSV sofisticata e personalizzabile (applicazioni per fogli di calcolo)"),
+            "iw-IL" to ("ייבוא CSV" to "מתוחכם וניתן להתאמה אישית  יבוא CSV   (יישומי גיליון אלקטרוני)"),
+            "ja-JP" to ("CSV インポート" to "洗練されたカスタマイズ可能な CSV インポート (スプレッドシート アプリケーション)"),
+            "km-KH" to ("ការនាំចូលស៊ីអេសវី" to "ទំនើប ប្ដូរតាមបំណង ការនាំចូល CSV (កម្មវិធីសៀវភៅបញ្ជី)"),
+            "kn-IN" to ("ಸಿಎಸ್ವಿ ಆಮದು" to "ಅತ್ಯಾಧುನಿಕ, ಗ್ರಾಹಕೀಯಗೊಳಿಸಬಹುದಾದ CSV ಆಮದು (ಸ್ಪ್ರೆಡ್‌ಶೀಟ್ ಅಪ್ಲಿಕೇಶನ್‌ಗಳು)"),
+            "ko-KR" to ("CSV 가져오기" to "정교하고 사용자 정의 가능한 CSV 가져 오기 (스프레드 시트 응용 프로그램)"),
+            "ms"    to ("Import CSV" to "Import CSV import (spreadsheet applications) yang canggih dan boleh disuaikan."),
+            "pl-PL" to ("Import CSV" to "Wyrafinowany, dostosowywany import CSV (aplikacje do arkuszy kalkulacyjnych)"),
+            "pt-PT" to (" Importação CSV" to "Importação de CSV sofisticada e personalizável (aplicativos de planilha)"),
+            "ro"    to ("CSV import" to "Import sofisticat și personalizabil CSV (aplicații de foi de calcul)"),
+            "ru-RU" to ("Импорт данных в формате CSV" to "Сложный, настраиваемый импорт CSV (приложения для работы с электронными таблицами)"),
+            "si-LK" to ("අ.වි.අ. (CSV) ආයාත කරන්න" to "නවීන, අභිරුචිකරණය කළ හැකි CSV ආනයනය (පැතුරුම්පත් යෙදුම්)"),
+            "ta-IN" to ("CSV இறக்குமதி" to "அதிநவீன, தனிப்பயனாக்கக்கூடிய CSV இறக்குமதி (விரிதாள் செயலிகள்)"),
+            "te-IN" to ("CSV దిగుమతి" to "అధునాతన, అనుకూలీకరించదగిన CSV దిగుమతి (స్ప్రెడ్‌షీట్ అనువర్తనాలు)"),
+            "tr-TR" to ("CSV içe aktar" to "Gelişmiş, özelleştirilebilen CSV içe aktarma (tablolama uygulamaları)"),
+            "vi"    to ("Nhập CSV" to "Tinh vi, có thể tùy chỉnh  Nhập CSV  (ứng dụng bảng tính)"),
+            "zh-CN" to ("导入 CSV" to "可自定义复杂的CSV导入文件（电子表格应用程序）"),
+            "zh-TW" to ("以CSV的檔案型式輸入" to "可自定義復雜的CSV導入文件（電子表格應用程序）"),
+        )
+
+        private val SKU_csv_distribution= "distribution" to mapOf(
+            "en-US" to ("Pie chart" to "Visualize the statistical distribution of transactions per category over different periods (years, months, weeks, days)"),
+            "ar"    to ("مخطط دائري" to "تصور التوزيع الإحصائي للمعاملات لكل فئة عبر فترات مختلفة (سنوات ، أشهر ، أسابيع ، أيام)"),
+            "bg"    to ("Графично разпределение" to "Визуализирате статистическото разпределение на транзакциите по категории за различни периоди (година, месец, седмица, ден)"),
+            //"ca"    to ("Gràfic de sectors" to ""),
+            //"cs-CZ" to ("Koláčový graf" to ""),
+             //"da-DK" to ("" to ""),
+            "de-DE" to ("Kreisdiagramm" to "Visualisieren Sie die statistische Verteilung der Buchungen pro Kategorie über verschiedene Perioden (Jahre, Monate, Wochen, Tage)"),
+            //"el-gr" to ("Γράφημα πίτας" to ""),
+            "es-ES" to ("Gráfico de sectores" to "Visualice la distribución estadística de transacciones por categoría entre diferentes periodos (años, meses, semanas, días)"),
+            "eu-ES" to ("Tarta diagrama" to "ikusi transakzioen banaketa estatistikoa kategoria bakoitzeko aldi desberdinetan zehar (urteak, hilabeteak, asteak, egunak)"),
+            "fr-FR" to ("Diagramme circulaire" to "Visualisez la distribution statistique des opérations par catégorie sur différentes périodes (années, mois, semaines, jours)"),
+            "hr"    to ("Kružni dijagram" to "Vizualiziraj statističku raspodjelu transakcija po kategorijama kroz različita razdoblja (godine, mjeseci, tjedni, dani)"),
+            "hu-HU" to ("Tortadiagram" to "A tranzakciók kategóriánkénti statisztikai eloszlásának szemléltetése különböző időszakokban (évek, hónapok, hetek, napok)"),
+            "it-IT" to ("Grafico a torta" to "Visualizza la distribuzione statistica delle transazioni per categoria su periodi diversi (anni, mesi, settimane, giorni)"),
+            "iw-IL" to ("תרשים עוגה" to "דמיינו את ההתפלגות הסטטיסטית של עסקאות לקטגוריה על פני תקופות שונות (שנים, חודשים, שבועות, ימים)"),
+            "ja-JP" to ("円グラフ" to "異なる期間にわたる分類ごとの取引の統計的分布を視覚化します (年、月、週、日)"),
+            //"km-KH" to ("" to ""),
+            "kn-IN" to ("ಪೈ ಚಾರ್ಟ್" to "ವಿವಿಧ ಅವಧಿಗಳಲ್ಲಿ (ವರ್ಷಗಳು, ತಿಂಗಳುಗಳು, ವಾರಗಳು, ದಿನಗಳು) ಪ್ರತಿ ವರ್ಗದ ವಹಿವಾಟಿನ ಸಂಖ್ಯಾಶಾಸ್ತ್ರೀಯ ವಿತರಣೆಯನ್ನು ದೃಶ್ಯೀಕರಿಸಿ"),
+            //"ko-KR" to ("" to ""),
+            "ms"    to ("Carta pai" to "Visualikan agihan berstatistik bagi transaksi per kategori mengikut tempoh berbeza (tahun, bulan, minggu atau hari)"),
+            "pl-PL" to ("Wykres kołowy" to "Wizualizuj rozkład statystyczny transakcji według kategorii w różnych okresach (lata, miesiące, tygodnie, dni)"),
+            "pt-PT" to ("Gráfico de pizza" to "visualize a distribuição estatística de transações por categoria em diferentes períodos (anos, meses, semanas, dias)"),
+            //"ro"    to ("" to ""),
+            "ru-RU" to ("Круговая диаграмма" to "визуализация статистического распределения транзакций по категориям за разные периоды (годы, месяцы, недели, дни)"),
+            //"si-LK" to ("" to ""),
+            "ta-IN" to ("பை விளக்கப்படம்" to "வெவ்வேறு காலகட்டங்களில் (ஆண்டுகள், மாதங்கள்,  வாரங்கள், நாட்கள்) ஒவ்வொரு வகையின் பரிவர்த்தனைகளின் புள்ளிவிவர பகிர்வை காட்சிப்படுத்தலாம்"),
+            "te-IN" to ("పై చార్ట్" to "వివిధ కాలాలలో (సంవత్సరాలు, నెలలు, వారాలు, రోజులు) ప్రతి వర్గానికి లావాదేవీల గణాంక పంపిణీని దృశ్యమానం చేయండి"),
+            "tr-TR" to ("Dilim grafik" to "değişik zaman dilimleri (sene, ay, hafta, gün) ile kategori başına muamelelerin istatistiki dağılımını görüntüleyin"),
+            //"vi"    to ("Biểu đồ quạt" to ""),
+            "zh-CN" to ("圆饼统计图" to "可视化不同时期（年、月、周、天）中，每个类别的交易统计分布"),
+            "zh-TW" to ("圓餅統計圖" to "可視化不同時期（年、月、週、天）中，每個類別的交易統計分佈")
+
+        )
+
         const val SERVICE_ACCOUNT_EMAIL =
             "fastlane@api-5950718857839288276-239934.iam.gserviceaccount.com"
 
@@ -39,6 +106,7 @@ class Main {
 
         @JvmStatic
         fun main(args: Array<String>) {
+            val sku = SKU_csv_distribution
             try {
                 // Create the API service.
                 val service: AndroidPublisher = AndroidPublisherHelper.init()
@@ -46,11 +114,11 @@ class Main {
 
                 val content = InAppProduct()
                     .setPackageName(PACKAGE_NAME)
-                    .setSku(SKU)
+                    .setSku(sku.first)
                     .setPurchaseType("managedUser")
                     .setListings(
                         buildMap {
-                            categorytree.forEach {
+                            sku.second.forEach {
                                 put(
                                     it.key,
                                     InAppProductListing().setTitle(it.value.first)
@@ -60,7 +128,7 @@ class Main {
                         }
                     )
 
-                inappproducts.patch(PACKAGE_NAME, SKU, content).execute()
+                inappproducts.patch(PACKAGE_NAME, sku.first, content).execute()
 
             } catch (ex: IOException) {
                 log.error("Exception was thrown while updating listing", ex)
