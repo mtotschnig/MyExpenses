4b767af83ceb431a1ab851429c0c430291c7f004
Michael Totschnig <michael@totschnig.org>
2022-08-18 16:46:18 +0200

    adjust prices add link to paypal button for contrib features

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/Package.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/Package.kt
@@ -37,13 +37,15 @@ sealed class Package(val defaultPrice: Long) : Parcelable {
 
     @Parcelize
     @Keep
-    object Contrib : Package(1290)
+    object Contrib : Package(880)
+
     @Parcelize
     @Keep
-    object Upgrade : Package(430)
+    object Upgrade : Package(300)
+
     @Parcelize
     @Keep
-    object Extended : Package(1650)
+    object Extended : Package(1100)
 }
 
 @Suppress("ClassName")
@@ -52,15 +54,19 @@ sealed class ProfessionalPackage(defaultPrice: Long, val duration: Int) : Packag
     @Parcelize
     @Keep
     object Professional_1 : ProfessionalPackage(100, 1)
+
     @Parcelize
     @Keep
     object Professional_6 : ProfessionalPackage(500, 6)
+
     @Parcelize
     @Keep
     object Professional_12 : ProfessionalPackage(800, 12)
+
     @Parcelize
     @Keep
     object Professional_24 : ProfessionalPackage(1500, 24)
+
     @Parcelize
     @Keep
     object Amazon : ProfessionalPackage(900, 0)
@@ -104,53 +110,73 @@ sealed class ProfessionalPackage(defaultPrice: Long, val duration: Int) : Packag
 }
 
 @Keep
-sealed class AddOnPackage(val feature: ContribFeature, defaultPrice: Long = 430) : Package(defaultPrice) {
+sealed class AddOnPackage(
+    val feature: ContribFeature,
+    val isPro: Boolean = true
+) : Package(if (isPro) 430 else 270) {
     override val optionName = "AddOn"
     val sku: String
         get() = this::class.simpleName!!.lowercase(Locale.ROOT)
 
     override fun payPalButtonId(isSandBox: Boolean) =
-        if (isSandBox) "9VF4Z9KSLHXZN" else "FNEEWJWU5YJ44"
+        when {
+            isSandBox -> if (isPro) "9VF4Z9KSLHXZN" else "UAWN7XUQNZ5PS"
+            isPro -> "FNEEWJWU5YJ44"
+            else -> "48RQY4SKUHTAQ"
+        }
 
     @Parcelize
     @Keep
     object SplitTemplate : AddOnPackage(ContribFeature.SPLIT_TEMPLATE)
+
     @Parcelize
     @Keep
     object History : AddOnPackage(ContribFeature.HISTORY)
+
     @Parcelize
     @Keep
     object Budget : AddOnPackage(ContribFeature.BUDGET)
+
     @Parcelize
     @Keep
     object Ocr : AddOnPackage(ContribFeature.OCR)
+
     @Parcelize
     @Keep
     object WebUi : AddOnPackage(ContribFeature.WEB_UI)
+
     @Parcelize
     @Keep
     object CategoryTree : AddOnPackage(ContribFeature.CATEGORY_TREE)
+
     @Parcelize
     @Keep
-    object AccountsUnlimited : AddOnPackage(ContribFeature.ACCOUNTS_UNLIMITED)
+    object AccountsUnlimited : AddOnPackage(ContribFeature.ACCOUNTS_UNLIMITED, false)
+
     @Parcelize
     @Keep
-    object PlansUnlimited : AddOnPackage(ContribFeature.PLANS_UNLIMITED)
+    object PlansUnlimited : AddOnPackage(ContribFeature.PLANS_UNLIMITED, false)
+
     @Parcelize
     @Keep
-    object SplitTransaction : AddOnPackage(ContribFeature.SPLIT_TRANSACTION)
+    object SplitTransaction : AddOnPackage(ContribFeature.SPLIT_TRANSACTION, false)
+
     @Parcelize
     @Keep
-    object Distribution : AddOnPackage(ContribFeature.DISTRIBUTION)
+    object Distribution : AddOnPackage(ContribFeature.DISTRIBUTION, false)
+
     @Parcelize
     @Keep
-    object Print : AddOnPackage(ContribFeature.PRINT)
+    object Print : AddOnPackage(ContribFeature.PRINT, false)
+
     @Parcelize
     @Keep
-    object AdFree : AddOnPackage(ContribFeature.AD_FREE)
+    object AdFree : AddOnPackage(ContribFeature.AD_FREE, false)
+
     @Parcelize
     @Keep
     object CsvImport : AddOnPackage(ContribFeature.CSV_IMPORT)
+
     @Parcelize
     @Keep
     object Synchronization : AddOnPackage(ContribFeature.SYNCHRONIZATION)
