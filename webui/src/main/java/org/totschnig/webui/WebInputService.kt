cd5397a97d83e0abdfc91ea298f0114a33eafc00
Michael Totschnig <michael@totschnig.org>
2022-09-05 19:48:05 +0200

    need to keep classes that Netty uses by reflection

--- webui/src/main/java/org/totschnig/webui/WebInputService.kt
+++ webui/src/main/java/org/totschnig/webui/WebInputService.kt
@@ -3,7 +3,6 @@ package org.totschnig.webui
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_ONE_SHOT
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
