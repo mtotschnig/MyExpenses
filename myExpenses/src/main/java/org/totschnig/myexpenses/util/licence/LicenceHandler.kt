1306dde45f3a4743e0d624b1b9e55698229b15da
Michael Totschnig <michael@totschnig.org>
2023-03-13 15:21:20 +0100

    reset hasOurLicence flag if addOnFeatures is empty

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/LicenceHandler.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/LicenceHandler.kt
@@ -128,6 +128,8 @@ open class LicenceHandler(
         if (!keepFeatures) {
             addOnFeatures.clear()
             licenseStatusPrefs.remove(LICENSE_FEATURES)
+        }
+        if (addOnFeatures.isEmpty()) {
             hasOurLicence = false
         }
         licenseStatusPrefs.commit()
