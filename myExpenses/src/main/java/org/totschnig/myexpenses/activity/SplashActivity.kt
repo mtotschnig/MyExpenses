4891e79c86d8172a31734f2320c6eef8a7147d23
Michael Totschnig <michael@totschnig.org>
2023-05-04 09:33:13 +0200

    https://issuetracker.google.com/u/1/issues/160946170

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/SplashActivity.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/SplashActivity.kt
@@ -3,17 +3,23 @@ package org.totschnig.myexpenses.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import org.totschnig.myexpenses.MyApplication
 import org.totschnig.myexpenses.preference.PrefKey
+import kotlin.system.exitProcess
 
 class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, if ((application as MyApplication).appComponent.prefHandler()
-                .getInt(PrefKey.CURRENT_VERSION, -1) == -1
-        ) OnboardingActivity::class.java else MyExpenses::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
-        })
+        (application as? MyApplication)?.also {
+            startActivity(Intent(this, if ((application as MyApplication).appComponent.prefHandler()
+                    .getInt(PrefKey.CURRENT_VERSION, -1) == -1
+            ) OnboardingActivity::class.java else MyExpenses::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+            })
+        } ?: run {
+            Handler().post { exitProcess(0) }
+        }
         finish()
     }
 }
