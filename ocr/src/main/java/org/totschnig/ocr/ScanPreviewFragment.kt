2416d72a2d8527174137c21f62cdce924f8a1b4e
Michael Totschnig <michael@totschnig.org>
2023-07-06 17:28:02 +0200

    fixes #1236

--- ocr/src/main/java/org/totschnig/ocr/ScanPreviewFragment.kt
+++ ocr/src/main/java/org/totschnig/ocr/ScanPreviewFragment.kt
@@ -4,54 +4,59 @@ import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
 import org.totschnig.myexpenses.MyApplication
 import org.totschnig.myexpenses.R
-import org.totschnig.myexpenses.dialog.BaseDialogFragment
+import org.totschnig.myexpenses.dialog.DialogViewBinding
 import org.totschnig.myexpenses.feature.OcrHost
 import org.totschnig.ocr.databinding.ScanPreviewBinding
 import javax.inject.Inject
 
-class ScanPreviewFragment : BaseDialogFragment() {
-    private lateinit var binding: ScanPreviewBinding
+class ScanPreviewFragment : DialogViewBinding<ScanPreviewBinding>() {
     @Inject
     lateinit var picasso: Picasso
     private lateinit var viewModel: ScanPreviewViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DaggerOcrComponent.builder().appComponent((requireActivity().application as MyApplication).appComponent).build().inject(this)
+        DaggerOcrComponent.builder()
+            .appComponent((requireActivity().application as MyApplication).appComponent).build()
+            .inject(this)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        binding = ScanPreviewBinding.inflate(LayoutInflater.from(requireContext()))
         viewModel = ViewModelProvider(this)[ScanPreviewViewModel::class.java]
         viewModel.getResult().observe(this) { result ->
             (activity as? OcrHost)?.processOcrResult(result, scanUri)
             dismiss()
         }
-        val builder: AlertDialog.Builder = MaterialAlertDialogBuilder(requireActivity())
-                .setView(binding.root)
-                .setCancelable(false)
-                .setNegativeButton(android.R.string.cancel, null)
-                .setPositiveButton(R.string.button_scan, null)
-        return builder.create().apply {
-            setOnShowListener {
-                loadImage()
-                getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                    getButton(AlertDialog.BUTTON_POSITIVE)?.let {
-                        it.isEnabled = false
+        return initBuilder {
+            ScanPreviewBinding.inflate(it)
+        }
+            .setCancelable(false)
+            .setNegativeButton(android.R.string.cancel, null)
+            .setPositiveButton(R.string.button_scan, null)
+            .create()
+            .apply {
+                setOnShowListener {
+                    loadImage()
+                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                        getButton(AlertDialog.BUTTON_POSITIVE)?.let {
+                            it.isEnabled = false
+                        }
+                        showSnackBar(
+                            getString(
+                                R.string.ocr_recognition_info,
+                                viewModel.getOcrInfo(requireContext())
+                            ), Snackbar.LENGTH_INDEFINITE, null
+                        )
+                        activity?.let { viewModel.runTextRecognition(scanUri, it) }
                     }
-                    showSnackBar(getString(R.string.ocr_recognition_info, viewModel.getOcrInfo(requireContext())), Snackbar.LENGTH_INDEFINITE, null)
-                    activity?.let { viewModel.runTextRecognition(scanUri, it) }
                 }
             }
-        }
     }
 
     private fun loadImage() {
