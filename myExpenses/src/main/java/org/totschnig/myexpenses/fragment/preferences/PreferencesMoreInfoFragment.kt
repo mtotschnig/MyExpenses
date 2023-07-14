165e071876e70e9ccae9104f3d4b5b93f8492c4d
Michael Totschnig <michael@totschnig.org>
2023-07-14 18:13:10 +0200

    More info stub

---
+++ myExpenses/src/main/java/org/totschnig/myexpenses/fragment/preferences/PreferencesMoreInfoFragment.kt
@@ -0,0 +1,10 @@
+package org.totschnig.myexpenses.fragment.preferences
+
+import androidx.fragment.app.Fragment
+import org.totschnig.myexpenses.R
+
+class PreferencesMoreInfoFragment : BasePreferenceFragment() {
+
+    override val preferencesResId = R.xml.preferences_more_info
+
+}
