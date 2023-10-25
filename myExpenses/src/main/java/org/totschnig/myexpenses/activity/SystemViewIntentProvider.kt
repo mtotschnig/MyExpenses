c3434e0638e8d4ad7f5f24c92a480ecd7b8a8833
Michael Totschnig <michael@totschnig.org>
2023-10-25 16:57:19 +0200

    restore fallback handling for file uris

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/SystemViewIntentProvider.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/SystemViewIntentProvider.kt
@@ -1,11 +1,21 @@
 package org.totschnig.myexpenses.activity
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import org.totschnig.myexpenses.util.AppDirHelper
+import org.totschnig.myexpenses.util.NougatFileProviderException
 
 class SystemViewIntentProvider : ViewIntentProvider {
-    override fun getViewIntent(uri: Uri) =
-        Intent(Intent.ACTION_VIEW, uri).apply {
+    override fun getViewIntent(context: Context, uri: Uri) = try {
+        Intent(Intent.ACTION_VIEW, AppDirHelper.ensureContentUri(uri, context)).apply {
             flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
         }
+    } catch (e: NougatFileProviderException) {
+        getFallbackIntent(context, uri)
+    }
+
+    private fun getFallbackIntent(context: Context, pictureUri: Uri) =
+        Intent(Intent.ACTION_VIEW, pictureUri, context, SimpleImageActivity::class.java)
 }
--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/ViewIntentProvider.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/ViewIntentProvider.kt
@@ -2,6 +2,7 @@ package org.totschnig.myexpenses.activity
 
 import android.content.Intent
 import android.app.Activity
+import android.content.Context
 import android.net.Uri
 import org.totschnig.myexpenses.util.crashreporting.CrashHandler
 import android.widget.Toast
@@ -9,10 +10,10 @@ import org.totschnig.myexpenses.util.safeMessage
 import java.lang.Exception
 
 interface ViewIntentProvider {
-    fun getViewIntent(uri: Uri): Intent
+    fun getViewIntent(context: Context, uri: Uri): Intent
     fun startViewAction(activity: Activity, uri: Uri) {
         try {
-            activity.startActivity(getViewIntent(uri))
+            activity.startActivity(getViewIntent(activity, uri))
         } catch (e: Exception) {
             CrashHandler.report(e, "uri", uri.toString())
             Toast.makeText(activity, e.safeMessage, Toast.LENGTH_LONG).show()
