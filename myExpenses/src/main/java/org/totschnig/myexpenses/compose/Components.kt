19f8bce0b08d65f849dc58fc9cc807ffbd4f4950
Michael Totschnig <michael@totschnig.org>
2023-11-10 16:42:01 +0100

    FlowRow gives better rendering if available width is small

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/Components.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/Components.kt
@@ -1,19 +1,38 @@
 package org.totschnig.myexpenses.compose
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ButtonRow(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
-    Row(
+    FlowRow(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
         content = content
     )
+}
+
+@Preview
+@Composable
+fun ButtonRowDemo() {
+    ButtonRow(modifier = Modifier.width(150.dp)) {
+        Button(onClick = { }) {
+            Text("Cancel")
+        }
+        Button(onClick = {}) {
+            Text("Load")
+        }
+    }
 }
