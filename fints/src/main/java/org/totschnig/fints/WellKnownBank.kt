72cc1b991778ab016702d30bb526cb1300fec407
Michael Totschnig <michael@totschnig.org>
2024-01-15 15:24:27 +0100

    added targo to well known banks

--- fints/src/main/java/org/totschnig/fints/WellKnownBank.kt
+++ fints/src/main/java/org/totschnig/fints/WellKnownBank.kt
@@ -11,11 +11,13 @@ enum class WellKnownBank(@DrawableRes val icon: Int, val color: Int) {
     COMDIRECT(R.drawable.comdirect, Color.rgb(255, 245, 0)),
     SPARKASSE(R.drawable.sparkasse, Color.rgb(255, 0, 0)),
     VOLKSBANK(R.drawable.volksbank, Color.rgb(0, 101, 178)),
-    SPARDA(R.drawable.sparda, Color.rgb(0, 92, 168))
+    SPARDA(R.drawable.sparda, Color.rgb(0, 92, 168)),
+    TARGO(R.drawable.targo, Color.rgb(122, 156, 192))
 }
 
 val Bank.asWellKnown: WellKnownBank?
     get() = when {
+        blz == "30020900" -> WellKnownBank.TARGO
         blz == "12030000" -> WellKnownBank.DKB
         blz == "43060967" -> WellKnownBank.GLS
         blz == "50010517" -> WellKnownBank.ING
