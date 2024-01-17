9584951dfc1eb2de2f2b882da1f6952b71e781bb
Michael Totschnig <michael@totschnig.org>
2024-01-17 09:17:26 +0100

    stub for TODO test

---
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/test/espresso/MyExpensesMethodFilterTest.kt
@@ -0,0 +1,38 @@
+package org.totschnig.myexpenses.test.espresso
+
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.totschnig.myexpenses.db2.findPaymentMethod
+import org.totschnig.myexpenses.model.*
+import org.totschnig.myexpenses.model2.Account
+import org.totschnig.myexpenses.testutils.BaseMyExpensesTest
+
+@Ignore("TODO")
+class MyExpensesMethodFilterTest: BaseMyExpensesTest() {
+    private lateinit var account: Account
+
+    @Before
+    fun fixture() {
+/*        val currency = CurrencyUnit.DebugInstance
+        account =  buildAccount("Test account 1")
+        val op = Transaction.getNewInstance(account.id, homeCurrency)
+        op.amount = Money(currency, -1200L)
+        op.save(contentResolver)
+        op.methodId = repository.findPaymentMethod(PreDefinedPaymentMethod.CHEQUE.name)
+        op.date = op.date - 10000
+        op.saveAsNew(contentResolver)
+        launch(account.id)*/
+    }
+
+    @Test
+    fun methodFilter() {
+        //TODO
+    }
+
+    private fun methodIsDisplayed(payee: String, position: Int) {
+        assertTextAtPosition(payee, position)
+    }
+
+}
