375bafd58fd5d39fce939a18fae3e8f9eeee3e45
Michael Totschnig <michael@totschnig.org>
2024-02-06 23:45:36 +0100

    Fixes #1397

--- myExpenses/src/main/java/org/totschnig/myexpenses/fragment/preferences/PreferenceDataFragment.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/fragment/preferences/PreferenceDataFragment.kt
@@ -1,5 +1,6 @@
 package org.totschnig.myexpenses.fragment.preferences
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.annotation.Keep
 import androidx.fragment.app.viewModels
@@ -14,6 +15,7 @@ import org.totschnig.myexpenses.R
 import org.totschnig.myexpenses.dialog.MessageDialogFragment
 import org.totschnig.myexpenses.injector
 import org.totschnig.myexpenses.preference.PrefKey
+import org.totschnig.myexpenses.retrofit.ExchangeRateSource
 import org.totschnig.myexpenses.util.TextUtils
 import org.totschnig.myexpenses.viewmodel.CurrencyViewModel
 import org.totschnig.myexpenses.viewmodel.data.Currency
@@ -74,11 +76,52 @@ class PreferenceDataFragment: BasePreferenceFragment() {
                 }
             }
         }
+        with(requirePreference<ListPreference>(PrefKey.EXCHANGE_RATE_PROVIDER)) {
+            entries = ExchangeRateSource.values.map { it.host }.toTypedArray()
+            entryValues = ExchangeRateSource.values.map { it.id }.toTypedArray()
+        }
+        arrayOf(ExchangeRateSource.OpenExchangeRates, ExchangeRateSource.CoinApi).forEach {
+            requirePreference<Preference>(it.prefKey).summary =
+                getString(R.string.pref_exchange_rates_api_key_summary, it.host)
+        }
+        configureExchangeRatesPreference(ExchangeRateSource.preferredSource(prefHandler))
+        requirePreference<Preference>(PrefKey.EXCHANGE_RATES_CLEAR_CACHE).title =
+            "${getString(R.string.clear_cache)} (${getString(R.string.pref_category_exchange_rates)})"
+    }
+
+    private fun configureExchangeRatesPreference(provider: ExchangeRateSource) {
+        arrayOf(ExchangeRateSource.OpenExchangeRates, ExchangeRateSource.CoinApi).forEach {
+            requirePreference<Preference>(it.prefKey).isVisible = provider == it
+        }
     }
 
     fun updateHomeCurrency(currencyCode: String) {
         findPreference<ListPreference>(PrefKey.HOME_CURRENCY)?.let {
             it.value = currencyCode
+        }
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference) = when {
+        super.onPreferenceTreeClick(preference) -> true
+        matches(preference, PrefKey.EXCHANGE_RATES_CLEAR_CACHE) -> {
+            viewModel.clearExchangeRateCache().observe(this) {
+                preferenceActivity.showSnackBar("${getString(R.string.clear_cache)} ($it)")
+            }
+            true
+        }
+
+        else -> false
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
+        when (key) {
+            getKey(PrefKey.EXCHANGE_RATE_PROVIDER) -> {
+                configureExchangeRatesPreference(
+                    ExchangeRateSource.preferredSource(
+                        sharedPreferences.getString(key, null)
+                    )
+                )
+            }
         }
     }
 }
