da9f016b964b9209b45efd305f7d65821a13f5ff
Michael Totschnig <michael@totschnig.org>
2024-06-01 20:35:03 +0200

    Experimental generation of BaselineProfile. When the Generator runs with a use case of onboarding -> main screen, the benchmark so 0 positive effect of baseline

---
+++ baselineprofile/src/main/java/org/totschnig/myexpenses/baselineprofile/BaselineProfileGenerator.kt
@@ -0,0 +1,69 @@
+package org.totschnig.myexpenses.baselineprofile
+
+import android.content.Intent
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * This test class generates a basic startup baseline profile for the target package.
+ *
+ * We recommend you start with this but add important user flows to the profile to improve their performance.
+ * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
+ * for more information.
+ *
+ * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
+ * the equivalent `generateBaselineProfile` gradle task:
+ * ```
+ * ./gradlew :.home.michael.MyExpenses.myExpenses:generateReleaseBaselineProfile
+ * ```
+ * The run configuration runs the Gradle task and applies filtering to run only the generators.
+ *
+ * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
+ * for more information about available instrumentation arguments.
+ *
+ * After you run the generator, you can verify the improvements running the [StartupBenchmarks] benchmark.
+ *
+ * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
+ *
+ * The minimum required version of androidx.benchmark to generate a baseline profile is 1.2.0.
+ **/
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BaselineProfileGenerator {
+
+    @get:Rule
+    val rule = BaselineProfileRule()
+
+    @Test
+    fun generate() {
+        // The application id for the running build variant is read from the instrumentation arguments.
+        rule.collect(
+            packageName = InstrumentationRegistry.getArguments().getString("targetAppId")
+                ?: throw Exception("targetAppId not passed as instrumentation runner arg"),
+
+            // See: https://d.android.com/topic/performance/baselineprofiles/dex-layout-optimizations
+            includeInStartupProfile = true
+        ) {
+            // This block defines the app's critical user journey. Here we are interested in
+            // optimizing for app startup. But you can also navigate and scroll through your most important UI.
+
+            // Start default activity for your app
+            pressHome()
+            startActivityAndWait()
+            device.wait(Until.hasObject(By.textContains("adapts to your preference")), 30_000)
+            device.findObject(By.text("Next")).click()
+            device.wait(Until.hasObject(By.text("Your data stays in your hands")), 30_000)
+            device.findObject(By.text("Next")).click()
+            device.wait(Until.hasObject(By.text("Let's set up your first account")), 30_000)
+            device.findObject(By.text("Get started")).click()
+            device.findObject(By.res("org.totschnig.myexpenses:id/CREATE_COMMAND"))
+        }
+    }
+}
