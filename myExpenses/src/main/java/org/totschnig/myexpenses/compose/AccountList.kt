351e9fbd72e4ab5dcc9119356e584cfac2fd7bf4
Michael Totschnig <michael@totschnig.org>
2024-07-20 01:05:28 +0200

    Second part to #1538

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/AccountList.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/AccountList.kt
@@ -8,12 +8,29 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Functions
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -47,6 +64,7 @@ fun AccountList(
     accountData: List<FullAccount>,
     grouping: AccountGrouping,
     selectedAccount: Long,
+    listState: LazyListState,
     onSelected: (Long) -> Unit,
     onEdit: (FullAccount) -> Unit,
     onDelete: (FullAccount) -> Unit,
@@ -64,6 +82,7 @@ fun AccountList(
 
     if (collapsedGroupIds != null && collapsedAccountIds != null) {
         LazyColumn(
+            state = listState,
             modifier = Modifier.testTag(TEST_TAG_ACCOUNTS),
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
@@ -78,7 +97,7 @@ fun AccountList(
                     isGroupHidden = collapsedGroupIds.contains(it.first)
                 }
                 if (!isGroupHidden) {
-                    item {
+                    item(key = account.id) {
                         AccountCard(
                             account = account,
                             isCollapsed = collapsedAccountIds.contains(account.id.toString()),
