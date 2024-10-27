3c16d73f3578b8cf21bf0c872333d16666fbcb57
Michael Totschnig <michael@totschnig.org>
2024-10-27 10:16:50 +0100

    Fixes #1589

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/IconSelector.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/IconSelector.kt
@@ -3,7 +3,9 @@ package org.totschnig.myexpenses.compose
 import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Build
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,6 +20,7 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -27,9 +30,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,19 +65,30 @@ fun IconSelector(
                 .sortedBy { it.third }
         }
     }
-    val localFocusManager = LocalFocusManager.current
 
     Column(modifier = modifier) {
+        // Since Compose 1.7.3, on Oreo and below we receive an unwanted focus for the
+        // Search tab, which overrides the default focus on the selected tab
+        // We try to ignore this first focus change
+        //TODO revisit if this is still necessary (instead of harmful) with future Compose versions
+        var initialFocusProcessed by remember { mutableStateOf(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) }
         ScrollableTabRow(selectedTabIndex = selectedTabIndex) {
             Tab(
                 modifier = Modifier.width(100.dp),
                 selected = selectedTabIndex == 0,
-                onClick = { selectedTabIndex = 0 }) {
+                onClick = {
+                    selectedTabIndex = 0
+                }
+            ) {
                 TextField(
                     modifier = Modifier
                         .onFocusChanged {
                             if (it.isFocused) {
-                                selectedTabIndex = 0
+                                if (initialFocusProcessed) {
+                                    selectedTabIndex = 0
+                                } else {
+                                    initialFocusProcessed = true
+                                }
                             }
                         },
                     value = searchTerm,
@@ -84,13 +99,12 @@ fun IconSelector(
             }
             categories.forEachIndexed { tabIndex, category ->
                 val effectiveIndex = tabIndex + 1
-                Tab(selected = selectedTabIndex == effectiveIndex,
-                    onClick = {
-                        selectedTabIndex = effectiveIndex
-                        localFocusManager.clearFocus()
-                    },
-                    text = { Text(text = stringResource(category.label)) }
-                )
+                CategoryTab(
+                    category = category,
+                    selected = selectedTabIndex == effectiveIndex
+                ) {
+                    selectedTabIndex = effectiveIndex
+                }
             }
         }
         LazyVerticalGrid(
@@ -118,6 +132,26 @@ fun IconSelector(
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CategoryTab(
+    category: IconCategory,
+    focusRequester: FocusRequester = FocusRequester(),
+    selected: Boolean,
+    onclick: () -> Unit
+) {
+    Tab(
+        modifier = Modifier.focusRequester(focusRequester).focusable(),
+        selected = selected,
+        onClick = onclick,
+        text = { Text(text = stringResource(category.label)) }
+    )
+    LaunchedEffect(selected) {
+        if (selected) {
+            focusRequester.requestFocus()
         }
     }
 }
