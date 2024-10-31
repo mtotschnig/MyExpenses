69a1061efcb1a64fd422191afd479e5d6ca4d2b0
Michael Totschnig <michael@totschnig.org>
2024-10-31 08:21:21 +0100

    actually honour cleanup argument

--- myExpenses/src/androidTest/java/org/totschnig/myexpenses/testutils/Utils.kt
+++ myExpenses/src/androidTest/java/org/totschnig/myexpenses/testutils/Utils.kt
@@ -3,7 +3,7 @@ package org.totschnig.myexpenses.testutils
 import androidx.test.platform.app.InstrumentationRegistry
 
 val isOrchestrated: Boolean
-    get() = false //TODO pass in argument
+    get() = !getBooleanInstrumentationArgument("withCleanup")
 
 fun getBooleanInstrumentationArgument(key: String) =
     getInstrumentationArgument(key, "false") == "true"
@@ -13,8 +13,9 @@ fun getInstrumentationArgument(key: String, defaultValue: String) =
 
 /**
  * With Android Test Orchestrator each test runs on clean database, so cleanup is not necessary,
- * but when we run from commandline with "am instrument" (which is the only way to test the universal apk, e.g. with sqlcrypt module),
- * this is not the case, so we conditionally allow to run cleanup tasks
+ * but when we run from commandline with "am instrument" (which is the only way to test the
+ * universal apk, e.g. with sqlcrypt module), this is not the case, so we conditionally allow to run
+ * cleanup tasks by passing in argument "-e withCleanup true"
  */
 fun cleanup(work: () -> Unit) {
     if (!isOrchestrated) {
