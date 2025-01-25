e4db6be503178b7cc3cd27f54f40295e29790b7c
Michael Totschnig <michael@totschnig.org>
2025-01-25 21:36:33 +0100

    Allow Talkback users to close Feature Discovery Overlays: Workaround for https://github.com/KeepSafe/TapTargetView/issues/370

--- myExpenses/src/main/java/org/totschnig/myexpenses/ui/DiscoveryHelper.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/ui/DiscoveryHelper.kt
@@ -2,14 +2,18 @@ package org.totschnig.myexpenses.ui
 
 import android.app.Activity
 import android.content.Context
+import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.getkeepsafe.taptargetview.TapTarget
 import com.getkeepsafe.taptargetview.TapTargetView
 import org.totschnig.myexpenses.R
 import org.totschnig.myexpenses.preference.PrefHandler
-import org.totschnig.myexpenses.util.ui.UiUtils
 import org.totschnig.myexpenses.util.Utils
+import org.totschnig.myexpenses.util.ui.UiUtils
 import timber.log.Timber
 
 class DiscoveryHelper(val prefHandler: PrefHandler) : IDiscoveryHelper {
@@ -22,11 +26,17 @@ class DiscoveryHelper(val prefHandler: PrefHandler) : IDiscoveryHelper {
         },
         FabLongPress("showDiscoveryFabLongPress") {
             override fun toTitle(context: Context) = with(context) {
-                String.format("%s / %s / %s / %s", getString(R.string.transfer), getString(R.string.split_transaction), getString(R.string.income), getString(R.string.expense))
+                String.format(
+                    "%s / %s / %s / %s",
+                    getString(R.string.transfer),
+                    getString(R.string.split_transaction),
+                    getString(R.string.income),
+                    getString(R.string.expense)
+                )
             }
         };
 
-        open fun getLabelResId(ctx: Context) = when(this) {
+        open fun getLabelResId(ctx: Context) = when (this) {
             ExpenseIncomeSwitch -> R.string.discover_feature_expense_income_switch
             FabLongPress -> R.string.discover_feature_fab_long_press
         }
@@ -36,40 +46,78 @@ class DiscoveryHelper(val prefHandler: PrefHandler) : IDiscoveryHelper {
         abstract fun toTitle(context: Context): String
     }
 
-    override fun discover(context: Activity, target: View, daysSinceInstall: Int, feature: Feature,
-                          measureTarget: Boolean) =
-            (Utils.getDaysSinceInstall(context) >= daysSinceInstall && prefHandler.getBoolean(feature.key, true)).also {
-                if (it) {
-                    if (measureTarget) {
-                        target.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-                            override fun onGlobalLayout() {
-                                discoveryShow(context, target, feature, UiUtils.px2Dp(target.width / 2))
-                                target.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                            }
-                        })
-                    } else {
-                        discoveryShow(context, target, feature)
-                    }
+    override fun discover(
+        context: Activity, target: View, daysSinceInstall: Int, feature: Feature,
+        measureTarget: Boolean,
+    ) =
+        (Utils.getDaysSinceInstall(context) >= daysSinceInstall && prefHandler.getBoolean(
+            feature.key,
+            true
+        )).also {
+            if (it) {
+                if (measureTarget) {
+                    target.viewTreeObserver.addOnGlobalLayoutListener(object :
+                        ViewTreeObserver.OnGlobalLayoutListener {
+                        override fun onGlobalLayout() {
+                            discoveryShow(context, target, feature, UiUtils.px2Dp(target.width / 2))
+                            target.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        }
+                    })
+                } else {
+                    discoveryShow(context, target, feature)
                 }
             }
+        }
 
     override fun markDiscovered(feature: Feature) {
         prefHandler.putBoolean(feature.key, false)
         Timber.d("Marked as discovered: %s", feature)
     }
 
-    private fun discoveryShow(context: Activity, target: View, feature: Feature, targetRadius: Int? = null) {
-        TapTargetView.showFor(context,
-                TapTarget.forView(target, feature.toTitle(context), feature.toDescription(context))
-                        .transparentTarget(true)
-                        .outerCircleColorInt(UiUtils.getColor(context, androidx.appcompat.R.attr.colorAccent))
-                        .apply {
-                            targetRadius?.let { this.targetRadius(it) }
-                        },
-                object : TapTargetView.Listener() {
-                    override fun onTargetDismissed(view: TapTargetView?, userInitiated: Boolean) {
-                        markDiscovered(feature)
-                    }
-                })
+    private fun discoveryShow(
+        context: Activity,
+        target: View,
+        feature: Feature,
+        targetRadius: Int? = null,
+    ) {
+        val title = feature.toTitle(context)
+        val description = feature.toDescription(context)
+        val tapTargetView = TapTargetView.showFor(context,
+            TapTarget.forView(target, title, description)
+                .transparentTarget(true)
+                .outerCircleColorInt(
+                    UiUtils.getColor(
+                        context,
+                        androidx.appcompat.R.attr.colorAccent
+                    )
+                )
+                .apply {
+                    targetRadius?.let { this.targetRadius(it) }
+                },
+            object : TapTargetView.Listener() {
+                override fun onTargetDismissed(view: TapTargetView?, userInitiated: Boolean) {
+                    markDiscovered(feature)
+                }
+            })
+        ViewCompat.setAccessibilityDelegate(tapTargetView, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(
+                host: View,
+                info: AccessibilityNodeInfoCompat,
+            ) {
+                super.onInitializeAccessibilityNodeInfo(host, info)
+                info.contentDescription = "$title: $description"
+                info.addAction(AccessibilityNodeInfoCompat.ACTION_DISMISS)
+                info.isDismissable = true
+            }
+
+            override fun performAccessibilityAction(
+                host: View,
+                action: Int,
+                args: Bundle?,
+            ) = if (action == AccessibilityNodeInfoCompat.ACTION_DISMISS) {
+                tapTargetView.dismiss(false)
+                true
+            } else super.performAccessibilityAction(host, action, args)
+        })
     }
 }
