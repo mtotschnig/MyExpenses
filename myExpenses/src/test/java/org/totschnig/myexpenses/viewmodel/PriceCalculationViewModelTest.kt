f8ab814fe9c09908cb3042358acc4246dd3caed7
Michael Totschnig <michael@totschnig.org>
2025-03-28 18:56:06 +0100

    adapt test

--- myExpenses/src/test/java/org/totschnig/myexpenses/viewmodel/PriceCalculationViewModelTest.kt
+++ myExpenses/src/test/java/org/totschnig/myexpenses/viewmodel/PriceCalculationViewModelTest.kt
@@ -25,9 +25,9 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 @RunWith(RobolectricTestRunner::class)
-class PriceHistoryViewModelTest: BaseTestWithRepository() {
+class PriceCalculationViewModelTest: BaseTestWithRepository() {
 
-    private fun buildViewModel() = PriceHistoryViewModel(
+    private fun buildViewModel() = PriceCalculationViewModel(
             application,
             SavedStateHandle()
         ).also {
@@ -52,45 +52,53 @@ class PriceHistoryViewModelTest: BaseTestWithRepository() {
         }
 
         val viewModel = buildViewModel()
-        var newPrices = viewModel.recalculatePrices("SAR")
-        newPrices["2025-01-01"]!!.verify("AED", 1.02040816326531)
-        newPrices["2025-01-01"]!!.verify("KGS", 23.469387755102)
-        newPrices["2025-01-01"]!!.verify("KZT", 142.85714285714)
-        newPrices["2025-01-01"]!!.verify("USD", 3.74489795918367)
-        newPrices["2025-01-06"]!!.verify("AED", 1.02249488752556)
-        newPrices["2025-01-06"]!!.verify("KGS", 24.5398773006135)
-        newPrices["2025-01-06"]!!.verify("KZT",145.19427402863)
-        newPrices["2025-01-06"]!!.verify("USD", 3.74335378323108)
+        var newPrices = viewModel.recalculateAndLoad("SAR")
+        var newPrices1 = newPrices["2025-01-01"]!!
+        var newPrices2 = newPrices["2025-01-06"]!!
+        newPrices1.verify("AED", 1.02040816326531)
+        newPrices1.verify("KGS", 23.469387755102)
+        newPrices1.verify("KZT", 142.85714285714)
+        newPrices1.verify("USD", 3.74489795918367)
+        newPrices2.verify("AED", 1.02249488752556)
+        newPrices2.verify("KGS", 24.5398773006135)
+        newPrices2.verify("KZT",145.19427402863)
+        newPrices2.verify("USD", 3.74335378323108)
 
-        newPrices = viewModel.recalculatePrices("KGS")
-        newPrices["2025-01-01"]!!.verify("AED", 0.0434782608695652)
-        newPrices["2025-01-01"]!!.verify("SAR", 0.0426086956521739)
-        newPrices["2025-01-01"]!!.verify("KZT", 6.08695652173913)
-        newPrices["2025-01-01"]!!.verify("USD", 0.159565217391304)
-        newPrices["2025-01-06"]!!.verify("AED", 0.0416666666666667)
-        newPrices["2025-01-06"]!!.verify("SAR", 0.04075)
-        newPrices["2025-01-06"]!!.verify("KZT",5.91666666666667)
-        newPrices["2025-01-06"]!!.verify("USD", 0.152541666666667)
+        newPrices = viewModel.recalculateAndLoad("KGS")
+        newPrices1 = newPrices["2025-01-01"]!!
+        newPrices2 = newPrices["2025-01-06"]!!
+        newPrices1.verify("AED", 0.0434782608695652)
+        newPrices1.verify("SAR", 0.0426086956521739)
+        newPrices1.verify("KZT", 6.08695652173913)
+        newPrices1.verify("USD", 0.159565217391304)
+        newPrices2.verify("AED", 0.0416666666666667)
+        newPrices2.verify("SAR", 0.04075)
+        newPrices2.verify("KZT",5.91666666666667)
+        newPrices2.verify("USD", 0.152541666666667)
 
-        newPrices = viewModel.recalculatePrices("KZT")
-        newPrices["2025-01-01"]!!.verify("AED", 0.00714285714285714)
-        newPrices["2025-01-01"]!!.verify("SAR", 0.007)
-        newPrices["2025-01-01"]!!.verify("KGS", 0.164285714285714)
-        newPrices["2025-01-01"]!!.verify("USD", 0.0262142857142857)
-        newPrices["2025-01-06"]!!.verify("AED", 0.00704225352112676)
-        newPrices["2025-01-06"]!!.verify("SAR", 0.00688732394366197)
-        newPrices["2025-01-06"]!!.verify("KGS",0.169014084507042)
-        newPrices["2025-01-06"]!!.verify("USD", 0.0257816901408451)
+        newPrices = viewModel.recalculateAndLoad("KZT")
+        newPrices1 = newPrices["2025-01-01"]!!
+        newPrices2 = newPrices["2025-01-06"]!!
+        newPrices1.verify("AED", 0.00714285714285714)
+        newPrices1.verify("SAR", 0.007)
+        newPrices1.verify("KGS", 0.164285714285714)
+        newPrices1.verify("USD", 0.0262142857142857)
+        newPrices2.verify("AED", 0.00704225352112676)
+        newPrices2.verify("SAR", 0.00688732394366197)
+        newPrices2.verify("KGS",0.169014084507042)
+        newPrices2.verify("USD", 0.0257816901408451)
 
-        newPrices = viewModel.recalculatePrices("USD")
-        newPrices["2025-01-01"]!!.verify("AED", 0.272479564032698)
-        newPrices["2025-01-01"]!!.verify("SAR", 0.267029972752044)
-        newPrices["2025-01-01"]!!.verify("KGS", 6.26702997275204)
-        newPrices["2025-01-01"]!!.verify("KZT", 38.1471389645777)
-        newPrices["2025-01-06"]!!.verify("AED", 0.273149412728763)
-        newPrices["2025-01-06"]!!.verify("SAR", 0.26714012564873)
-        newPrices["2025-01-06"]!!.verify("KGS",6.5555859054903)
-        newPrices["2025-01-06"]!!.verify("KZT", 38.7872166074843)
+        newPrices = viewModel.recalculateAndLoad("USD")
+        newPrices1 = newPrices["2025-01-01"]!!
+        newPrices2 = newPrices["2025-01-06"]!!
+        newPrices1.verify("AED", 0.272479564032698)
+        newPrices1.verify("SAR", 0.267029972752044)
+        newPrices1.verify("KGS", 6.26702997275204)
+        newPrices1.verify("KZT", 38.1471389645777)
+        newPrices2.verify("AED", 0.273149412728763)
+        newPrices2.verify("SAR", 0.26714012564873)
+        newPrices2.verify("KGS",6.5555859054903)
+        newPrices2.verify("KZT", 38.7872166074843)
      }
 
     @Test
@@ -259,7 +267,7 @@ class PriceHistoryViewModelTest: BaseTestWithRepository() {
     }
 
 
-    private suspend fun PriceHistoryViewModel.recalculateAmountsAndAssert(
+    private suspend fun PriceCalculationViewModel.recalculateAmountsAndAssert(
         newHomeCurrency: String,
         onlyMissing: Boolean = true,
         onlyAccount: Long? = null,
@@ -303,7 +311,7 @@ class PriceHistoryViewModelTest: BaseTestWithRepository() {
         }
     }
 
-    private suspend fun PriceHistoryViewModel.recalculatePrices(newHomeCurrency: String): Map<String, List<Pair<String, Double>>> {
+    private suspend fun PriceCalculationViewModel.recalculateAndLoad(newHomeCurrency: String): Map<String, List<Pair<String, Double>>> {
         reCalculatePrices(newHomeCurrency)
         return contentResolver.query(
             PRICES_URI,
