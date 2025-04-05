54e3a1044d388894646042da2689384476b1ef0c
Michael Totschnig <michael@totschnig.org>
2025-04-05 23:05:23 +0200

    unused import

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/BaseMyExpenses.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/BaseMyExpenses.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
