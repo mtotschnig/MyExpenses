22f4bb5cd7abbee075275276b454a2bd825923be
Michael Totschnig <michael@totschnig.org>
2025-06-12 18:49:46 +0200

    Fixes #1717

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/BaseMyExpenses.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/BaseMyExpenses.kt
@@ -1045,8 +1045,12 @@ abstract class BaseMyExpenses : LaunchActivity(), OnDialogResultListener, Contri
                         toolbar.subtitle = null
                     }
                 }
-                val pagerState = rememberPagerState { accountData.count() }
+
                 if (accountData.isNotEmpty()) {
+                    val pagerState = rememberPagerState(
+                        accountData.indexOfFirst { viewModel.selectedAccountId == it.id }
+                            .coerceAtLeast(0)
+                    ) { accountData.count() }
                     LaunchedEffect(viewModel.selectedAccountId) {
                         if (pagerState.currentPage != currentPage) {
                             pagerState.scrollToPage(currentPage)
