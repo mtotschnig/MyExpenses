7d5b9998103c297c6aea7601ac28c8b329589c16
Michael Totschnig <michael@totschnig.org>
2025-07-16 22:41:54 +0200

    report exceptional condition

--- myExpenses/src/main/java/org/totschnig/myexpenses/sync/SyncDelegate.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/sync/SyncDelegate.kt
@@ -716,6 +716,7 @@ class SyncDelegate(
             change.parentUuid()?.let {
                 val parentId = resolver(account.id, it)
                 if (parentId == -1L) {
+                    CrashHandler.report(Exception("Could not find parent for split"))
                     return ArrayList() //if we fail to link a split part to a parent, we need to ignore it
                 }
                 t.parentId = parentId
