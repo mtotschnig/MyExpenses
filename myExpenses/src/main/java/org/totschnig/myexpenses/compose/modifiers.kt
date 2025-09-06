9ac64e3f352502a0f2875789fb2e006325e515b9
Michael Totschnig <michael@totschnig.org>
2025-09-06 11:58:45 +0200

    Fixed icon rendering with large font scale

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/modifiers.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/modifiers.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +13,7 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
@@ -32,8 +34,17 @@ inline fun <T> Modifier.optional(
     ifPresent: Modifier.(T) -> Modifier
 ) = if (optional != null) ifPresent(optional) else ifAbsent()
 
+@Composable
+private fun TextUnit.toDpScaled(): Dp =
+    with(LocalDensity.current) { this@toDpScaled.toDp() }
+
+
 fun Modifier.size(spSize: TextUnit) =
-    composed { this.size(with(LocalDensity.current) { spSize.toDp() }) }
+    composed { size(spSize.toDpScaled()) }
+
+fun Modifier.width(spWidth: TextUnit): Modifier = composed {
+    width(spWidth.toDpScaled())
+}
 
 @Preview
 @Composable
@@ -88,9 +99,9 @@ private fun ConditionalTest2() {
             .conditional(
                 condition = false,
                 ifTrue = {
-                background(Color.Red)
-            }, ifFalse = {
-                background(Color.Blue)
+                    background(Color.Red)
+                }, ifFalse = {
+                    background(Color.Blue)
                 }
             )
             .padding(16.dp),
