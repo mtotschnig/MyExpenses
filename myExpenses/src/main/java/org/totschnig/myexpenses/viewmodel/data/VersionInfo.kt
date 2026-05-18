420985e90b977c69bd78bcb7256363bba6ccad4b
Michael Totschnig <michael@totschnig.org>
2026-05-18 20:06:43 +0200

    Internally distributed bug fix

--- myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/data/VersionInfo.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/viewmodel/data/VersionInfo.kt
@@ -168,6 +168,10 @@ class VersionInfo(val code: Int, val name: String) : Parcelable {
                 "${t(R.string.pref_translation_title)} : ${Locale("sv").displayLanguage}"
             )
 
+            "4081" -> arrayOf(
+                "Fix for ForegroundServiceDidNotStartInTimeException"
+            )
+
             else -> {
                 //noinspection DiscouragedApi
                 val resId = res.getIdentifier(
