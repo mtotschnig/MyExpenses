mtotschnig/MyExpenses

commit d0a093b0946efac75a03ce7e896e26d43c2f2381

Fixes #2008

myExpenses/src/main/java/org/totschnig/myexpenses/provider/BaseTransactionProvider.kt
@@ -2613,6 +2613,21 @@ abstract class BaseTransactionProvider : ContentProvider() {
                         arrayOf(oldCode)
                     )
                 }
+                if (values.containsKey(KEY_LABEL)) {
+                    val label = values.getAsString(KEY_LABEL)
+                    val effectiveCode = code ?: query(
+                        TABLE_CURRENCIES, arrayOf(KEY_CODE), "$KEY_ROWID = ?",
+                        arrayOf(id.toString()), null, null, null
+                    ).use { if (it.moveToFirst()) it.getString(0) else null }
+                    if (effectiveCode != null) {
+                        update(
+                            TABLE_ACCOUNTS,
+                            ContentValues(1).apply { put(KEY_LABEL, label) },
+                            "$KEY_CURRENCY = ? AND $KEY_IS_PORTFOLIO = ?",
+                            arrayOf(effectiveCode, PORTFOLIO_ASSET.toString())
+                        )
+                    }
+                }
                 if (oldFractionDigits != null && values.containsKey(KEY_FRACTION_DIGITS)) {
                     accountsUpdated = updateFractionDigits(
                         this,

commit 80645fd7958f10d0a7af6a517d797759f39f1cea

Fix crash introduced by d0a093b0946efac75a03ce7e896e26d43c2f2381: 1) Edit a commodity currency used as subaccount, and change fraction digits or 2) App crashed with  android.database.sqlite.SQLiteConstraintException: NOT NULL constraint failed: accounts.label (code 1299 SQLITE_CONSTRAINT_NOTNULL)

myExpenses/src/main/java/org/totschnig/myexpenses/provider/BaseTransactionProvider.kt
@@ -2620,7 +2620,7 @@ abstract class BaseTransactionProvider : ContentProvider() {
                         TABLE_CURRENCIES, arrayOf(KEY_CODE), "$KEY_ROWID = ?",
                         arrayOf(id.toString()), null, null, null
                     ).use { if (it.moveToFirst()) it.getString(0) else null }
-                    if (effectiveCode != null) {
+                    if (effectiveCode != null && label != null) {
                         update(
                             TABLE_ACCOUNTS,
                             ContentValues(1).apply { put(KEY_LABEL, label) },