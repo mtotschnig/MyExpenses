721b765dac16bc8829b632a9ac3d4decd5567ead
Michael Totschnig <michael@totschnig.org>
2026-08-29 13:01:43 +0200

    Allow one more item on landscape phone rail

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/main/MainScreen.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/main/MainScreen.kt
@@ -274,9 +274,7 @@ fun MainScreenAdaptive(
             )
 
     val maxQuickItems = if (
-        (layoutType == NavigationSuiteType.ShortNavigationBarMedium) ||
-        (isRail && !isLandscape)
-    ) 2 else 1
+        (layoutType == NavigationSuiteType.ShortNavigationBarMedium) || isRail) 2 else 1
 
     val quickItems = menuConfig.take(maxQuickItems)
     val overflowItems = menuConfig.drop(maxQuickItems)
