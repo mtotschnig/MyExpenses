f53dc7b2e24787d341971e7092c21f0a5d13c5d2
Michael Totschnig <michael@totschnig.org>
2026-08-31 18:38:31 +0200

    Add back Credits for translators. make sure icon credits have enough vertical space

--- myExpenses/src/main/java/org/totschnig/myexpenses/dialog/MoreInfoDialogFragment.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/dialog/MoreInfoDialogFragment.kt
@@ -5,7 +5,6 @@ import android.graphics.drawable.Animatable2.AnimationCallback
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Pair
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -13,12 +12,11 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import org.totschnig.myexpenses.BuildConfig
 import org.totschnig.myexpenses.R
-import org.totschnig.myexpenses.activity.PreferenceActivity
 import org.totschnig.myexpenses.databinding.MoreInfoBinding
+import org.totschnig.myexpenses.util.TranslatorsHelper.buildTranslationCredits
 import org.totschnig.myexpenses.util.Utils
 import org.totschnig.myexpenses.util.distrib.DistributionHelper
 import org.totschnig.myexpenses.util.ui.UiUtils
-import java.util.Locale
 
 class MoreInfoDialogFragment : DialogViewBinding<MoreInfoBinding>() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -35,24 +33,26 @@ class MoreInfoDialogFragment : DialogViewBinding<MoreInfoBinding>() {
                     "${if (project.containsKey("extra_info")) "$name (${project["extra_info"]})" else name}, from ${project["url"]}, licenced under ${project["licence"]}"
                 }.toList(), R.drawable.ic_menu_forward
         )
-        val lines: List<CharSequence> = listOf(
-            *resources.getStringArray(R.array.additional_credits),
-            "${getString(R.string.translated_by)}: ${buildTranslationCredits()}"
-        )
         binding.additionalContainer.text =
-            Utils.makeBulletList(context, lines, R.drawable.ic_menu_forward)
-        val nounIcons =
-            listOf<CharSequence>(*resources.getStringArray(R.array.noun_icon_credits))
+            Utils.makeBulletList(context, resources.getStringArray(R.array.additional_credits).toList(), R.drawable.ic_menu_forward)
+
+        binding.translatorsContainer.text =
+            Utils.makeBulletList(context, context.buildTranslationCredits(), R.drawable.ic_menu_forward)
+
+
+        val nounIcons = listOf<CharSequence>(*resources.getStringArray(R.array.noun_icon_credits))
         //Warning TypedArray.close is API 31 -> We can not use use (tryWithResources)
         resources.obtainTypedArray(R.array.noun_icon_credits_keys).let {
-            val height = UiUtils.dp2Px(32f, resources)
             val drawablePadding = UiUtils.dp2Px(8f, resources)
+            val verticalMargin = UiUtils.dp2Px(2f, resources)
             var i = 0
             while (i < it.length()) {
                 val textView = TextView(context)
                 textView.gravity = Gravity.CENTER_VERTICAL
                 val layoutParams =
-                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height)
+                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                layoutParams.topMargin = verticalMargin
+                layoutParams.bottomMargin = verticalMargin
                 textView.layoutParams = layoutParams
                 textView.compoundDrawablePadding = drawablePadding
                 textView.text = nounIcons[i]
@@ -92,24 +92,5 @@ class MoreInfoDialogFragment : DialogViewBinding<MoreInfoBinding>() {
         binding.useanimationsNotificationv4.startAnimation()
     }
 
-    private fun buildTranslationCredits() =
-        resources.getStringArray(R.array.pref_ui_language_values)
-            .map { lang ->
-                val parts = lang.split("-".toRegex()).toTypedArray()
-                Pair.create(
-                    lang,
-                    (requireActivity() as PreferenceActivity).getTranslatorsArrayResId(
-                        parts[0],
-                        if (parts.size == 2) parts[1].lowercase(Locale.ROOT) else null
-                    )
-                )
-            }
-            .filter { pair -> pair.second != 0 }
-            .map { pair -> Pair.create(pair.first, resources.getStringArray(pair.second)) }
-            .flatMap { pair -> pair.second.map { name -> Pair.create(name, pair.first) } }
-            .groupBy({ it.first }, { it.second })
-            .toSortedMap()
-            .map { entry -> "${entry.key} (${entry.value.joinToString(", ")})" }
-            .joinToString(", ")
 
 }
