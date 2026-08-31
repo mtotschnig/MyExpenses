37ccc14dcb3424f58189502ab58ba6f43c8592e0
Michael Totschnig <michael@totschnig.org>
2026-08-31 18:38:45 +0200

    Fix content description

--- myExpenses/src/main/java/org/totschnig/myexpenses/compose/main/MainScreen.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/compose/main/MainScreen.kt
@@ -311,7 +311,7 @@ fun MainScreenAdaptive(
                             IconButton(onClick = { onAppEvent(AppEvent.ToggleNavigation) }) {
                                 Icon(
                                     Icons.Default.Close,
-                                    contentDescription = stringResource(R.string.drawer_open)
+                                    contentDescription = stringResource(R.string.drawer_close)
                                 )
                             }
                         },
