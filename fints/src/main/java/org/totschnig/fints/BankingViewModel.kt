5bb0b0441b4ac877d6776e32333b643ba0df264a
Michael Totschnig <michael@totschnig.org>
2026-09-21 17:31:41 +0200

    DKKKU support from hbci4java

--- fints/src/main/java/org/totschnig/fints/BankingViewModel.kt
+++ fints/src/main/java/org/totschnig/fints/BankingViewModel.kt
@@ -550,6 +550,13 @@ class BankingViewModel(application: Application) : ContentResolvingAndroidViewMo
                                 setParam("my", konto)
                                 log("Setting my param to $konto")
                                 setStartParam(accountInformation.lastSynced!!)
+                                if (gv == GV.DKKKU) {
+                                    if (!setCreditCardParameters(konto)) {
+                                        error("Das Kreditkartenkonto besitzt keine Kartennummer")
+                                        _workState.value = WorkState.Abort
+                                        return@doHBCI
+                                    }
+                                }
                                 addToQueue()
                             }
                     }
@@ -643,6 +650,23 @@ class BankingViewModel(application: Application) : ContentResolvingAndroidViewMo
         )
     }
 
+    private fun HBCIJob<*>.setCreditCardParameters(konto: Konto): Boolean {
+        val cardNumber = konto.number
+        if (cardNumber == null || cardNumber.isBlank()) return false
+
+        setParam("cardnumber", cardNumber)
+
+        val cardSubNumber = konto.subnumber
+        if (cardSubNumber != null && !cardSubNumber.isBlank()) {
+            setParam("cardsubnumber", cardSubNumber)
+        }
+        setParam(
+            "enddate",
+            Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant())
+        )
+        return true
+    }
+
     private fun HBCIPassport.supportedGvs() =
         GV.entries.filter { bpd.supports(it) }
 
@@ -673,7 +697,7 @@ class BankingViewModel(application: Application) : ContentResolvingAndroidViewMo
                         _workState.value = WorkState.Loading(
                             getString(
                                 RF.string.progress_importing_account,
-                                konto.iban
+                                konto.iban ?: konto.number
                             )
                         )
                         val umsatzJob = handle.newJob(targetAccountConfig.gv.jobName)
@@ -684,7 +708,14 @@ class BankingViewModel(application: Application) : ContentResolvingAndroidViewMo
                         }
                         log("Setting my param to $kontoParam")
                         umsatzJob.setParam("my", kontoParam)
-                        startDate?.let { umsatzJob.setStartParam(startDate) }
+
+                        startDate ?.let { umsatzJob.setStartParam(startDate) }
+                        if (targetAccountConfig.gv == GV.DKKKU) {
+                            if (!umsatzJob.setCreditCardParameters(konto)) {
+                                error("Das Kreditkartenkonto besitzt keine Kartennummer")
+                                return@doHBCI
+                            }
+                        }
 
                         try {
                             umsatzJob.addToQueue()
@@ -717,7 +748,10 @@ class BankingViewModel(application: Application) : ContentResolvingAndroidViewMo
                             ?.let { config -> config.targetAccountId to this@BankingViewModel.accounts.value.first { it.id == config.targetAccountId }.type!! }
                             ?: run {
                                 val accountType =
-                                    repository.findAccountType(AccountType.BANK.name)!!
+                                    repository.findAccountType(
+                                        (if (targetAccountConfig.gv == GV.DKKKU) AccountType.CCARD
+                                        else AccountType.BANK).name
+                                    )!!
                                 repository.createAccount(
                                     konto.toAccount(
                                         bank,
